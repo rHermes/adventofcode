0b8fd5b26d0c0ac064a9160b9fbfaf23695f3184
rHermes <teodor@spaeren.no>
2019-12-09 06:47:46 +0100

    2019 Day 9
    This was very fun as usual with the intcode stuff! I really hope we get
    to see much more of it!
    
    It really did bite me in the ass though, that I had not cleaned up my
    intcode implementation earlier. This would have been a 2 minute job and
    an easy leaderboard placing if I had, but instead I ended up spending my
    time reimplementing how I get the parameters.
    
    Mark my words, there will be a day where we have to reverse engineer
    some intcode and I will have to write a disassembler then. But I'll take
    that when we come there.
    
    Score:
          --------Part 1--------   --------Part 2--------
    Day       Time   Rank  Score       Time   Rank  Score
      9   00:43:45    893      0   00:44:17    839      0

---
+++ 2019/09/y2019_d09_p02.py
@@ -0,0 +1,122 @@
+import fileinput
+import itertools as it
+from collections import defaultdict
+
+
+
+def get_params(ops, eip, base, nargs, last_dst=False):
+    op = ops[eip]
+    ddd = ("{:0" + str(nargs+2) + "}").format(op)
+    parms = list(reversed([int(x) for x in ddd[:-2]]))
+
+    if last_dst and parms[-1] == 1:
+        raise Exception("With last destination, you cannot have an intermedite value")
+
+    outs = []
+    for i, x in enumerate(parms):
+        a = ops[eip+1+i]
+        if x == 0:
+            if last_dst and i == (len(parms)-1):
+                outs.append(a)
+            else:
+                outs.append(ops[a])
+        elif x == 1:
+            outs.append(a)
+        elif x == 2:
+            if last_dst and i == (len(parms)-1):
+                outs.append(a+base)
+            else:
+                outs.append(ops[a+base])
+        else:
+            raise Exception("Not a valid instruction")
+
+    return outs
+
+def exec(ops, inputs):
+    #ops = [int(x) for x in s.split(",")]
+    
+    mops = defaultdict(int)
+
+    for i, x in enumerate(ops):
+        mops[i] = x
+
+    ops = mops
+
+    outputs = []
+    eip = 0
+    base = 0
+    while True:
+        op = ops[eip]
+        bop = op % 100
+
+        if bop == 1: # ADD 
+            a, b, dst = get_params(ops, eip, base, 3, last_dst=True)
+            ops[dst] = a + b
+            eip += 4
+
+        elif bop == 2: # MULT
+            a, b, dst = get_params(ops, eip, base, 3, last_dst=True)
+            ops[dst] = a * b
+            eip += 4
+
+        elif bop == 3: # INPUT
+            dst, = get_params(ops, eip, base, 1, last_dst=True)
+            
+            # We are faking the input
+            ins = inputs.pop(0)
+            ops[dst] = ins
+
+            eip += 2
+        
+        elif bop == 4: # OUTPUT
+            src, = get_params(ops, eip, base, 1)
+            outputs.append(src)
+
+            eip += 2
+
+        elif bop == 5: # JT
+            a, b = get_params(ops, eip, base, 2)
+
+            if a != 0:
+                eip = b
+            else:
+                eip += 3
+
+        elif bop == 6: # JF
+            a, b = get_params(ops, eip, base, 2)
+
+            if a == 0:
+                eip = b
+            else:
+                eip += 3
+
+        elif bop == 7: # LT
+            a, b, dst = get_params(ops, eip, base, 3, last_dst=True)
+            ops[dst] = int(a < b)
+            eip += 4
+
+        elif bop == 8: # EQ
+            a, b, dst = get_params(ops, eip, base, 3, last_dst=True)
+            ops[dst] = int(a == b)
+            eip += 4
+
+        elif bop == 9: # BASE
+            a, = get_params(ops, eip, base, 1)
+            base += a
+            eip += 2
+
+        elif bop == 99:
+            break
+        else:
+            raise Exception("Unkown op: {}".format(op))
+
+    
+    return (ops, outputs)
+
+def solve(s):
+    codes = [int(x) for x in s.split(",")]
+    ops, outs = exec(codes, [2])
+    return outs[0]
+
+for line in fileinput.input():
+    print(solve(line.strip()))
