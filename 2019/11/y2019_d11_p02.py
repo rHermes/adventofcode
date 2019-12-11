c53b26742957941239077ee3f4b59e5163b168c6
rHermes <teodor@spaeren.no>
2019-12-11 06:36:14 +0100

    2019 Day 11
    Intcode makes a return and that always means fun! I really should have
    made a seperate module for my intcode by now or made it stopable and
    startable, but I have not. I kind of like the fact that all of my
    solutions so far are standalone.
    
    Part 1 and Part 2 are both easy in python, due to defaultdict. I look
    forward to trying to implement these in other programming languages.
    
    The description is close to something called langtons ants, going to
    look at the intcode and see if it really is :)
    
    Score:
          --------Part 1--------   --------Part 2--------
    Day       Time   Rank  Score       Time   Rank  Score
     11   00:16:03    244      0   00:21:24    218      0

---
+++ 2019/11/y2019_d11_p02.py
@@ -0,0 +1,166 @@
+import fileinput
+import itertools as it
+from collections import defaultdict
+
+
+def get_params(ops, eip, base, nargs, last_dst=False):
+    op = ops[eip]
+    ddd = ("{:0" + str(nargs+2) + "}").format(op)
+    parms = list(reversed([int(x) for x in ddd[:-2]]))
+
+    if last_dst and parms[-1] == 1:
+        raise Exception("With last destination, you cannot have an intermedite value")
+
+    outs = []
+    for i, x in enumerate(parms):
+        a = ops[eip+1+i]
+        if x == 0:
+            if last_dst and i == (len(parms)-1):
+                outs.append(a)
+            else:
+                outs.append(ops[a])
+        elif x == 1:
+            outs.append(a)
+        elif x == 2:
+            if last_dst and i == (len(parms)-1):
+                outs.append(a+base)
+            else:
+                outs.append(ops[a+base])
+        else:
+            raise Exception("Not a valid instruction")
+
+    return outs
+
+def exec(ops):
+    plane = defaultdict(bool)
+    plane[(0,0)] = True 
+    direction = "N"
+    x, y = 0, 0
+    trans = {
+            "N": {0: "W", 1: "E"},
+            "E": {0: "N", 1: "S"},
+            "S": {0: "E", 1: "W"},
+            "W": {0: "S", 1: "N"}
+            }
+    mov = {
+            "N": (0,1),
+            "E": (1,0),
+            "S": (0,-1),
+            "W": (-1,0)
+            }
+
+    phase = 0
+    
+    mops = defaultdict(int)
+
+    for i, x in enumerate(ops):
+        mops[i] = x
+
+    ops = mops
+
+
+    outputs = []
+    eip = 0
+    base = 0
+    while True:
+        op = ops[eip]
+        bop = op % 100
+
+        if bop == 1: # ADD 
+            a, b, dst = get_params(ops, eip, base, 3, last_dst=True)
+            ops[dst] = a + b
+            eip += 4
+
+        elif bop == 2: # MULT
+            a, b, dst = get_params(ops, eip, base, 3, last_dst=True)
+            ops[dst] = a * b
+            eip += 4
+
+        elif bop == 3: # INPUT
+            dst, = get_params(ops, eip, base, 1, last_dst=True)
+            
+            ops[dst] = int(plane[(x,y)])
+
+            eip += 2
+        
+        elif bop == 4: # OUTPUT
+            src, = get_params(ops, eip, base, 1)
+            outputs.append(src)
+            if phase == 0:
+                phase = 1
+                plane[(x,y)] = bool(src)
+            else:
+                direction = trans[direction][src]
+                dx,dy = mov[direction]
+                x, y = x + dx, y + dy
+                phase = 0
+
+            eip += 2
+
+        elif bop == 5: # JT
+            a, b = get_params(ops, eip, base, 2)
+
+            if a != 0:
+                eip = b
+            else:
+                eip += 3
+
+        elif bop == 6: # JF
+            a, b = get_params(ops, eip, base, 2)
+
+            if a == 0:
+                eip = b
+            else:
+                eip += 3
+
+        elif bop == 7: # LT
+            a, b, dst = get_params(ops, eip, base, 3, last_dst=True)
+            ops[dst] = int(a < b)
+            eip += 4
+
+        elif bop == 8: # EQ
+            a, b, dst = get_params(ops, eip, base, 3, last_dst=True)
+            ops[dst] = int(a == b)
+            eip += 4
+
+        elif bop == 9: # BASE
+            a, = get_params(ops, eip, base, 1)
+            base += a
+            eip += 2
+
+        elif bop == 99:
+            break
+        else:
+            raise Exception("Unkown op: {}".format(op))
+
+    
+    return (ops, outputs, plane)
+
+def solve(s):
+    codes = [int(x) for x in s.split(",")]
+    _, _, plane = exec(codes)
+
+    minx, miny = 10000000, 100000000
+    maxx, maxy = -10000000, -1000000
+    
+    # Figure out range for identifier
+    for x,y in plane.keys():
+        if plane[(x,y)] == False:
+            continue
+        minx, miny = min(minx, x), min(miny, y)
+        maxx, maxy = max(maxx, x), max(maxy, y)
+
+    
+    buf = ""
+    for y in range(maxy,miny-1,-1):
+        for x in range(minx,maxx+1):
+            if plane[(x,y)]:
+                buf += "X"
+            else:
+                buf += " "
+        buf += "\n"
+
+    return buf[:-1]
+
+for line in fileinput.input():
+    print(solve(line.strip()))
