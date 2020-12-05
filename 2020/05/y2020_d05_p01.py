18085c06559fa10a3b82c3222cb56eb629e0384e
rHermes <teodor@spaeren.no>
2020-12-05 09:17:37 +0100

    Cleanup of 2020 Day 5

--- 2020/05/y2020_d05_p01.py
+++ 2020/05/y2020_d05_p01.py
@@ -1,38 +1,15 @@
 import fileinput
 
+# The list is of true, meaning upper, and false meaning lower.
+def bsp(a):
+    s = [0, 2**len(a) - 1]
+    for d in a:
+        s[not d] = (s[0] + s[1]) // 2 + d
 
-def pass_to_seat(p):
-    s = [0, 127]
-    for c in p[:7]:
-        if c == 'F':
-            s[1] = (s[0] + s[1]) // 2
-        elif c == 'B':
-            s[0] = (s[0] + s[1]) // 2 + 1
-        else:
-            raise Exception("WTF")
+    return s[0]
 
-
-    assert(s[0] == s[1])
-
-    k = [0, 7]
-    for c in p[7:]:
-        if c == 'L':
-            k[1] = (k[0] + k[1]) // 2
-        elif c == 'R':
-            k[0] = (k[0] + k[1]) // 2 + 1
-        else:
-            raise Exception("WTF")
+def pass_to_id(p):
+    b = [x in 'BR' for x in p]
+    return bsp(b[:7])*8 + bsp(b[7:10])
     
-    assert(k[0] == k[1])
-                
-    # return (s[0], k[0]) 
-    return s[0]*8 + k[0] 
-
-ans = 0
-passes = [line.rstrip() for line in fileinput.input()]
-
-print(max(map(pass_to_seat, passes)))
-# for p in passes:
-#     print("{}: {}".format(p, pass_to_seat(p)))
-
-
+print(max(map(pass_to_id, fileinput.input())))
--- 2020/05/y2020_d05_p02.py
+++ 2020/05/y2020_d05_p02.py
@@ -1,51 +1,16 @@
 import fileinput
 
+# The list is of true, meaning upper, and false meaning lower.
+def bsp(a):
+    s = [0, 2**len(a) - 1]
+    for d in a:
+        s[not d] = (s[0] + s[1]) // 2 + d
 
-def pass_to_seat(p):
-    s = [0, 127]
-    for c in p[:7]:
-        if c == 'F':
-            s[1] = (s[0] + s[1]) // 2
-        elif c == 'B':
-            s[0] = (s[0] + s[1]) // 2 + 1
-        else:
-            raise Exception("WTF")
+    return s[0]
 
+def pass_to_id(p):
+    b = [x in 'BR' for x in p]
+    return bsp(b[:7])*8 + bsp(b[7:10])
 
-    assert(s[0] == s[1])
-
-    k = [0, 7]
-    for c in p[7:]:
-        if c == 'L':
-            k[1] = (k[0] + k[1]) // 2
-        elif c == 'R':
-            k[0] = (k[0] + k[1]) // 2 + 1
-        else:
-            raise Exception("WTF")
-    
-    assert(k[0] == k[1])
-                
-    return (s[0], k[0]) 
-
-def seat_to_id(s):
-    return s[0]*8 + s[1]
-
-ans = 0
-passes = [line.rstrip() for line in fileinput.input()]
-
-places = set()
-ids = set()
-for x in range(128):
-    for y in range(8):
-        places.add((x,y))
-
-
-for p in passes:
-    s = pass_to_seat(p)
-    places.remove(s)
-    ids.add(seat_to_id(s))
-
-for s in places:
-    i = seat_to_id(s)
-    if i+1 in ids and i-1 in ids:
-        print(s, i)
+seen = set(map(pass_to_id, fileinput.input()))
+print(next(x-1 for x in seen if x-1 not in seen and x-2 in seen))
