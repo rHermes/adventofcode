af83a347a43712e6f056c058cb98fff30c2886e1
rHermes <teodor@spaeren.no>
2020-12-06 07:33:14 +0100

    Cleanup of 2020 Day 6
    This became two one liners, quite nice :)

--- 2020/06/y2020_d06_p01.py
+++ 2020/06/y2020_d06_p01.py
@@ -1,21 +1,3 @@
 import fileinput
 
-
-grps = []
-ans = 0
-cur = set()
-for line in fileinput.input():
-    line = line.rstrip()
-
-    if line == "":
-        ans += len(cur)
-        cur.clear()
-    else:
-        for c in line:
-            cur.add(c)
-
-ans += len(cur)
-
-print(ans)
-
-
+print(sum(len(set(x) - set("\n")) for x in "".join(fileinput.input()).split("\n\n")))
--- 2020/06/y2020_d06_p02.py
+++ 2020/06/y2020_d06_p02.py
@@ -1,29 +1,5 @@
-import fileinput
+import fileinput as fi
+from functools import reduce
+from operator import and_ 
 
-
-grps = []
-ans = 0
-cur = set()
-for line in fileinput.input():
-    line = line.rstrip()
-
-    if line == "":
-        wc = grps[0]
-        for k in grps[1:]:
-            wc = wc & k
-        ans += len(wc)
-        grps = []
-    else:
-        cu = set(line)
-        grps.append(cu)
-        # for c in line:
-        #     cur.add(c)
-
-wc = grps[0]
-for k in grps[1:]:
-    wc = wc & k
-ans += len(wc)
-
-print(ans)
-
-
+print(sum(len(reduce(and_, map(set, x.split("\n")))) for x in "".join(fi.input()).split("\n\n")))
