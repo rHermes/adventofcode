4973f5709f92dc30f311714bac3844899296432b
rHermes <teodor@spaeren.no>
2020-12-08 09:49:46 +0100

    Cleanup of 2020 Day 2

--- 2020/02/y2020_d02_p02.py
+++ 2020/02/y2020_d02_p02.py
@@ -3,7 +3,7 @@ import re
 
 ans = 0
 for line in fileinput.input():
-    (lo, hi, c, pwd) = re.match("^(\d+)-(\d+) ([a-z]): ([a-z]+)$", line).groups()
+    lo, hi, c, pwd = re.match("^(\d+)-(\d+) ([a-z]): ([a-z]+)$", line).groups()
 
     if sum(pwd[int(x)-1] == c for x in (lo, hi)) == 1:
         ans += 1
