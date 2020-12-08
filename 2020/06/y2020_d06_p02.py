07595ba083f050e111eca211160c228bb69c2a61
rHermes <teodor@spaeren.no>
2020-12-08 09:58:23 +0100

    Cleanup of 2020 Day 6
    This reduces the number of imports

--- 2020/06/y2020_d06_p02.py
+++ 2020/06/y2020_d06_p02.py
@@ -1,5 +1,3 @@
 import fileinput as fi
-from functools import reduce
-from operator import and_ 
 
-print(sum(len(reduce(and_, map(set, x.split("\n")))) for x in "".join(fi.input()).split("\n\n")))
+print(sum(len(set.intersection(*map(set, x.split("\n")))) for x in "".join(fi.input()).split("\n\n")))
