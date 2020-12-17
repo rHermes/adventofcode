c282aac820c3c3448b91eca67a1b414814a1b990
rHermes <teodor@spaeren.no>
2020-12-17 07:41:28 +0100

    Cleanup of 2020 Day 17
    Once I switched over to just tracking the active cells it became a lot
    faster. Boring solution to a boring task :/

--- 2020/17/y2020_d17_p01.py
+++ 2020/17/y2020_d17_p01.py
@@ -1,35 +1,5 @@
 import fileinput as fi
-import collections
-
-# findall
-# search
-# parse
-
-lines = []
-
-for line in fi.input():
-    if line.rstrip():
-        lines.append(line.rstrip())
-
-# lines = "".join(fi.input()).rstrip().split("\n\n")
-# lines = "".join(fi.input()).rstrip().split("\n")
-# for line in fi.input():
-#     if line.rstrip():
-#     else:
-#         cur.append()
-
-grid = collections.defaultdict(lambda: collections.defaultdict(lambda: collections.defaultdict(bool)))
-
-y = 0
-gx = 0
-for line in lines:
-    for i,x in enumerate(line):
-        grid[0][y][i] = (x == '#')
-    gx = len(line)
-    y += 1
-
-# print(gx, y)
-
+from collections import defaultdict
 
 def around(x, y, z):
     a = set()
@@ -38,61 +8,40 @@ def around(x, y, z):
             for k in range(-1,2):
                 a.add((x+i,y+j,z+k))
 
-    
     a.remove((x,y,z))
     return a
 
-def print_board(g, gx, gy, gz):
-    for z in range(-(gz),(gz+1)):
-        print("z={}".format(z))
-        for y in range(gy):
-            for x in range(gx):
-                if g[z][y][x]:
-                    print("#", end="")
-                else:
-                    print(".", end="")
-            print()
-        print()
+def step(G):
+    heat = defaultdict(int)
+    for l in G:
+        # This is to make sure we evaluate nodes even if they have no one around them
+        heat[l] += 0
+        for p in around(*l):
+            heat[p] += 1
 
-def step(g, gx, gy, gz):
-    changes = []
-    for z in range(-(gz),(gz+1)):
-        for y in range(-gy,gy):
-            for x in range(-gx,gx):
-                arr = sum(g[az][ay][ax] for (ax,ay,az) in around(x,y,z))
-                ll = g[z][y][x]
-                if ll:
-                    if arr not in [2,3]:
-                        changes.append((x,y,z,False))
-                else:
-                    if arr == 3:
-                        changes.append((x,y,z,True))
+    add = set()
+    remove = set()
+    for l, v in heat.items():
+        if l in G:
+            if v not in [2,3]:
+                remove.add(l)
+        else:
+            if v == 3:
+                add.add(l)
 
-    for x,y,z,n in changes:
-        g[z][y][x] = n
+    # Update in place, to avoid allocating more memory
+    G -= remove
+    G |= add
 
-# print(lines)
-# print(len(around(0,0,0)))
-# print(around(0,0,0))
 
-# print(grid)
-# step(grid, gx, y, 1)
 
-# print(grid)
+G = set()
+for y, line in enumerate(map(str.rstrip, fi.input())):
+    for x, c in enumerate(line):
+        if c == '#':
+            G.add((x,y,0))
 
-for i in range(1,7):
-    # print("STEP: {}".format(i))
-    # print_board(grid, 10000,10000,i-1)
-    # print("====")
-    # step(grid,gx,y,i)
-    step(grid,100,100,i)
-    # print("====")
+for _ in range(6):
+    step(G)
 
-ans = 0
-for v in grid.values():
-    for vv in v.values():
-        for vvv in vv.values():
-            if vvv == True:
-                ans += 1
-print(ans)
-
+print(len(G))
