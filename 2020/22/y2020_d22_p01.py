386adace9a4082cc0fb6592cb821039a667627f3
rHermes <teodor@spaeren.no>
2020-12-22 07:41:59 +0100

    Cleanup of 2020 Day 22
    This turned out pretty clean. It still takes some time to run part 2,
    haven't checked the subreddit to see if there is a smarter way to do
    this.
    
    This is also a nice illustration of the power of itertools and iterators
    in general. Python bless!

--- 2020/22/y2020_d22_p01.py
+++ 2020/22/y2020_d22_p01.py
@@ -1,68 +1,23 @@
 import fileinput as fi
+from itertools import takewhile, islice
+from collections import deque
 
-import re
-import itertools as it
-import functools as ft
+def get_cards():
+    G = map(str.strip, fi.input())
+    p1d = deque(map(int, takewhile(bool, islice(G,1,None))))
+    p2d = deque(map(int, takewhile(bool, islice(G,1,None))))
+    return p1d, p2d
 
-import more_itertools as mit
-
-import math
-
-import collections
-
-import z3
-
-import numpy as np
-
-# findall
-# search
-# parse
-from parse import *
-
-INPUT = "".join(fi.input())
-
-groups = INPUT.split("\n\n")
-# print(groups[-1])
-lines = list(INPUT.splitlines())
-
-
-
-def zround(a, b):
-    aa = a.popleft()
-    bb = b.popleft()
-
-    if aa < bb:
-        b.append(bb)
-        b.append(aa)
-    elif bb < aa:
-        a.append(aa)
-        a.append(bb)
-
-    return (a, b)
-
-def zscore(a):
-    ans = 0
-    for i,x in enumerate(reversed(a),1):
-        ans += i*x
-    return ans
-
-
-# print(groups)
-
-p1d = collections.deque()
-for line in groups[0].splitlines()[1:]:
-    p1d.append(int(line))
-
-p2d = collections.deque()
-for line in groups[1].splitlines()[1:]:
-    p2d.append(int(line))
-
-
+p1d, p2d = get_cards()
 
 while p1d and p2d:
-    # print("P1", p1d)
-    # print("P2", p2d)
-    p1d, p2d = zround(p1d, p2d)
-print(max([zscore(p1d), zscore(p2d)]))
-# print("p1d", zscore(p1d))
-# print("p2d", zscore(p2d))
+    p1, p2 = p1d.popleft(), p2d.popleft()
+    if p2 < p1:
+        p1d.extend((p1, p2))
+    else:
+        p2d.extend((p2, p1))
+
+ans = 0
+for i, x in enumerate(reversed(p1d or p2d), 1):
+    ans += i*x
+print(ans)
--- 2020/22/y2020_d22_p02.py
+++ 2020/22/y2020_d22_p02.py
@@ -1,127 +1,47 @@
 import fileinput as fi
+from itertools import takewhile, islice
+from collections import deque
 
-import re
-import itertools as it
-import functools as ft
+def get_cards():
+    G = map(str.strip, fi.input())
+    p1d = deque(map(int, takewhile(bool, islice(G,1,None))))
+    p2d = deque(map(int, takewhile(bool, islice(G,1,None))))
+    return p1d, p2d
 
-import more_itertools as mit
+# This returns the object we use to lookup in the hash
+def zsig(p1d, p2d):
+    return (tuple(p1d), tuple(p2d))
 
-import math
+# Returns True for p1d and False for p2d
+# modifies both p1d and p2d. Seen is a set
+def game(p1d, p2d, seen):
+    while p1d and p2d:
+        zs = zsig(p1d, p2d)
+        if zs in seen:
+            return True
+        seen.add(zs)
 
-import collections
+        p1, p2 = p1d.popleft(), p2d.popleft()
 
-import z3
+        if p1 <= len(p1d) and p2 <= len(p2d):
+            z1d = deque(islice(p1d, p1))
+            z2d = deque(islice(p2d, p2))
+            winner = game(z1d, z2d, set())
+        else:
+            winner = p2 < p1
 
-import numpy as np
+        if winner:
+            p1d.extend((p1, p2))
+        else:
+            p2d.extend((p2, p1))
 
-# findall
-# search
-# parse
-from parse import *
-
-import sys
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-INPUT = "".join(fi.input())
-
-groups = INPUT.split("\n\n")
-# print(groups[-1])
-lines = list(INPUT.splitlines())
-
-def do_nothing(*args, **kwargs): pass
-
-debug = False
-if debug:
-    gprint = print
-else:
-    gprint = do_nothing
-
-def zround(a, b, cache, game=1, prevgame=0, rr=1):
-    assert(a)
-    assert(b)
-    if game != prevgame:
-        gprint("=== Game {} ===".format(game))
-
-    gprint("")
-    gprint("-- Round {} (Game {}) --".format(rr, game))
-    gprint("Player 1's deck: {}".format(", ".join(map(str,a))))
-    gprint("Player 2's deck: {}".format(", ".join(map(str,b))))
+    return bool(p1d)
 
 
-    if (tuple(a),tuple(b)) in cache:
-        gprint(" seen before!!!!!")
-        return True
+p1d, p2d = get_cards()
+game(p1d, p2d, set())
 
-    cache.add((tuple(a),tuple(b)))
-
-    # drawing top card
-    aa = a.popleft()
-    bb = b.popleft()
-    gprint("Player 1 plays: {}".format(aa))
-    gprint("Player 2 plays: {}".format(bb))
-
-    if len(a) >= aa and len(b) >= bb:
-        za = collections.deque(list(a.copy())[:aa])
-        zb = collections.deque(list(b.copy())[:bb])
-        gprint("Playing a sub-game to determine the winner...")
-        gprint("")
-        winner = zround(za, zb, set(),game=game+1,prevgame=game,rr=1)
-        gprint("\n...anyway, back to game {}.".format(game))
-    else:
-        if aa < bb:
-            winner = False
-        elif bb < aa:
-            winner = True
-
-    if winner:
-        gprint("Player 1 wins round {} of game {}!".format(rr, game))
-        a.append(aa)
-        a.append(bb)
-    else:
-        gprint("Player 2 wins round {} of game {}!".format(rr, game))
-        b.append(bb)
-        b.append(aa)
-    
-    # Ending game
-    if len(a) == 0:
-        gprint("The winner of game {} is player 2!".format(game))
-        return False
-    elif len(b) == 0:
-        gprint("The winner of game {} is player 1!".format(game))
-        return True
-    
-    return zround(a,b,cache,game=game,prevgame=game,rr=rr+1)
-
-def zscore(a):
-    ans = 0
-    for i,x in enumerate(reversed(a),1):
-        ans += i*x
-    return ans
-
-
-# gprint(groups)
-
-p1d = collections.deque()
-for line in groups[0].splitlines()[1:]:
-    p1d.append(int(line))
-
-p2d = collections.deque()
-for line in groups[1].splitlines()[1:]:
-    p2d.append(int(line))
-
-
-
-c = set()
-# while p1d and p2d:
-#     gprint()
-#     gprint("P1", p1d)
-#     gprint("P2", p2d)
-#     winner = zround(p1d, p2d, c)
-
-winner = zround(p1d, p2d, c)
-
-print(max([zscore(p1d), zscore(p2d)]))
-# print("p1d", zscore(p1d))
-# print("p2d", zscore(p2d))
+ans = 0
+for i, x in enumerate(reversed(p1d or p2d), 1):
+    ans += i*x
+print(ans)
