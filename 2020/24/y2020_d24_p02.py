988c22deb6c759c3d9344ddb4e1609ad6ebe25ca
rHermes <teodor@spaeren.no>
2020-12-24 07:20:13 +0100

    Cleanup of 2020 Day 24
    This turned out super neat! Using complex numbers and axial coordinates
    really did the trick. What turned it around for me was a comment on
    reddit which linked to a picture showing the connection between a
    triangular grid and the hexagon system.
    
    Part 2 still takes a bit more time than I want, but I don't know if
    there is anything to be done for that.

--- 2020/24/y2020_d24_p02.py
+++ 2020/24/y2020_d24_p02.py
@@ -1,120 +1,24 @@
 import fileinput as fi
-
 import re
-import itertools as it
-import functools as ft
-
-import math
-
 import collections
 
-import string
+dirs = {"e": 2+0j, "se": 1-1j, "sw": -1-1j, "w": -2+0j, "nw": -1+1j, "ne": 1+1j}
+seen = set()
+for line in fi.input():
+    seen ^= {sum(dirs[x] for x in re.findall("e|se|sw|w|nw|ne", line))}
 
-# findall
-# search
-# parse
-# from parse import *
+dd = collections.defaultdict(int)
+for _ in range(100):
+    dd.clear()
+    for tile in seen:
+        for d in dirs.values():
+            dd[tile+d] += 1
 
-import redblob
+    good = set()
+    for tile, v in dd.items():
+        if v <= 2 and (tile in seen or v == 2):
+            good.add(tile)
 
+    seen = good
 
-e, se, sw, w, nw, ne = [0,1,2,3,4,5]
-
-dirs = {"e": 0, "se": 1, "sw": 2, "w": 3, "nw": 4, "ne": 5}
-
-def solve(INPUT):
-    groups = INPUT.split("\n\n")
-    # print(groups[-1])
-    lines = list(INPUT.splitlines())
-
-    # print(INPUT)
-    # print(groups)
-
-
-    dd = collections.defaultdict(int)
-    for line in lines:
-        # print(line)
-        a = redblob.Hex(0,0,0)
-        # dd[a] += 1
-        south = False
-        north = False
-        for c in line:
-            if c == "s":
-                south = True
-            elif c == "n":
-                north = True
-            elif c == "e":
-                if south:
-                    south = False
-                    a = redblob.hex_neighbor(a, se)
-                    # dd[a] += 1
-                elif north:
-                    north = False
-                    a = redblob.hex_neighbor(a, ne)
-                    # dd[a] += 1
-                else:
-                    a = redblob.hex_neighbor(a, e)
-                    # dd[a] += 1
-            elif c == "w":
-                if south:
-                    south = False
-                    a = redblob.hex_neighbor(a, sw)
-                    # dd[a] += 1
-                elif north:
-                    north = False
-                    a = redblob.hex_neighbor(a, nw)
-                    # dd[a] += 1
-                else:
-                    a = redblob.hex_neighbor(a, w)
-
-
-        dd[a] += 1
-
-    flipped = 0
-    zzz = set()
-    for k, v in dd.items():
-        if v % 2:
-            zzz.add(k)
-            flipped += 1
-        # print("WOW")
-
-    # print(dd)
-    # print(flipped)
-    for i in range(100):
-        added = set()
-        removed = set()
-        lookers = set()
-        for tile in zzz:
-            count = 0
-            for d in dirs.values():
-                btile = redblob.hex_neighbor(tile, d)
-                if btile in zzz:
-                    count += 1
-                else:
-                    lookers.add(btile)
-
-            if count == 0 or count > 2:
-                removed.add(tile)
-
-        for tile in lookers:
-            count = 0
-            for d in dirs.values():
-                btile = redblob.hex_neighbor(tile, d)
-                if btile in zzz:
-                    count += 1
-
-            if count == 2:
-                added.add(tile)
-
-        zzz = (zzz - removed) | added
-        # print("Day {}: {}".format(i+1, len(zzz)))
-
-    return len(zzz)
-
-
-
-
-
-
-INPUT = "".join(fi.input()).rstrip()
-print(solve(INPUT))
+print(len(seen))
