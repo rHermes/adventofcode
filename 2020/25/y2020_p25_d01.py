4a7e02df3bad20a7db314a6160fdfbe87d4de945
rHermes <teodor@spaeren.no>
2020-12-25 07:22:15 +0100

    Cleanup of 2020 Day 25
    It runs quite fast, even without the powmod. I read on wikipedia that
    this problem is called the Discrete logarithm problem and there are
    faster algorithms, and I might try to implement them.

--- 2020/25/y2020_p25_d01.py
+++ 2020/25/y2020_p25_d01.py
@@ -1,71 +1,15 @@
 import fileinput as fi
 
-import re
-import itertools as it
-import functools as ft
+card, door = [int(x) for x in fi.input()]
 
-import more_itertools as mit
-
-import math
-
-import collections
-
-import z3
-
-import numpy as np
-
-import string
-
-# findall
-# search
-# parse
-from parse import *
-
-from tqdm import tqdm
-
-
-def trans(s, n):
-    v = 1
-    for l in range(n):
-        v = (v * s) % 20201227
-    return v
-
-# def solve(a, b, M=20201227):
-#     v = 1
-#     i = 0
-#     while v != a and v != b:
-#         v = (v*v) % M
-#         i += 1
-
-#     if v == a:
-#         return pow(b,i,M)
-#     else:
-#         return pow(a,i,M)
-
-def solve(INPUT):
-    groups = INPUT.split("\n\n")
-    # print(groups[-1])
-    lines = list(INPUT.splitlines())
-    
-    card = 12232269
-    door = 19452773
-    for n in tqdm(range(20201227+10)):
-        k = pow(7, n, 20201227)
-        # print(n, k)
-        if k == card:
-            print("Card is ", n)
-            return pow(door, n, 20201227)
-            # card_l = n
-        elif k == door:
-            print("Door is ", n)
-            return pow(card, n, 20201227)
-
-    return None
-
-
-INPUT = "".join(fi.input()).rstrip()
-print(solve(INPUT))
-# card = 12232269
-# door = 19452773
-
-# print(solve(card,door))
+M = 20201227
+v = 1
+for i in range(1,M+1):
+    v = (v*7) % M
+    if v == card:
+        print(pow(door,i,M))
+    elif v == door:
+        print(pow(card,i,M))
+    else:
+        continue
+    break
