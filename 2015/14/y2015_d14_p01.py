ff59e3ebaeaf58bd9bd1c7a361190149e0357cff
Teodor Spæren <teodor@spaeren.no>
2021-10-23 12:17:27 +0200

    Cleanup of 2015 Day 14
    Eliminated the use of a loop in part one and removed most external
    dependencies. I'm sure the second part could be made neater and to use
    less memory, but I'm not really too concerned with that. I want to do
    multiple tasks per day to really get up to speed with these tasks.

--- 2015/14/y2015_d14_p01.py
+++ 2015/14/y2015_d14_p01.py
@@ -1,60 +1,20 @@
 import fileinput as fi
 
-import re
-import itertools as it
-import functools as ft
-
-import more_itertools as mit
-
-import math
-
-import collections
-
-import z3
-
-import numpy as np
-
-import string
-
-# findall
-# search
-# parse
-from parse import *
-
-INPUT = "".join(fi.input()).rstrip()
-
-groups = INPUT.split("\n\n")
-# print(groups[-1])
-lines = list(INPUT.splitlines())
-
-def calc(speed, act, rest, n=2503):
+def calc(speed, act, rest, N=2503):
     dist = 0
     sec = 0
-    while sec < n:
-        i = 0
-        while i < act and sec < n:
-            dist += speed
-            i += 1
-            sec += 1
-        
-        sec += rest
+    while sec < N:
+        dist += min(act*speed, (N-sec)*speed)
+        sec += act + rest
 
     return dist
 
-print("Comet: {}".format(calc(14, 10, 127, n=10)))
-print("Comet: {}".format(calc(14, 10, 127, n=1000)))
-print("Dancer: {}".format(calc(16, 11, 162, n=10)))
-print("Dancer: {}".format(calc(16, 11, 162, n=1000)))
-
 dists = []
-for line in lines:
-    # print(line)
-    # print(parse("{} can fly {:d} km/s for {:d} seconds, but then must rest for {:d} seconds.", line))
-    name, speed, act, rest = parse("{} can fly {:d} km/s for {:d} seconds, but then must rest for {:d} seconds.", line)
-    # print("{}: {}", name, calc(speed, act, rest))
-    dists.append((calc(speed, act, rest), name))
+for line in map(str.rstrip, fi.input()):
+    if not line:
+        continue
+    prts = line.split()
+    speed, act, rest = int(prts[3]), int(prts[6]), int(prts[13])
+    dists.append(calc(speed, act, rest))
 
-print(dists)
 print(max(dists))
-
-
--- 2015/14/y2015_d14_p02.py
+++ 2015/14/y2015_d14_p02.py
@@ -1,32 +1,5 @@
 import fileinput as fi
 
-import re
-import itertools as it
-import functools as ft
-
-import more_itertools as mit
-
-import math
-
-import collections
-
-import z3
-
-import numpy as np
-
-import string
-
-# findall
-# search
-# parse
-from parse import *
-
-INPUT = "".join(fi.input()).rstrip()
-
-groups = INPUT.split("\n\n")
-# print(groups[-1])
-lines = list(INPUT.splitlines())
-
 def calc(speed, act, rest, n=2503):
     dist = [0]
     sec = 0
@@ -36,7 +9,7 @@ def calc(speed, act, rest, n=2503):
             dist.append(dist[-1] + speed)
             i += 1
             sec += 1
-        
+
         j = 0
         while j < rest and sec < n:
             dist.append(dist[-1])
@@ -46,37 +19,22 @@ def calc(speed, act, rest, n=2503):
     return dist[1:]
 
 
-def points(speeds):
-    pnts = [0 for _ in speeds]
-    for jj, x in enumerate(zip(*speeds)):
+def points(dists):
+    pnts = [0 for _ in dists]
+    for x in zip(*dists):
         mm = max(x)
         for i, y in enumerate(x):
             if y == mm:
                 pnts[i] += 1
-        # print("For {} we have {} with max {} and pnts {}".format(jj+1, x, mm, pnts))
 
     return pnts
 
-nn = 150
-speeds = [calc(14, 10, 127, n=1000), calc(16, 11, 162, n=1000)]
-
-print(points(speeds))
-# print("Comet: {}".format(calc(14, 10, 127, n=10)))
-# print("Comet: {}".format(calc(14, 10, 127, n=1000)))
-# print("Dancer: {}".format(calc(16, 11, 162, n=10)))
-# print("Dancer: {}".format(calc(16, 11, 162, n=1000)))
-
 dists = []
+for line in map(str.rstrip, fi.input()):
+    if not line:
+        continue
+    prts = line.split()
+    speed, act, rest = int(prts[3]), int(prts[6]), int(prts[13])
+    dists.append(calc(speed, act, rest))
 
-speeds = []
-names = []
-for line in lines:
-    # print(line)
-    # print(parse("{} can fly {:d} km/s for {:d} seconds, but then must rest for {:d} seconds.", line))
-    name, speed, act, rest = parse("{} can fly {:d} km/s for {:d} seconds, but then must rest for {:d} seconds.", line)
-    # print("{}: {}", name, calc(speed, act, rest))
-    names.append(name)
-    speeds.append(calc(speed, act, rest))
-
-pts = points(speeds)
-print(max(pts))
+print(max(points(dists)))
