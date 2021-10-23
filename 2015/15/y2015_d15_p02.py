587f15a1b255183dd5f71d56d909847c9d58e5f8
Teodor Spæren <teodor@spaeren.no>
2021-10-23 14:57:32 +0200

    2015 Day 15
    This one was weird, part one took way too long, given how I ended up
    solving it. Almost all the time was spent generating the possible ways
    to configure 100 across the input. Ended up doing a simple brute force
    loop here, but I'm not sure this was really the fastest way to do
    things. I'm not really sure how I will solve this one for the future,
    refining this is going to be some good practice. Some of the mental math
    was also not so easy to do.
    
    I ran into one bug here, which is that the negative totals become 0, but
    I found this rather quickly, so it was no big hinder. Important reminder
    to always read the task description.
    
    Part two was relatively easy to do and most of the time was spent
    figuring out how to do the multiplications correctly. In the future I
    should really start using np arrays for stuff like this, as it would
    have given me a lot of stuff builtin.
    
    Not pleased with my time here, but it's all good training
    
    Part one: 00:29:36
    Part two: 00:02:36

---
+++ 2015/15/y2015_d15_p02.py
@@ -0,0 +1,88 @@
+import fileinput as fi
+
+import re
+import itertools as it
+import functools as ft
+
+import more_itertools as mit
+
+import math
+
+import collections
+
+import z3
+
+import numpy as np
+
+import string
+
+# findall
+# search
+# parse
+from parse import *
+
+INPUT = "".join(fi.input()).rstrip()
+
+groups = INPUT.split("\n\n")
+# print(groups[-1])
+lines = list(INPUT.splitlines())
+
+
+def get_combs(left, n, tabs=0):
+    # print("{}called with {} {}".format(" "*tabs, left, n))
+    if n == 1:
+        yield [left]
+        return
+
+
+    for x in range(left+1):
+        for y in get_combs(left-x, n-1, tabs=tabs+1):
+            yield [x] + y
+
+
+def calc_score(times, ings):
+    s = [0 for _ in ings[0]]
+    for time, ing in zip(times, ings):
+        for i in range(len(s)):
+            s[i] += time*ing[i]
+
+    l = 1
+    for xx in s:
+        l *= max(xx,0)
+
+    return l
+
+
+
+# print(lines)
+xs = []
+cals = []
+for line in lines:
+    if not line:
+        continue
+
+    things = line.split(", ")
+    xx = [int(x.split()[-1]) for x in things]
+    xs.append(xx[:-1])
+    cals.append(xx[-1])
+
+# print(xs)
+
+ans = None
+for x in get_combs(100, len(xs)):
+    cal = 0
+    for i, xss in enumerate(x):
+        cal += xss*cals[i]
+
+    if cal != 500:
+        continue
+
+    sco = calc_score(x, xs)
+    if ans is None:
+        ans = sco
+    elif ans < sco:
+        ans = sco
+
+    # print(x, sco)
+
+print(ans)
