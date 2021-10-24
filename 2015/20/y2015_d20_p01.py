83ef4a45f186c9c98b5c2605cc3ed438b39b3571
Teodor Spæren <teodor@spaeren.no>
2021-10-24 14:03:32 +0200

    Cleanup of 2015 Day 20
    This ended up being very neat once I cleaned it up. It's still slow in
    CPython, but with pypy it's near instantaneous! I'll look at the answers
    in the thread after this.
    
    One neat trick I learned in this challenge is that you can limit an
    iterator by using "zip" and "range". "zip" will only iterate as long as
    there is data from all inputs, so it terminates on whatever is shorter.
    This is a really neat way to put limits on iterators!

--- 2015/20/y2015_d20_p01.py
+++ 2015/20/y2015_d20_p01.py
@@ -1,45 +1,15 @@
 import fileinput as fi
 
-import re
-import itertools as it
-import functools as ft
-
-import more_itertools as mit
-
-import math
-
-import collections
-
-import z3
-
-import numpy as np
-
-import string
-
-# findall
-# search
-# parse
-from parse import *
-
-import regex
-
-from tqdm import tqdm
-INPUT = "".join(fi.input()).rstrip()
-
-groups = INPUT.split("\n\n")
-# print(groups[-1])
-lines = list(INPUT.splitlines())
-
-LIMIT = 36000000
+LIMIT = int(next(fi.input()))
 UL = LIMIT//10
 
 data = [0 for _ in range(UL)]
 
-for x in tqdm(range(1, UL+1)):
+for x in range(1, UL+1):
     for y in range(x, UL+1, x):
         data[y-1] += x
 
-for i, x in enumerate(data):
-    if x >= LIMIT//10:
-        print(i+1)
+    if data[x-1]*10 >= LIMIT:
         break
+
+print(x)
