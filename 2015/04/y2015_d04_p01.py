5d7e07c7f396c0d9d799e7ff3b2bf0cf4b77c239
Teodor Spæren <teodor@spaeren.no>
2021-10-25 11:58:41 +0200

    2015 Day 4
    I'm redoing the ones I've already done, just to have them all in python.
    This one was not really that fun to program, it's just a bruteforce. It
    ends up being quite slow in python, but fast enough to be doable.
    
    Learned about the hashlib library today. The timings aren't really all
    that important here, as I've done the tasks before, but if I can pick up
    some skills then it is useful. It would also be nice to have done all
    the tasks in python, to have a reference implementation to compare other
    languages that I might end up using.
    
    Part one: 00:04:40
    Part two: 00:00:32

---
+++ 2015/04/y2015_d04_p01.py
@@ -0,0 +1,25 @@
+import fileinput as fi
+import hashlib
+
+
+INPUT = "".join(fi.input()).rstrip()
+
+groups = INPUT.split("\n\n")
+# print(groups[-1])
+lines = list(INPUT.splitlines())
+
+for line in lines:
+    key = line
+
+i = 1
+while True:
+    tkey = key + str(i)
+    m = hashlib.md5(tkey.encode("latin-1")).hexdigest()
+    if m.startswith("00000"):
+        break
+
+    i += 1
+
+print(i)
+
+
