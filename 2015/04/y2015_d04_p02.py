153f244fad07cead92880155d648f1ee37b5a7da
Teodor Spæren <teodor@spaeren.no>
2021-10-25 12:19:07 +0200

    Cleanup of 2015 Day 4
    Wasn't that much to clean up here, but it ends up being pretty neat.

--- 2015/04/y2015_d04_p02.py
+++ 2015/04/y2015_d04_p02.py
@@ -1,25 +1,14 @@
 import fileinput as fi
+import itertools as it
 import hashlib
 
+key = next(fi.input()).strip().encode("latin-1")
+m = hashlib.md5(key)
 
-INPUT = "".join(fi.input()).rstrip()
-
-groups = INPUT.split("\n\n")
-# print(groups[-1])
-lines = list(INPUT.splitlines())
-
-for line in lines:
-    key = line
-
-i = 1
-while True:
-    tkey = key + str(i)
-    m = hashlib.md5(tkey.encode("latin-1")).hexdigest()
-    if m.startswith("000000"):
+for i in map(str, it.count(1)):
+    mm = m.copy()
+    mm.update(i.encode("latin-1"))
+    if mm.hexdigest().startswith("000000"):
         break
 
-    i += 1
-
 print(i)
-
-
