db160ac20de67cb6a725e35fd5afa2289ecc7307
Teodor Spæren <teodor@spaeren.no>
2021-10-26 20:03:36 +0200

    Cleanup of 2015 Day 25
    Realized two things from reading the reddit thread! The first is that
    there is in fact a way to compute the index directly, but I haven't
    worked that out yet.
    
    The second part is that this is just modular exponentiation and I can
    simply wait with doing any complicated calculations til the end. This
    should make this a O(1) once I figure out the index calculation!
    
    I think this task is greater than it first appears ;)

--- 2015/25/y2015_d25_p01.py
+++ 2015/25/y2015_d25_p01.py
@@ -1,50 +1,26 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
 
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-import z3
-import numpy as np
-import lark
-import regex
+def solve(col, row, S=20151125):
+    M = 33554393
+    B = 252533
 
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
+    max_y = 1
+    idx = 0
+    while True:
+        y = max_y
+        x = 1
+        max_y += 1
 
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
+        while y > 0:
+            if y == row and x == col:
+                return (S * pow(B, idx, M)) % M
 
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
+            idx += 1
+            y -=1
+            x += 1
 
-# for line in lines:
-#     gprint(line)
+parts = next(fi.input()).split()
+row = int(parts[parts.index("row")+1][:-1])
+column = int(parts[parts.index("column")+1][:-1])
 
-max_y = 1
-x = 1
-l = 20151125
-while True:
-    y = max_y
-    x = 1
-    max_y += 1
-
-    while y > 0:
-        if y == 3010 and x == 3019:
-            print(l)
-            sys.exit(1)
-
-        y -=1
-        x += 1
-        l *= 252533
-        l = l % 33554393
+print(solve(column, row))
