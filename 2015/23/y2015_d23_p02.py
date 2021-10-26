7b7125eef40d4385ed8b5f1d8e4810260369c14c
Teodor Spæren <teodor@spaeren.no>
2021-10-26 17:35:10 +0200

    Cleanup of 2015 Day 23
    Not much to clean up here, other than to remove the cruft from the
    template.

--- 2015/23/y2015_d23_p02.py
+++ 2015/23/y2015_d23_p02.py
@@ -1,66 +1,33 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
 
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-import z3
-import numpy as np
-import lark
-import regex
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
+lines = list("".join(fi.input()).rstrip().splitlines())
 
 reg = {"a": 1, "b": 0}
 
 i = 0
-while i < len(lines):
-    de = lines[i].split()
+while 0 <= i < len(lines):
+    ins, *de = lines[i].split()
 
-    if de[0] == "hlf":
-        reg[de[1]] //= 2
+    if ins == "hlf":
+        reg[de[0]] //= 2
         i += 1
-
-    if de[0] == "tpl":
-        reg[de[1]] *= 3
+    elif ins == "tpl":
+        reg[de[0]] *= 3
         i += 1
-
-    if de[0] == "inc":
-        reg[de[1]] += 1
+    elif ins == "inc":
+        reg[de[0]] += 1
         i += 1
-
-    if de[0] == "jmp":
-        i += int(de[1])
-
-    if de[0] == "jie":
-        if reg[de[1][0]] % 2 == 0:
-            i += int(de[2])
+    elif ins == "jmp":
+        i += int(de[0])
+    elif ins == "jie":
+        if reg[de[0][0]] % 2 == 0:
+            i += int(de[1])
+        else:
+            i += 1
+    elif ins == "jio":
+        if reg[de[0][0]] == 1:
+            i += int(de[1])
         else:
             i += 1
 
-    if de[0] == "jio":
-        if reg[de[1][0]] == 1:
-            i += int(de[2])
-        else:
-            i += 1
-
-
-        
 print(reg["b"])
