49b05fd09f783bf60c57541e47151d28c9aeb145
Teodor Spæren <teodor@spaeren.no>
2021-10-27 09:39:10 +0200

    Improvement of 2015 Day 25
    Figured out how to calculate the index directly! Left a writeup of it in
    the file :)

--- 2015/25/y2015_d25_p01.py
+++ 2015/25/y2015_d25_p01.py
@@ -1,26 +1,38 @@
 import fileinput as fi
 
+# idx calculates the index of number in a given column and row. We know that
+# the first row of the sheet of paper, are all triangle numbers. Thus we have
+#
+# idx(c, 1) = (c * (c+1) / 2)
+#
+# For a given row r, we can then follow the pattern until we reach the next
+# number in row 1. Since we go one up and one to the right on each step, it
+# will take r-1 steps to get to 1.
+#
+# We know that this number is (r-1) bigger than the answer we want, so the
+# formula is then:
+#
+# idx(c, r) = idx(c + (r-1), 1) - (r-1)
+# idx(c, r) = ((c + r - 1) * (c + r)) // 2 - r + 1
+#
+# Which is our final answer.
+def idx(c, r):
+    return ((c+r-1) * (c+r))//2 - r + 1
+
+# We can use Modular exponentiation to calculate the answer here. We know
+# that for each step until the row, we will be multiplicating with B and
+# doing it modulo M. Since we do this idx(c, r) - 1 times, the first step
+# is simply S, we can factor the multiplications out, which we arrive at
+# the expression S * B**(idx(c, r) - 1) mod M
 def solve(col, row, S=20151125):
     M = 33554393
     B = 252533
 
-    max_y = 1
-    idx = 0
-    while True:
-        y = max_y
-        x = 1
-        max_y += 1
+    return (S * pow(B, idx(col, row) - 1, M)) % M
 
-        while y > 0:
-            if y == row and x == col:
-                return (S * pow(B, idx, M)) % M
 
-            idx += 1
-            y -=1
-            x += 1
-
-parts = next(fi.input()).split()
-row = int(parts[parts.index("row")+1][:-1])
-column = int(parts[parts.index("column")+1][:-1])
+words = next(fi.input()).split()
+row = int(words[words.index("row")+1][:-1])
+column = int(words[words.index("column")+1][:-1])
 
 print(solve(column, row))
