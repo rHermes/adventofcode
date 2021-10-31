6a78ed39ec83a75275ac8a9a21d08bc071d5021c
Teodor Spæren <teodor@spaeren.no>
2021-10-31 23:13:26 +0100

    Cleanup of 2016 Day 6
    This cleaned up rather nicely.

--- 2016/06/y2016_d06_p01.py
+++ 2016/06/y2016_d06_p01.py
@@ -1,41 +1,13 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
 import collections
-import math
-import sys
 
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-import z3
-import numpy as np
-import lark
-import regex
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-# Input parsing
 INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
 lines = list(INPUT.splitlines())
 
-cols = [[] for _ in lines[0]]
+cols = [collections.Counter() for _ in lines[0]]
 for line in lines:
-    for i, k in enumerate(line):
-        cols[i].append(k)
+    for cnt, k in zip(cols, line):
+        cnt[k] += 1
 
-for c in cols:
-    w = collections.Counter(c)
-    t = w.most_common(1)[0][0]
-    print(t, end="")
-
-print()
+ans = "".join(c.most_common(1)[0][0] for c in cols)
+print(ans)
