a00c2debaadc928e72e730700ed9061541fc91b3
Teodor Spæren <teodor@spaeren.no>
2021-10-31 22:58:20 +0100

    Cleanup of 2016 Day 5
    This needs to be optimized later, but other than that it turned out
    pretty well.

--- 2016/05/y2016_d05_p01.py
+++ 2016/05/y2016_d05_p01.py
@@ -1,36 +1,15 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
-
-
 import hashlib
 
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
 
 # Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-
-ins = "ugkcyxxp".encode('ascii')
+INPUT = "".join(fi.input()).strip().encode('ascii')
 
 ans = ""
-for i in range(100000000000000000000):
-    hsh = hashlib.md5(ins + str(i).encode("ascii")).hexdigest()
+for i in range(100000000000000):
+    hsh = hashlib.md5(INPUT + str(i).encode("ascii")).hexdigest()
 
     if hsh.startswith("00000"):
-        print(hsh[5])
         ans += hsh[5]
         if len(ans) == 8:
             break
--- 2016/05/y2016_d05_p02.py
+++ 2016/05/y2016_d05_p02.py
@@ -1,46 +1,22 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
-
-
 import hashlib
 
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
 
 # Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
+INPUT = "".join(fi.input()).strip().encode('ascii')
 
-ins = "ugkcyxxp".encode('ascii')
-# ins = "abc".encode('ascii')
-
-# for j in range(8):
-    # key1 = ins + str(j).encode('ascii')
 ans = ['' for _ in range(8)]
 added = 0
-for i in range(100000000000000000000):
-    hsh = hashlib.md5(ins + str(i).encode("ascii")).hexdigest()
+for i in range(100000000000000):
+    hsh = hashlib.md5(INPUT + str(i).encode("ascii")).hexdigest()
 
     if hsh.startswith("00000"):
         if not hsh[5].isnumeric() or int(hsh[5]) >= 8 or ans[int(hsh[5])] != "":
-            print("SKIPPING", hsh[5], hsh[6])
             continue
 
-        print(hsh[5], hsh[6])
         ans[int(hsh[5])] = hsh[6]
         added += 1
+
         if added == 8:
             break
 
