c7304461d86e6e9dd5bce5a6685948f8af175483
Teodor Spæren <teodor@spaeren.no>
2021-11-06 10:39:54 +0100

    Cleanup of 2016 Day 8
    This cleaned up rather nice. I haven't created the the OCR yet, but it's
    something to get at later.
    
    Another thing to look into is using numpy, that is also on the agenda.

--- 2016/08/y2016_d08_p02.py
+++ 2016/08/y2016_d08_p02.py
@@ -1,53 +1,25 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
 
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-import z3
-import numpy as np
-import lark
-import regex
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
+def print_screen(screen):
+    for row in screen:
+        for c in row:
+            if c:
+                print("█", end="")
+            else:
+                print(" ", end="")
+        print("")
 
 W = 50
 H = 6
 
 screen = [[False for _ in range(W)] for _ in range(H)]
 
-def print_screen(screen):
-    for row in screen:
-        for c in row:
-            if c:
-                print("#", end="")
-            else:
-                print(".", end="")
-        print("")
-
-for line in lines:
+for line in fi.input():
     if line.startswith("rect "):
         a, b = map(int,line[5:].split("x"))
-        for y in range(b):
-            for x in range(a):
-                screen[y][x] = True
+        for row in range(b):
+            for col in range(a):
+                screen[row][col] = True
 
     elif line.startswith("rotate column "):
         rest = line[14:]
@@ -55,37 +27,19 @@ for line in lines:
         col = int(first[2:])
         shift = int(sec)
 
-        newy = [False for _ in range(H)]
-        for y in range(H):
-            newy[(y+shift) % H] = screen[y][col]
-
-        for y in range(H):
-            screen[y][col] = newy[y]
+        oldy = [screen[row][col] for row in range(H)]
+        for row in range(H):
+            screen[(row+shift) % H][col] = oldy[row]
 
     elif line.startswith("rotate row "):
-        rest = line[len("rotate row "):]
+        rest = line[11:]
         first, sec = rest.split(" by ")
         row = int(first[2:])
         shift = int(sec)
 
-        newx = [False for _ in range(W)]
-        for x in range(W):
-            newx[(x + shift) % W] = screen[row][x]
-
-        for x in range(W):
-            screen[row][x] = newx[x]
-
-    else:
-        print("ERROR: {}".format(line))
+        oldx = [screen[row][col] for col in range(W)]
+        for col in range(W):
+            screen[row][(col + shift) % W] = oldx[col]
 
 
-    print(line)
-    print_screen(screen)
-
-print("final")
 print_screen(screen)
-ans = 0
-for row in screen:
-    ans += sum(row)
-
-print(ans)
