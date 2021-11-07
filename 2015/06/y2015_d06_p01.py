e3a7daecf81619d1e5f6abc2e3ee84bad5e859fe
Teodor Spæren <teodor@spaeren.no>
2021-11-08 00:40:28 +0100

    Cleanup of 2015 Day 6
    This reduced quite a bit. Not really any cool tricks here, just simple
    programming.

--- 2015/06/y2015_d06_p01.py
+++ 2015/06/y2015_d06_p01.py
@@ -1,43 +1,17 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
 
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-import z3
 import numpy as np
-import lark
-import regex
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
 
 a = np.full((1000,1000), False)
 
-for line in lines:
+for line in map(str.rstrip, fi.input()):
+    if line == "":
+        continue
+
     w = line.split(" ")
     p1, p2 = w[-3], w[-1]
     pa, pb = map(int, p1.split(","))
-    ja, jb = map(int, p2.split(","))
-    ja = ja+1
-    jb = jb+1
-
+    ja, jb = map(lambda x: int(x) + 1, p2.split(","))
 
     if line.startswith("toggle "):
         a[pb:jb, pa:ja] = ~a[pb:jb, pa:ja]
