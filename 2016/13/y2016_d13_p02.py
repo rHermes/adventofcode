c0d7fd3e45ba3305129b097454d83020d9526b0a
Teodor Spæren <teodor@spaeren.no>
2021-11-14 19:26:05 +0100

    Cleanup of 2016 Day 13
    This cleaned up quite well. Not as fast as it might be, but more than
    fast enough.

--- 2016/13/y2016_d13_p02.py
+++ 2016/13/y2016_d13_p02.py
@@ -1,46 +1,11 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
-
-# findall, search, parse
-# from parse import *
-# import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-
 import heapq
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-# # Input parsing
-# INPUT = "".join(fi.input()).rstrip()
-# groups = INPUT.split("\n\n")
-# lines = list(INPUT.splitlines())
 
 def is_open(x, y, c):
     w = x*x + 3*x + 2*x*y + y + y*y
     w += c
-    bits = sum(z == "1" for z in bin(w)[2:])
+    bits = bin(w).count('1')
     return bits % 2 == 0
-
-start = (1, 1)
-goal = (31, 39)
-
-c = 1362
-
 
 def pos_moves(pos):
     px, py = pos
@@ -52,39 +17,28 @@ def pos_moves(pos):
     if py > 0:
         pas.append((px, py-1))
 
-
     return pas
 
-
-
-
 def solve(c, start, stop):
-    sx, sy = start
     gx, gy = stop
 
     Q = [(0, start)]
 
     seen = set([start])
     while len(Q) > 0:
-        depth, (px, py) = heapq.heappop(Q)
+        depth, pos = heapq.heappop(Q)
 
         if depth == 50:
             continue
 
-        for nx, ny in pos_moves((px, py)):
+        for nx, ny in pos_moves(pos):
             if (nx, ny) not in seen and is_open(nx, ny, c):
                 heapq.heappush(Q, (depth+1, (nx, ny)))
                 seen.add((nx, ny))
 
-
-
-
-
-
-    
     return len(seen)
 
-# print(solve(10, (1,1), (7,4)))
+start = (1,1)
+goal = (31,39)
+c = int(next(fi.input()))
 print(solve(c, start, goal))
-# for line in lines:
-#     gprint(line)
