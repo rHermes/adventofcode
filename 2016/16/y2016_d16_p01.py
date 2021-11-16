e254f2918f5e871e555433c311cfd42e93e6d46c
Teodor Spæren <teodor@spaeren.no>
2021-11-16 21:29:04 +0100

    Cleanup of 2016 Day 16
    This was cleaned quite good, but still very slow. Will try to optimize
    it further.

--- 2016/16/y2016_d16_p01.py
+++ 2016/16/y2016_d16_p01.py
@@ -1,47 +1,6 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
-
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-import z3
-import numpy as np
-import lark
-import regex
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-
-# for line in lines:
-#     gprint(line)
-
-ins = lines[0]
-
-def step(s):
-    a = s
-    b = a
-    b = "".join(reversed(b))
-    b = b.replace('1', 'x').replace("0", "1").replace("x", "0")
-    return a + "0" + b
 
 def checksum(s):
-    a = s
     chk = ""
     for i in range(0, len(s), 2):
         x, y = s[i], s[i+1]
@@ -57,8 +16,8 @@ def checksum(s):
 
 def solve(s, n):
     while len(s) < n:
-        s = step(s)
+        s += "0" + "".join(['1', '0'][x == '1'] for x in reversed(s))
 
     return checksum(s[:n])
 
-print(solve(ins, 272))
+print(solve(next(fi.input()).rstrip(), 272))
