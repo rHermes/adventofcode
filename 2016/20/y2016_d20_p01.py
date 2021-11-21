8ee7eb23d9f2a039499967cce20de3783e8093e0
Teodor Spæren <teodor@spaeren.no>
2021-11-21 12:22:52 +0100

    Cleanup of 2016 Day 20
    This cleaned up well. I might go back and reimplement part 2 in my own
    code.

--- 2016/20/y2016_d20_p01.py
+++ 2016/20/y2016_d20_p01.py
@@ -1,45 +1,12 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
 
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-import z3
-import numpy as np
-import lark
-import regex
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-
-# state = set(range(0,10))
 ranges = []
-for line in lines:
+for line in fi.input():
     a, b = line.split("-")
-    a = int(a)
-    b = int(b)
-    ranges.append((a,b))
+    ranges.append((int(a),int(b)))
 
-ranges = sorted(ranges)
 ans = 0
-for l, h in ranges:
-    print(ans, l, h)
+for l, h in sorted(ranges):
     if ans < l:
         break
     else:
--- 2016/20/y2016_d20_p02.py
+++ 2016/20/y2016_d20_p02.py
@@ -1,15 +1,11 @@
 import fileinput as fi
 import intervaltree as itree
 
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-
-# state = set(range(0,10))
-tree = itree.IntervalTree([itree.Interval(0, 4294967295)])
-for line in lines:
+tree = itree.IntervalTree([itree.Interval(0, 4294967295+1)])
+for line in fi.input():
     a, b = line.split("-")
     tree.chop(int(a), int(b)+1)
 
+# We want to merge all intervals into one.
+tree.merge_overlaps(strict=False)
 print(sum(x.length() for x in tree))
