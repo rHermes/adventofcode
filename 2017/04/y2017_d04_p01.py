82bc274122aa24d64ef5fb432256e38b22dd1097
Teodor Spæren <teodor@spaeren.no>
2021-11-24 19:39:11 +0100

    Cleanup of 2017 Day 4

--- 2017/04/y2017_d04_p01.py
+++ 2017/04/y2017_d04_p01.py
@@ -1,37 +1,7 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
-import heapq
-
-# findall, search, parse
-# from parse import *
-# import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
 
 ans = 0
-for line in lines:
+for line in map(str.rstrip, fi.input()):
     seen = set()
     for word in line.split(" "):
         if word in seen:
