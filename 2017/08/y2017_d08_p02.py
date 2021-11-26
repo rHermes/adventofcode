a60c814ed3f049bd117bc7e40f33baee1627ce0a
Teodor Spæren <teodor@spaeren.no>
2021-11-26 21:17:03 +0100

    2017 Day 8
    This was really easy and eval helped. Could make a lot of assumptions
    about the input and this helped.
    
    Part one: 00:04:08
    Part two: 00:01:31

---
+++ 2017/08/y2017_d08_p02.py
@@ -0,0 +1,53 @@
+import fileinput as fi
+import re
+import itertools as it
+import functools as ft
+import string
+import collections
+import math
+import sys
+import heapq
+
+# findall, search, parse
+# from parse import *
+# import more_itertools as mit
+# import z3
+# import numpy as np
+# import lark
+# import regex
+# import intervaltree as itree
+
+# print(sys.getrecursionlimit())
+sys.setrecursionlimit(6500)
+
+# Debug logging
+DEBUG = True
+def gprint(*args, **kwargs):
+    if DEBUG: print(*args, **kwargs)
+
+# Input parsing
+INPUT = "".join(fi.input()).rstrip()
+groups = INPUT.split("\n\n")
+lines = list(INPUT.splitlines())
+
+regs = collections.defaultdict(int)
+
+max_now = 0
+
+for line in lines:
+    reg, op, delta, _, op1, rel, op2 = line.split(" ")
+    if op == "inc":
+        delta = int(delta)
+    elif op == "dec":
+        delta = -int(delta)
+    else:
+        raise Exception("WTF!")
+
+    if eval("{} {} {}".format(regs[op1], rel, op2)):
+        regs[reg] += delta
+        max_now = max(max(regs.values()), max_now)
+
+print(max_now)
+
+
+
