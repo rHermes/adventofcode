c837f1fd10dab2cebfdefeebe40a6c4051acfba8
Teodor Spæren <teodor@spaeren.no>
2021-11-27 12:08:59 +0100

    Cleanup of 2017 Day 10
    Haven't cleaned up the knot hash algorithm yet, want to leave that for
    later.

--- 2017/10/y2017_d10_p01.py
+++ 2017/10/y2017_d10_p01.py
@@ -1,38 +1,8 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
 import collections
-import math
-import sys
-import heapq
-
-# findall, search, parse
-# from parse import *
-# import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-# Input parsing
-# INPUT = "".join(fi.input()).rstrip()
-# groups = INPUT.split("\n\n")
-# lines = list(INPUT.splitlines())
 
 def knot_hash(ins):
     ar = list(range(256))
-    # ar = list(range(5))
     c = 0
     skip = 0
     cur = 0
@@ -49,9 +19,5 @@ def knot_hash(ins):
     bw.rotate(cur)
     return list(bw)
 
-# print(knot_hash([3, 4, 1, 5]))
-al = knot_hash([187,254,0,81,169,219,1,190,19,102,255,56,46,32,2,216])
-print(al)
+al = knot_hash([int(x) for x in next(fi.input()).rstrip().split(",")])
 print(al[0] * al[1])
-# for line in lines:
-#     gprint(line)
