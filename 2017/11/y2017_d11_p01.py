af94c20d035ace2e6a47abcc5fcb4456ca480087
Teodor Spæren <teodor@spaeren.no>
2021-11-27 12:45:26 +0100

    2017 Day 11
    This is familiar to a previous problem, where there was also a hex grid.
    I remembered from that you can express it as a complex number. I went to
    my main resource for this[1] and quickly got the formula. I did however,
    fuck it up with the distance calculation. I swapped an imaginary for a
    real part in one of the operations and that was it.
    
    Once that was sorted it went really quick. Disappointed with my time in
    this one, but a good reminder that the guy likes his hex grids.
    
    [1]: https://www.redblobgames.com/grids/hexagons/
    
    Part one: 00:14:35
    Part two: 00:01:32

---
+++ 2017/11/y2017_d11_p01.py
@@ -0,0 +1,63 @@
+import fileinput as fi
+import re
+import itertools as it
+import functools as ft
+import string
+import collections
+import math
+import sys
+import heapq
+
+# findall, search, parse
+# from parse import *
+# import more_itertools as mit
+# import z3
+# import numpy as np
+# import lark
+# import regex
+# import intervaltree as itree
+
+# print(sys.getrecursionlimit())
+sys.setrecursionlimit(6500)
+
+# Debug logging
+DEBUG = True
+def gprint(*args, **kwargs):
+    if DEBUG: print(*args, **kwargs)
+
+# Input parsing
+INPUT = "".join(fi.input()).rstrip()
+groups = INPUT.split("\n\n")
+lines = list(INPUT.splitlines())
+
+
+
+di = {
+    "n": 0 -1j,
+    "ne": 1 -1j,
+    "se": 1 + 0j,
+    "s": 0 + 1j,
+    "sw": -1 + 1j,
+    "nw": -1 + 0j,
+}
+def axial_subtract(a, b):
+    return a - b # Hex(a.q - b.q, a.r - b.r)
+
+def axial_distance(a, b):
+    vec = axial_subtract(a, b)
+    return (abs(vec.real)
+          + abs(vec.real + vec.imag)
+          + abs(vec.imag)) / 2
+
+def solve(s):
+    steps = s.split(",")
+    pt = 0+0j
+    for stp in steps:
+        pt += di[stp]
+
+    return int(axial_distance(pt, 0 + 0j))
+
+
+
+for line in lines:
+    print(solve(line))
