f12687491b77484ea26d32d6eac403b2abbf1b35
Teodor Spæren <teodor@spaeren.no>
2021-11-28 10:56:30 +0100

    Cleanup of 2017 Day 14

--- 2017/14/y2017_d14_p01.py
+++ 2017/14/y2017_d14_p01.py
@@ -1,41 +1,12 @@
 import fileinput as fi
-import re
 import itertools as it
 import functools as ft
-import string
 import collections
-import math
-import sys
-import heapq
 import operator
 
-# findall, search, parse
-# from parse import *
 import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-# # Input parsing
-# INPUT = "".join(fi.input()).rstrip()
-# groups = INPUT.split("\n\n")
-# lines = list(INPUT.splitlines())
 
 def knot_hash(ins, ar, cur, skip):
-    bw = collections.deque(ar)
-    bw.rotate(-cur)
-    ar = list(bw)
-
     for l in ins:
         ar[:l] = ar[:l][::-1]
         bw = collections.deque(ar)
@@ -44,9 +15,7 @@ def knot_hash(ins, ar, cur, skip):
         ar = list(bw)
         skip += 1
 
-    bw = collections.deque(ar)
-    bw.rotate(cur)
-    return list(bw), cur, skip
+    return ar, cur, skip
 
 def kn(inp):
     cur = 0
@@ -56,6 +25,10 @@ def kn(inp):
     ar = list(range(256))
     for i in range(64):
         ar, cur, skip = knot_hash(anp, ar, cur, skip)
+
+    bw = collections.deque(ar)
+    bw.rotate(cur)
+    ar = list(bw)
 
     dense_hash = (ft.reduce(operator.xor, g) for g in mit.chunked(ar, 16, strict=True))
     return "".join(map("{:08b}".format, dense_hash))
@@ -69,6 +42,4 @@ def solve(inp):
     return ans
 
 
-inp = "oundnydw" # real
-# inp = "flqrgnkx" # test
-print(solve(inp))
+print(solve(next(fi.input()).rstrip()))
--- 2017/14/y2017_d14_p02.py
+++ 2017/14/y2017_d14_p02.py
@@ -1,41 +1,12 @@
 import fileinput as fi
-import re
 import itertools as it
 import functools as ft
-import string
 import collections
-import math
-import sys
-import heapq
 import operator
 
-# findall, search, parse
-# from parse import *
 import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-# # Input parsing
-# INPUT = "".join(fi.input()).rstrip()
-# groups = INPUT.split("\n\n")
-# lines = list(INPUT.splitlines())
 
 def knot_hash(ins, ar, cur, skip):
-    bw = collections.deque(ar)
-    bw.rotate(-cur)
-    ar = list(bw)
-
     for l in ins:
         ar[:l] = ar[:l][::-1]
         bw = collections.deque(ar)
@@ -44,9 +15,7 @@ def knot_hash(ins, ar, cur, skip):
         ar = list(bw)
         skip += 1
 
-    bw = collections.deque(ar)
-    bw.rotate(cur)
-    return list(bw), cur, skip
+    return ar, cur, skip
 
 def kn(inp):
     cur = 0
@@ -57,11 +26,15 @@ def kn(inp):
     for i in range(64):
         ar, cur, skip = knot_hash(anp, ar, cur, skip)
 
+    bw = collections.deque(ar)
+    bw.rotate(cur)
+    ar = list(bw)
+
     dense_hash = (ft.reduce(operator.xor, g) for g in mit.chunked(ar, 16, strict=True))
     return "".join(map("{:08b}".format, dense_hash))
 
 
-def solve_p2(G, src):
+def find_group(G, src):
     Q = collections.deque([src])
     seen = set()
 
@@ -76,17 +49,6 @@ def solve_p2(G, src):
             Q.append(x)
 
     return seen
-
-def solva(G):
-    not_seen = set(G.keys())
-
-    groups = 0
-    while len(not_seen) > 0:
-        src = list(not_seen)[0]
-        not_seen -= solve_p2(G, src)
-        groups += 1
-
-    return groups
 
 def solve(inp):
     grid = [kn("{}-{}".format(inp, x)) for x in range(128)]
@@ -107,7 +69,7 @@ def solve(inp):
                 deltas.append((0, -1))
             if y < (len(grid)-1):
                 deltas.append((0, 1))
-        
+
             neigh = []
             for dx, dy in deltas:
                 tx, ty = x + dx, y + dy
@@ -115,11 +77,17 @@ def solve(inp):
                     neigh.append((tx,ty))
 
             G[(x,y)] = neigh
-                    
-
-    return solva(G) 
 
 
-inp = "oundnydw" # real
-# inp = "flqrgnkx" # test
-print(solve(inp))
+    not_seen = set(G.keys())
+
+    groups = 0
+    while len(not_seen) > 0:
+        src = list(not_seen)[0]
+        not_seen -= find_group(G, src)
+        groups += 1
+
+    return groups
+
+
+print(solve(next(fi.input()).rstrip()))
