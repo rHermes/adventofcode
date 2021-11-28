3e4c613b22bf93003db417ceb7b2358e70717f5b
Teodor Spæren <teodor@spaeren.no>
2021-11-28 09:49:51 +0100

    Cleanup of 2017 Day 13
    There is still room to improve the time here, by trying to narrow down
    the window.

--- 2017/13/y2017_d13_p01.py
+++ 2017/13/y2017_d13_p01.py
@@ -3,58 +3,20 @@ import re
 import itertools as it
 import functools as ft
 import string
-import collections
-import math
-import sys
-import heapq
-
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-import z3
-import numpy as np
-import lark
-import regex
-import intervaltree as itree
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-
-scanners = {}
-for line in lines:
-    a, b = map(int, line.split(": "))
-    scanners[a] = b
 
 def solve(S):
     ans = 0
-    for d in sorted(S.keys()):
+    for d in S.keys():
         r = S[d]
-        t = d
-        p = 0
-        on_up = True
-        for _ in range(t):
-            if on_up:
-                p += 1
-                if p == r-1:
-                    on_up = False
-            else:
-                p -= 1
-                if p == 0:
-                    on_up = True
-
+        p = d % ((r-1)*2)
         if p == 0:
             ans += r * d
-        # print("at sec {} we are at {} and the scanner is at {}".format(t, d, p))
+
     return ans
+
+scanners = {}
+for line in map(str.rstrip, fi.input()):
+    a, b = map(int, line.split(": "))
+    scanners[a] = b
 
 print(solve(scanners))
