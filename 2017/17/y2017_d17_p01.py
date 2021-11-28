37832c0b58295711d4571ad3541ceb76412d487e
Teodor Spæren <teodor@spaeren.no>
2021-11-28 13:33:50 +0100

    Cleanup of 2017 Day 17
    Reading the reddit thread it became clear that for part two, we don't
    actually have to keep a list at all. We can simply just keep track of if
    anything is inserted at the spot where 0 is, which is at the beginning
    of the list.

--- 2017/17/y2017_d17_p01.py
+++ 2017/17/y2017_d17_p01.py
@@ -1,45 +1,13 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
 import collections
-import math
-import sys
-import heapq
-
-# # findall, search, parse
-# from parse import *
-# import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-# # Input parsing
-# INPUT = "".join(fi.input()).rstrip()
-# groups = INPUT.split("\n\n")
-# lines = list(INPUT.splitlines())
 
 def solve(step):
     Q = collections.deque([0])
 
     for i in range(1, 2018):
-    # for i in range(1, 10):
         Q.rotate(-(step+1))
         Q.appendleft(i)
-        # print(i, Q)
 
     return Q[1]
 
-# print(solve(3)) # test
-print(solve(335)) # real
+print(solve(int(next(fi.input()).rstrip())))
