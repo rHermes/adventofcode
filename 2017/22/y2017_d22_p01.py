14fdb1880c8f390db71f18e988ce1f616a170847
Teodor Spæren <teodor@spaeren.no>
2021-11-28 23:39:38 +0100

    2017 Day 22
    Funny one! It's a variation of langtons ants. I was a bit foolish, I had
    a bug for a while, which was that I was using the normal Y increases
    downward when initializing the board, but then used Y increases upwards
    when doing the directions. Once I rectified this, I go the correct
    answer.
    
    I liked my transition to part two, I was able to get the answer quite
    quickly there.
    
    Part one: 00:21:53
    Part two: 00:05:43

---
+++ 2017/22/y2017_d22_p01.py
@@ -0,0 +1,96 @@
+import fileinput as fi
+import re
+import itertools as it
+import functools as ft
+import string
+import collections
+import math
+import sys
+import heapq
+
+# findall, search, parse
+# from parse import *
+import more_itertools as mit
+# import z3
+# import numpy as np
+# import lark
+# import regex
+# import intervaltree as itree
+
+# print(sys.getrecursionlimit())
+sys.setrecursionlimit(6500)
+
+# Debug logging
+DEBUG = True
+def gprint(*args, **kwargs):
+    if DEBUG: print(*args, **kwargs)
+
+# Input parsing
+INPUT = "".join(fi.input()).rstrip()
+groups = INPUT.split("\n\n")
+lines = list(INPUT.splitlines())
+
+dirs = "NESW"
+
+dir_delta = {
+    "N": (0, -1),
+    "E": (1, 0),
+    "S": (0, +1),
+    "W": (-1, 0)
+}
+
+def step(M, c, d):
+    pass
+
+def print_map(M):
+    min_x = min(x for (x, y), c in M.items() ) #if c)
+    max_x = max(x for (x, y), c in M.items() ) #if c)
+    min_y = min(y for (x, y), c in M.items() ) #if c)
+    max_y = max(y for (x, y), c in M.items() ) #if c)
+
+    for y in range(min_y, max_y+1):
+        for x in range(min_x, max_x+1):
+            print("{}".format(".#"[M[(x,y)]]), end="")
+        print("")
+
+
+def solve(M, c, d):
+    x, y = c
+    ans = 0
+    for i in range(10000):
+        # print(i)
+        # print_map(M)
+        # print()
+        c = M[(x, y)]
+        if c:
+            M[(x, y)] = False
+            # print("Turning right from {} to ".format(d), end="")
+            d = dirs[(dirs.index(d)+1)%len(dirs)]
+            # print("{}.".format(d))
+        else:
+            M[(x, y)] = True
+            ans += 1
+            # print("Turning left from {} to ".format(d), end="")
+            d = dirs[(dirs.index(d)-1)%len(dirs)]
+            # print("{}.".format(d))
+
+        # print(d)
+        dx, dy = dir_delta[d]
+        x, y = x + dx, y + dy
+        # print("We are now at ({}, {})".format(x,y))
+
+    # print_map(M)
+
+    return ans
+
+
+        
+M = collections.defaultdict(bool)
+for (y, line) in enumerate(lines):
+    for (x, c) in enumerate(line):
+        if c == '#':
+            M[(x,y)] = True
+
+x, y = (x+1)//2, (y+1)//2
+d = "N"
+print(solve(M, (x,y), d))
