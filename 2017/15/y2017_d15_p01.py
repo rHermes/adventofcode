1df119bc287542a7f61f1f8b543b754b7b2cda27
Teodor Spæren <teodor@spaeren.no>
2021-11-28 11:29:56 +0100

    Cleanup of 2017 Day 15
    This cleaned up quite nice, but I still need pypy to run it fast enough.

--- 2017/15/y2017_d15_p01.py
+++ 2017/15/y2017_d15_p01.py
@@ -1,63 +1,23 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
-import heapq
-
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-import z3
-import numpy as np
-import lark
-import regex
-import intervaltree as itree
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-# Input parsing
-# INPUT = "".join(fi.input()).rstrip()
-# groups = INPUT.split("\n\n")
-# lines = list(INPUT.splitlines())
 
 GEN_A_FAC = 16807
 GEN_B_FAC = 48271
 
 MOD = 2147483647
 
-def step(cur, fac):
-    return (cur*fac) % MOD
-
-def comp(a, b):
-    mask = 0b1111_1111_1111_1111
-    return (a & mask) == (b & mask)
-
-
-import tqdm
 def solve(a, b):
     cnt = 0
-    for i in tqdm.trange(40_000_000):
-        a = step(a, GEN_A_FAC)
-        b = step(b, GEN_B_FAC)
-        if comp(a, b):
-            cnt += 1
+    mask = 0b1111_1111_1111_1111
+    for _ in range(40_000_000):
+        a = (a * GEN_A_FAC) % MOD
+        b = (b * GEN_B_FAC) % MOD
+
+        cnt += (a & mask) == (b & mask)
 
     return cnt
 
+g = fi.input()
+a = int(next(g).split(" ")[-1])
+b = int(next(g).split(" ")[-1])
 
-
-
-
-
-# print(solve(65, 8921)) # test
-print(solve(699, 124)) # real
+print(solve(a, b)) # real
--- 2017/15/y2017_d15_p02.py
+++ 2017/15/y2017_d15_p02.py
@@ -1,75 +1,26 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
-import heapq
-
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-import z3
-import numpy as np
-import lark
-import regex
-import intervaltree as itree
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-# Input parsing
-# INPUT = "".join(fi.input()).rstrip()
-# groups = INPUT.split("\n\n")
-# lines = list(INPUT.splitlines())
 
 GEN_A_FAC = 16807
 GEN_B_FAC = 48271
 
 MOD = 2147483647
 
-def step(cur, fac):
-    return (cur*fac) % MOD
-
-def comp(a, b):
-    mask = 0b1111_1111_1111_1111
-    return (a & mask) == (b & mask)
-
-def gen_a(cur):
+def g(cur, fac, md):
     while True:
-        cur = (cur*GEN_A_FAC) % MOD
-        if cur % 4 == 0:
+        cur = (cur*fac) % MOD
+        if cur % md == 0:
             yield cur
 
-def gen_b(cur):
-    while True:
-        cur = (cur*GEN_B_FAC) % MOD
-        if cur % 8 == 0:
-            yield cur
-
-
-import tqdm
 def solve(a, b):
-    ga = gen_a(a)
-    gb = gen_b(b)
     cnt = 0
-    for i, ac, bc in zip(tqdm.trange(5_000_000), ga, gb):
-        if comp(ac, bc):
-            cnt += 1
+    mask = 0b1111_1111_1111_1111
+    for a, b, _ in zip(g(a, GEN_A_FAC, 4), g(b, GEN_B_FAC, 8), range(5_000_000)):
+        cnt += (a & mask) == (b & mask)
 
     return cnt
 
+inp = fi.input()
+a = int(next(inp).split(" ")[-1])
+b = int(next(inp).split(" ")[-1])
 
-
-
-
-
-# print(solve(65, 8921)) # test
-print(solve(699, 124)) # real
+print(solve(a, b))
