3017b55f265d7c793e98b9ac4e8fef0a2e7956c3
Teodor Spæren <teodor@spaeren.no>
2021-12-03 09:31:16 +0100

    Cleanup of 2021 Day 3

--- 2021/03/y2021_d03_p01.py
+++ 2021/03/y2021_d03_p01.py
@@ -1,53 +1,12 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
-import heapq
 
-# findall, search, parse
-# from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
+x = None
+for line in map(str.rstrip, fi.input()):
+    if x is None:
+        x = [[0,0] for _ in range(len(line))]
 
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("[0-9]+", line))) for line in lines]
-
-x = [[0,0] for _ in range(len(lines[0]))]
-for line in lines:
-    print(line)
     for i, c in enumerate(line):
         x[i][int(c)] += 1
 
-gamma = ""
-epsilon = ""
-for a, b in x:
-    if a < b:
-        gamma += "1"
-        epsilon += "0"
-    else:
-        gamma += "0"
-        epsilon += "1"
-
-print(gamma, int(gamma, 2))
-print(epsilon, int(epsilon, 2))
-
-print(int(gamma, 2) * int(epsilon, 2))
+gamma = "".join("01"[a < b] for a,b in x)
+print(int(gamma, 2) * int(gamma.translate(str.maketrans("01", "10")), 2))
