9ce45e56bfc6c256f5476a441e5b50e2015d41df
Teodor Spæren <teodor@spaeren.no>
2021-12-05 08:07:18 +0100

    2018 Day 8
    An ok day, I felt slow on part one and ok fast on part two. Lost some
    time on part two, due to not realizing that the meta data was 1 indexed
    instead of 0 indexed.
    
    I was mentally preparing for having to cache the node values in classic
    dynamic programming style, but in hindsight this is obviously not
    needed, as we ever only visit each node once.
    
    Will clean this up in a second.
    
    Part one: 00:06:47
    Part two: 00:02:55

---
+++ 2018/08/y2018_d08_p01.py
@@ -0,0 +1,55 @@
+import fileinput as fi
+import re
+import itertools as it
+import functools as ft
+import string
+import collections
+import math
+import sys
+import heapq
+
+# findall, search, parse
+# from parse import *
+import more_itertools as mit
+# import z3
+# import numpy as np
+# import lark
+# import regex
+# import intervaltree as itree
+
+# print(sys.getrecursionlimit())
+sys.setrecursionlimit(6500)
+
+# Debug logging
+DEBUG = True
+def gprint(*args, **kwargs):
+    if DEBUG: print(*args, **kwargs)
+
+# Input parsing
+INPUT = "".join(fi.input()).rstrip()
+groups = INPUT.split("\n\n")
+lines = list(INPUT.splitlines())
+numbers = [list(map(int, re.findall("[0-9]+", line))) for line in lines]
+
+def parse(node):
+    nchild = node[0]
+    nmeta = node[1]
+    childs = []
+
+    cc = 2
+    for i in range(nchild):
+        c = parse(node[cc:])
+        childs.append(c)
+        cc += c["len"]
+
+
+    meta = []
+    for i in range(nmeta):
+        print(node[cc])
+        meta.append(node[cc])
+        cc += 1
+
+    return {"len": cc, "children": childs, "metas": meta}
+
+
+root = parse(numbers[0])
