73d35a5076f130a00ceb295f1c615f3f0b15fd63
Teodor Spæren <teodor@spaeren.no>
2021-12-05 08:18:14 +0100

    Cleanup of 2018 Day 8
    Cleaned up quite nice and made both solutions finish in one pass of the
    numbers.

--- 2018/08/y2018_d08_p02.py
+++ 2018/08/y2018_d08_p02.py
@@ -1,68 +1,26 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
-import heapq
-
-# findall, search, parse
-# from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("[0-9]+", line))) for line in lines]
 
 def parse(node):
     nchild = node[0]
     nmeta = node[1]
-    childs = []
 
+    ch = []
     cc = 2
     for i in range(nchild):
-        c = parse(node[cc:])
-        childs.append(c)
-        cc += c["len"]
+        c, m = parse(node[cc:])
+        ch.append(m)
+        cc += c
+
+    if nchild == 0:
+        mt = sum(node[cc:cc+nmeta])
+    else:
+        mt = 0
+        for m in node[cc:cc+nmeta]:
+            if 0 <= m-1 < len(ch):
+                mt += ch[m-1]
+
+    return cc + nmeta, mt
 
 
-    meta = []
-    for i in range(nmeta):
-        meta.append(node[cc])
-        cc += 1
-
-    return {"len": cc, "children": childs, "metas": meta}
-
-def val(node):
-    ch = node["children"]
-    mt = node["metas"]
-    if len(ch) == 0:
-        return sum(mt)
-
-    ans = 0
-    for m in mt:
-        if 0 <= m-1 < len(ch):
-            ans += val(ch[m-1])
-
-    return ans
-
-
-root = parse(numbers[0])
-print(val(root))
+_, ans = parse([int(x) for x in next(fi.input()).split()])
+print(ans)
