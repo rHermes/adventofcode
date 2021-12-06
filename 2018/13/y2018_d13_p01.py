bf3ddec74bb51315e0203a4a994f655b3c5c29cf
Teodor Spæren <teodor@spaeren.no>
2021-12-06 07:57:53 +0100

    Cleanup of 2018 Day 13
    This cleaned up rather nicely! It helped to pull the turns and such into
    dictionaries.

--- 2018/13/y2018_d13_p01.py
+++ 2018/13/y2018_d13_p01.py
@@ -1,106 +1,54 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
-import heapq
 
-# findall, search, parse
-# from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
+# The dy, dx pairs for each direction.
+DIR = {"^": (-1,0), "v": (1,0), ">": (0,1), "<": (0,-1)}
 
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
+# Corner handling
+NDIR = {
+    '\\': { "^": "<", ">": "v", "v": ">", "<": "^"},
+    '/': { "^": ">", ">": "^", "v": "<", "<": "v" },
+    "|": {"^": "^", "v": "v"},
+    "-": {"<": "<", ">": ">"},
+}
 
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
+TURN = {
+    "^": "<>",
+    ">": "^v",
+    "v": "><",
+    "<": "v^",
+}
 
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
+lines = list(fi.input())
 
+# Read the carts from
 carts = []
+seen = set()
 for y, line in enumerate(lines):
     for x, c in enumerate(line):
         if c in "^v<>":
             carts.append((y,x,0,c))
+            seen.add((y,x))
 
-
-seen = set()
+# Remove the carts from the board
 for y,x, _, c in carts:
     wl = list(lines[y])
-    wl[x] = {"<": "-", ">": "-", "v": "|", "^": "|"}[c]
+    wl[x] = "-|"[c in "^v"]
     lines[y] = "".join(wl)
-    seen.add((y,x))
 
 
-def print_track(lines, carts):
-    nl = [x for x in lines]
-    for y,x, _, c in carts:
-        wl = list(nl[y])
-        wl[x] = c
-        nl[y] = "".join(wl)
-
-    print("\n".join(nl))
-
-DIR = {"^": (-1,0), "v": (1,0), ">": (0,1), "<": (0,-1)}
-ts = 0
 done = False
 while not done:
-    # print(ts)
-    # print_track(lines, carts)
-    # print(ts, carts)
     carts = sorted(carts)
     for i in range(len(carts)):
         y, x, gen, c = carts[i]
 
-        if c == 'v' and lines[y][x] == '\\':
-            c = ">"
-        elif c == '<' and lines[y][x] == '\\':
-            c = "^"
-        elif c == '>' and lines[y][x] == '\\':
-            c = "v"
-        elif c == '^' and lines[y][x] == '\\':
-            c = "<"
-        elif c == ">" and lines[y][x] == '/':
-            c = "^"
-        elif c == "v" and lines[y][x] == '/':
-            c = '<'
-        elif c == "<" and lines[y][x] == '/':
-            c = 'v'
-        elif c == "^" and lines[y][x] == '/':
-            c = '>'
-        elif lines[y][x] == "+":
+        if lines[y][x] in NDIR:
+            c = NDIR[lines[y][x]][c]
+        else:
             if gen == 0:
-                if c == "^":
-                    c = "<"
-                elif c == ">":
-                    c = "^"
-                elif c == "v":
-                    c = ">"
-                else:
-                    c = "v"
+                c = TURN[c][0]
             elif gen == 2:
-                if c == "^":
-                    c = ">"
-                elif c == ">":
-                    c = "v"
-                elif c == "v":
-                    c = "<"
-                else:
-                    c = "^"
+                c = TURN[c][1]
 
             gen = (gen+1) % 3
 
@@ -110,14 +58,10 @@ while not done:
         x += dx
 
         if (y,x) in seen:
-            print("We have a crash at {} at: {},{}".format(ts, x,y))
+            print("{},{}".format(x,y))
             done = True
             break
         else:
             seen.add((y,x))
 
         carts[i] = (y,x,gen,c)
-
-
-
-    ts += 1
--- 2018/13/y2018_d13_p02.py
+++ 2018/13/y2018_d13_p02.py
@@ -1,135 +1,77 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
-import heapq
 
-# findall, search, parse
-# from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
+# The dy, dx pairs for each direction.
+DIR = {"^": (-1,0), "v": (1,0), ">": (0,1), "<": (0,-1)}
 
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
+# Corner handling
+NDIR = {
+    '\\': { "^": "<", ">": "v", "v": ">", "<": "^"},
+    '/': { "^": ">", ">": "^", "v": "<", "<": "v" },
+    "|": {"^": "^", "v": "v"},
+    "-": {"<": "<", ">": ">"},
+}
 
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
+TURN = {
+    "^": "<>",
+    ">": "^v",
+    "v": "><",
+    "<": "v^",
+}
 
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
+lines = list(fi.input())
 
+# Read the carts from
 carts = []
 for y, line in enumerate(lines):
     for x, c in enumerate(line):
         if c in "^v<>":
             carts.append((y,x,0,c))
 
-print(carts)
-
+# Remove the carts from the board
 for y,x, _, c in carts:
     wl = list(lines[y])
-    wl[x] = {"<": "-", ">": "-", "v": "|", "^": "|"}[c]
+    wl[x] = "-|"[c in "^v"]
     lines[y] = "".join(wl)
 
 
-def print_track(lines, carts):
-    nl = [x for x in lines]
-    for y,x, _, c in carts:
-        wl = list(nl[y])
-        wl[x] = c
-        nl[y] = "".join(wl)
-
-    print("\n".join(nl))
-
-DIR = {"^": (-1,0), "v": (1,0), ">": (0,1), "<": (0,-1)}
-ts = 0
-# print("\n".join(lines))
-
-done = False
 while len(carts) > 1:
-    seen = set()
-    for y, x, _, _ in carts:
-        seen.add((y,x))
-
-    # print(ts)
-    # print_track(lines, carts)
-    # print(ts, carts)
-    to_remove = set()
     carts = sorted(carts)
+
+    seen = {}
+    for j, (y, x, _, _) in enumerate(carts):
+        seen[(y,x)] = j
+
+    to_remove = set()
     for i in range(len(carts)):
         y, x, gen, c = carts[i]
 
-        if c == 'v' and lines[y][x] == '\\':
-            c = ">"
-        elif c == '<' and lines[y][x] == '\\':
-            c = "^"
-        elif c == '>' and lines[y][x] == '\\':
-            c = "v"
-        elif c == '^' and lines[y][x] == '\\':
-            c = "<"
-        elif c == ">" and lines[y][x] == '/':
-            c = "^"
-        elif c == "v" and lines[y][x] == '/':
-            c = '<'
-        elif c == "<" and lines[y][x] == '/':
-            c = 'v'
-        elif c == "^" and lines[y][x] == '/':
-            c = '>'
-        elif lines[y][x] == "+":
+        if lines[y][x] in NDIR:
+            c = NDIR[lines[y][x]][c]
+        else:
             if gen == 0:
-                if c == "^":
-                    c = "<"
-                elif c == ">":
-                    c = "^"
-                elif c == "v":
-                    c = ">"
-                else:
-                    c = "v"
+                c = TURN[c][0]
             elif gen == 2:
-                if c == "^":
-                    c = ">"
-                elif c == ">":
-                    c = "v"
-                elif c == "v":
-                    c = "<"
-                else:
-                    c = "^"
+                c = TURN[c][1]
 
             gen = (gen+1) % 3
 
-        seen.remove((y,x))
+        del seen[(y,x)]
+
         dy, dx = DIR[c]
         y += dy
         x += dx
 
         if (y,x) in seen:
-            print("We have a crash at {} at: {},{}".format(ts, x,y))
             to_remove.add(i)
-            for j, (py,px, _, _) in enumerate(carts):
-                if py == y and px == x:
-                    to_remove.add(j)
+            to_remove.add(seen[(y,x)])
         else:
-            seen.add((y,x))
+            seen[(y,x)] = i
+
 
         carts[i] = (y,x,gen,c)
 
     for j in reversed(sorted(to_remove)):
-        print("We deleted {} at {}".format(j, ts))
         del carts[j]
 
-    ts += 1
 
 print("{},{}".format(carts[0][1],carts[0][0]))
