60e1a78ff73866e4e177ca4513b30afec735eadc
Teodor Spæren <teodor@spaeren.no>
2021-12-06 06:51:05 +0100

    Cleanup of 2021 Day 6
    This did clean up rather well. Now let's check the reddit thread to see
    if there are some cool tricks here!

--- 2021/06/y2021_d06_p01.py
+++ 2021/06/y2021_d06_p01.py
@@ -1,52 +1,17 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
-import heapq
 
-# findall, search, parse
-# from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
+def solve(s, n=80):
+    xs = [0 for _ in range(9)]
+    for x in s:
+        xs[x] += 1
 
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
+    for i in range(n):
+        y = xs[0]
+        xs[:8] = xs[1:]
+        xs[8] = y
+        xs[6] += y
 
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
+    return sum(xs)
 
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-
-
-def solve(s):
-    for i in range(80):
-        new_fish = []
-        for x in s:
-            x -= 1
-            if x == -1:
-                new_fish.append(6)
-                new_fish.append(8)
-            else:
-                new_fish.append(x)
-
-        s = new_fish
-
-    return len(s)
-
-nums = numbers[0]
+nums = map(int, next(fi.input()).split(","))
 print(solve(nums))
-
--- 2021/06/y2021_d06_p02.py
+++ 2021/06/y2021_d06_p02.py
@@ -1,66 +1,17 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
-import heapq
 
-# findall, search, parse
-# from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
+def solve(s, n=256):
+    xs = [0 for _ in range(9)]
+    for x in s:
+        xs[x] += 1
 
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
+    for i in range(n):
+        y = xs[0]
+        xs[:8] = xs[1:]
+        xs[8] = y
+        xs[6] += y
 
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
+    return sum(xs)
 
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-
-
-def solve(s):
-    l = collections.defaultdict(int)
-    for k in s:
-        l[k] += 1
-
-    for i in range(256):
-        print(i, len(s))
-        ll = collections.defaultdict(int)
-        for k, v in l.items():
-            if k == 0:
-                ll[6] += v
-                ll[8] += v
-            else:
-                ll[k-1] += v
-
-        
-        # new_fish = []
-        # for x in s:
-        #     x -= 1
-        #     if x == -1:
-        #         new_fish.append(6)
-        #         new_fish.append(8)
-        #     else:
-        #         new_fish.append(x)
-
-        l = ll
-
-    return sum(l.values())
-
-nums = numbers[0]
+nums = map(int, next(fi.input()).split(","))
 print(solve(nums))
-
