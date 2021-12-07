ebd93228186e22a9379bee800e3acd0b0021d0c7
Teodor Spæren <teodor@spaeren.no>
2021-12-07 06:12:07 +0100

    2021 Day 7
    So this was a disgrace, I am shamed by my performance here. This day was
    easy to code, but I spent a lot of time in the begining thinking that we
    where trying to sort the crabs, instead of just moving them to one
    position. This is a MUCH easier problem and one I solved almost
    instantly.
    
    THEN I MADE ANOTHER BIG MISTAKE. I thought they wanted the position with
    least sum, but rather, they wanted the sum. Had to wait a whole minute
    for this *facepalm*.
    
    Part two was easy, I just brute forced it at first, but looking at it
    now, it is clear that you can use the triangle numbers. It would be
    interesting to see if there is some way to get the best position or
    close to it, with some sort of formula.
    
    Score:
          --------Part 1--------   -------Part 2--------
    Day       Time   Rank  Score       Time  Rank  Score
      7   00:08:07   3423      0   00:09:12  1480      0

---
+++ 2021/07/y2021_d07_p01.py
@@ -0,0 +1,51 @@
+import fileinput as fi
+import re
+import itertools as it
+import functools as ft
+import string
+import collections
+import math
+import sys
+import heapq
+
+# findall, search, parse
+# from parse import *
+import more_itertools as mit
+# import z3
+# import numpy as np
+# import lark
+# import regex
+# import intervaltree as itree
+
+# print(sys.getrecursionlimit())
+sys.setrecursionlimit(6500)
+
+# Debug logging
+DEBUG = True
+def gprint(*args, **kwargs):
+    if DEBUG: print(*args, **kwargs)
+
+# Input parsing
+INPUT = "".join(fi.input()).rstrip()
+groups = INPUT.split("\n\n")
+lines = list(INPUT.splitlines())
+numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
+
+
+def subsolve():
+    pass
+
+
+def solve(nums):
+    ans = 1000000000000000
+    best_i = 0
+    for i in range(min(nums), max(nums)+1):
+        w = sum(abs(i - x) for x in nums)
+        if w < ans:
+            ans = w
+            best_i = i
+
+    return (ans, best_i)
+
+
+print(solve(list(map(int,lines[0].split(",")))))
