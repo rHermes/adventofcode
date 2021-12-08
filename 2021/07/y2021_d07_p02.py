345dbb7ca43783e14c2e8f0981e6f8f71223f044
Teodor Spæren <teodor@spaeren.no>
2021-12-08 05:55:14 +0100

    Cleanup of 2021 Day 7
    This cleaned up a lot, due to the fact that you can actually find the
    best position with just a median and mean! I found this out in the
    reddit thread, my first idea was to use a binary search.

--- 2021/07/y2021_d07_p02.py
+++ 2021/07/y2021_d07_p02.py
@@ -1,52 +1,9 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
-import heapq
+import statistics as st
 
-# findall, search, parse
-# from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-
-
-def subsolve():
-    pass
-
-
+# https://en.wikipedia.org/wiki/Arithmetic_mean#Motivating_properties
 def solve(nums):
-    ans = 1000000000000000
-    best_i = 0
-    for i in range(min(nums), max(nums)+1):
-        print(i)
-        w = sum(sum(range(1,abs(i - x)+1)) for x in nums)
-        if w < ans:
-            ans = w
-            best_i = i
+    x = int(st.fmean(nums))
+    return sum((abs(x - n)*(abs(x-n) + 1)) // 2 for n in nums)
 
-    return (ans, best_i)
-
-
-print(solve(list(map(int,lines[0].split(",")))))
+print(solve([int(x) for x in next(fi.input()).split(",")]))
