23039ac5d3b7060faf8d4a915eefde671818f36d
Teodor Spæren <teodor@spaeren.no>
2021-12-09 07:02:53 +0100

    Add ortho and adj functions to template

--- misc/template.py
+++ misc/template.py
@@ -25,6 +25,29 @@ DEBUG = True
 def gprint(*args, **kwargs):
     if DEBUG: print(*args, **kwargs)
 
+def ortho(y, x, shape):
+    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
+    sy, sx = shape
+    if 0 < x: yield (y, x-1)
+    if x < sx-1: yield (y, x+1)
+    if 0 < y: yield (y-1, x)
+    if y < sy-1: yield (y+1, x)
+
+# Returns a
+def adj(y, x, shape):
+    """Returns all points around a point, given the shape of the array"""
+    sy, sx = shape
+    for dy,dx in it.product([-1,0,1], [-1,0,1]):
+        if dy == 0 and dx == 0:
+            continue
+
+        py = y + dy
+        px = x + dx
+
+        if 0 <= px < sx and 0 <= py < sy:
+            yield (py,px)
+
+
 # Input parsing
 INPUT = "".join(fi.input()).rstrip()
 groups = INPUT.split("\n\n")
