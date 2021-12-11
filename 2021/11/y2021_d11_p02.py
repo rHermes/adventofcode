5c5b29cf8bb37f7d4316e52634e8e794a0a60f31
Teodor Spæren <teodor@spaeren.no>
2021-12-11 18:11:02 +0100

    Cleanup of 2021 Day 11
    This cleaned up quite nicely, I switched to using a queue for the
    flashing part, so I don't have to iterate through the entire array to
    find which ones will be flashing.

--- 2021/11/y2021_d11_p02.py
+++ 2021/11/y2021_d11_p02.py
@@ -1,39 +1,6 @@
 import fileinput as fi
-import re
 import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
-import heapq
 
-# findall, search, parse
-# from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-def ortho(y, x, shape):
-    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
-    sy, sx = shape
-    if 0 < x: yield (y, x-1)
-    if x < sx-1: yield (y, x+1)
-    if 0 < y: yield (y-1, x)
-    if y < sy-1: yield (y+1, x)
-
-# Returns a
 def adj(y, x, shape):
     """Returns all points around a point, given the shape of the array"""
     sy, sx = shape
@@ -48,40 +15,28 @@ def adj(y, x, shape):
             yield (py,px)
 
 
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-
-octo = [[int(c) for c in line] for line in lines]
-
-def step(octo):
-    noc = [[x + 1 for x in row] for row in octo]
-    seen = set()
-    while True:
-        no_flash = True
-        for y in range(10):
-            for x in range(10):
-                if noc[y][x] > 9 and (y,x) not in seen:
-                    no_flash = False
-                    seen.add((y,x))
-                    for (y,x) in adj(y, x, (10,10)):
-                        noc[y][x] += 1
-                    
-        if no_flash:
-            break
-
-    for (y,x) in seen:
-        noc[y][x] = 0
-
-    return (noc, len(seen))
-
-
-noc = octo
+octo = [[int(c) for c in line.rstrip()] for line in fi.input()]
 for i in it.count(1):
-    noc, flashes = step(noc)
-    if flashes == 100:
+    flashing, flashed = set(), set()
+    # First we add 1 to each number
+    for y, x in it.product(range(10), range(10)):
+        octo[y][x] += 1
+        if octo[y][x] > 9:
+            flashing.add((y,x))
+
+    while 0 < len(flashing):
+        y, x = flashing.pop()
+        flashed.add((y,x))
+        for (y,x) in adj(y, x, (10,10)):
+            octo[y][x] += 1
+            if octo[y][x] > 9 and (y,x) not in flashed:
+                flashing.add((y,x))
+
+    for y, x in flashed:
+        octo[y][x] = 0
+
+    if len(flashed) == 100:
         break
-    
+
+
 print(i)
