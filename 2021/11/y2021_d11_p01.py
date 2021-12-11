e8596691512e54ff63830d179b55fe0968aaf6b2
Teodor Spæren <teodor@spaeren.no>
2021-12-11 14:36:25 +0100

    2021 Day 11
    Well this day was very boring, the task was way to easy. I got to use
    my prebuilt "adj" function, which is nice, but other than that it was
    shite.
    
    The reason I was so slow to complete today, was that I was sleeping out
    in the forest and it was really cold in the morning, so I didn't fancy
    getting up and doing it. I did however have my pc with me and I did code
    the solution with freezing hands and a computer that was visibly slow
    from the cold.
    
    I really hope that these tasks pick up at some point, because this is
    getting very boring.
    
    Score:
          --------Part 1--------   --------Part 2--------
    Day       Time   Rank  Score       Time   Rank  Score
     11   04:22:30  13127      0   04:24:38  12822      0

---
+++ 2021/11/y2021_d11_p01.py
@@ -0,0 +1,87 @@
+import fileinput as fi
+import re
+import itertools as it
+import functools as ft
+import string
+import collections
+import math
+import sys
+import heapq
+
+# findall, search, parse
+# from parse import *
+import more_itertools as mit
+# import z3
+# import numpy as np
+# import lark
+# import regex
+# import intervaltree as itree
+
+# print(sys.getrecursionlimit())
+sys.setrecursionlimit(6500)
+
+# Debug logging
+DEBUG = True
+def gprint(*args, **kwargs):
+    if DEBUG: print(*args, **kwargs)
+
+def ortho(y, x, shape):
+    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
+    sy, sx = shape
+    if 0 < x: yield (y, x-1)
+    if x < sx-1: yield (y, x+1)
+    if 0 < y: yield (y-1, x)
+    if y < sy-1: yield (y+1, x)
+
+# Returns a
+def adj(y, x, shape):
+    """Returns all points around a point, given the shape of the array"""
+    sy, sx = shape
+    for dy,dx in it.product([-1,0,1], [-1,0,1]):
+        if dy == 0 and dx == 0:
+            continue
+
+        py = y + dy
+        px = x + dx
+
+        if 0 <= px < sx and 0 <= py < sy:
+            yield (py,px)
+
+
+# Input parsing
+INPUT = "".join(fi.input()).rstrip()
+groups = INPUT.split("\n\n")
+lines = list(INPUT.splitlines())
+numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
+
+octo = [[int(c) for c in line] for line in lines]
+
+def step(octo):
+    noc = [[x + 1 for x in row] for row in octo]
+    seen = set()
+    while True:
+        no_flash = True
+        for y in range(10):
+            for x in range(10):
+                if noc[y][x] > 9 and (y,x) not in seen:
+                    no_flash = False
+                    seen.add((y,x))
+                    for (y,x) in adj(y, x, (10,10)):
+                        noc[y][x] += 1
+                    
+        if no_flash:
+            break
+
+    for (y,x) in seen:
+        noc[y][x] = 0
+
+    return (noc, len(seen))
+
+
+ans = 0
+noc = octo
+for i in range(100):
+    noc, flashes = step(noc)
+    ans += flashes
+    
+print(ans)
