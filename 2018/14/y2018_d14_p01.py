5effb1415c8492ff844a5d881c069b88d616100d
Teodor Spæren <teodor@spaeren.no>
2021-12-12 08:34:15 +0100

    Cleanup of 2018 Day 14
    I've read the solutions thread, and besides askalski's insane solution
    there are no easy speedups.

--- 2018/14/y2018_d14_p01.py
+++ 2018/14/y2018_d14_p01.py
@@ -1,69 +1,14 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
-import heapq
 
-# findall, search, parse
-# from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-def ortho(y, x, shape):
-    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
-    sy, sx = shape
-    if 0 < x: yield (y, x-1)
-    if x < sx-1: yield (y, x+1)
-    if 0 < y: yield (y-1, x)
-    if y < sy-1: yield (y+1, x)
-
-# Returns a
-def adj(y, x, shape):
-    """Returns all points around a point, given the shape of the array"""
-    sy, sx = shape
-    for dy,dx in it.product([-1,0,1], [-1,0,1]):
-        if dy == 0 and dx == 0:
-            continue
-
-        py = y + dy
-        px = x + dx
-
-        if 0 <= px < sx and 0 <= py < sy:
-            yield (py,px)
-
-
-def solve(n):
+def solve(n: int) -> str:
     w = [3, 7]
-    a = 0
-    b = 1
+    a, b = 0, 1
 
-    added = 0
-    i = 0
     while len(w) < n+10:
-        # print(i, w)
         c = w[a] + w[b]
-        digits = [int(x) for x in str(c)]
-        w.extend(digits)
+        w.extend(divmod(c, 10) if c >= 10 else (c,))
         a = (a + w[a] + 1) % len(w)
         b = (b + w[b] + 1) % len(w)
-        i += 1
-        added += len(digits)
 
     lop = []
     for j in range(n,n+10):
@@ -71,11 +16,4 @@ def solve(n):
 
     return "".join(lop)
 
-   
-    
-
-
-assert(solve(9) == "5158916779")
-# print(solve(9))
-# print(solve(18))
-print(solve(652601))
+print(solve(int(next(fi.input()))))
--- 2018/14/y2018_d14_p02.py
+++ 2018/14/y2018_d14_p02.py
@@ -1,27 +1,11 @@
-def part1(n: int) -> str:
-    w = [3, 7]
-    a, b = 0, 1
+import fileinput as fi
 
-    while len(w) < n+10:
-        c = w[a] + w[b]
-        digits = [int(x) for x in str(c)]
-        w.extend(digits)
-        a = (a + w[a] + 1) % len(w)
-        b = (b + w[b] + 1) % len(w)
-
-    lop = []
-    for j in range(n,n+10):
-        lop.append(str(w[j % len(w)]))
-
-    return "".join(lop)
-
-def part2(s: str) -> int:
+def solve(s: str) -> int:
     w = [3, 7]
     cur = "37"
     a, b = 0, 1
-    i = 0
     while True:
-        kw = cur.find(s, -10)
+        kw = cur.find(s, -len(s)-1)
         if kw != -1:
             return kw
 
@@ -32,29 +16,4 @@ def part2(s: str) -> int:
         a = (a + w[a] + 1) % len(w)
         b = (b + w[b] + 1) % len(w)
 
-
-tc_1 = [
-    (5, "0124515891"),
-    (9,"5158916779"),
-    (18, "9251071085"),
-    (2018, "5941429882"),
-    (652601, "1221283494"),
-]
-for i, o in tc_1:
-    k = part1(i)
-    if k != o:
-        print("part1 of {} should yield {}, but gave {}".format(i, o, k))
-
-tc_2 = [
-    ("01245", 5),
-    ("51589", 9),
-    ("92510", 18),
-    ("59414", 2018),
-    ("652601", 20261485),
-]
-for ik, ok in tc_2:
-    kk = part2(ik)
-    if kk != ok:
-        print("part2 of {} should yield {}, but gave {}".format(ik, ok, kk))
-
-print(part2("652601"))
+print(solve(next(fi.input()).rstrip()))
