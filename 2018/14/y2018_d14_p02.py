3fc3193c4f50a7a8db9afb6a79d4b65cdaad0d31
Teodor Spæren <teodor@spaeren.no>
2021-12-12 08:12:47 +0100

    2018 Day 14 Part 2
    Well, this turned out to be easier than expected? I sat down now to
    solve this task, given that I thought you couldn't bruteforce it. I
    implemented a dumb bruteforce solution, to have a baseline to compare
    against and lo and behold the answer popped out, thought a bit slowly.
    
    I think the mistake I made before was that I was only looking back by
    the length of the input, but this doesn't work if you are incrementing
    the string by two. Once this was fixed, it still takes some time, but it
    does finish. I'm going to look at the megathread now to see how other
    people solved it.

--- 2018/14/y2018_d14_p02.py
+++ 2018/14/y2018_d14_p02.py
@@ -1,91 +1,60 @@
-import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
-import heapq
-
-# findall, search, parse
-# from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-def ortho(y, x, shape):
-    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
-    sy, sx = shape
-    if 0 < x: yield (y, x-1)
-    if x < sx-1: yield (y, x+1)
-    if 0 < y: yield (y-1, x)
-    if y < sy-1: yield (y+1, x)
-
-# Returns a
-def adj(y, x, shape):
-    """Returns all points around a point, given the shape of the array"""
-    sy, sx = shape
-    for dy,dx in it.product([-1,0,1], [-1,0,1]):
-        if dy == 0 and dx == 0:
-            continue
-
-        py = y + dy
-        px = x + dx
-
-        if 0 <= px < sx and 0 <= py < sy:
-            yield (py,px)
-
-import tqdm
-
-def solve(n):
+def part1(n: int) -> str:
     w = [3, 7]
-    a = 0
-    b = 1
+    a, b = 0, 1
 
-    needle = n
-
-    added = 0
-    i = 0
-    stack = "37"
-    for wow in tqdm.tqdm(it.count()):
-    # while True:
-        # print(i, w)
+    while len(w) < n+10:
         c = w[a] + w[b]
         digits = [int(x) for x in str(c)]
         w.extend(digits)
-        stack += str(c)
-
-        ak = stack.find(needle, -len(n))
-        if ak > -1:
-            # return ak + len(stack) + 10
-            return ak
-
-
         a = (a + w[a] + 1) % len(w)
         b = (b + w[b] + 1) % len(w)
-        i += 1
-        added += len(digits)
+
+    lop = []
+    for j in range(n,n+10):
+        lop.append(str(w[j % len(w)]))
 
     return "".join(lop)
 
-   
-    
+def part2(s: str) -> int:
+    w = [3, 7]
+    cur = "37"
+    a, b = 0, 1
+    i = 0
+    while True:
+        kw = cur.find(s, -10)
+        if kw != -1:
+            return kw
+
+        c = w[a] + w[b]
+        digits = [int(x) for x in str(c)]
+        w.extend(digits)
+        cur += str(c)
+        a = (a + w[a] + 1) % len(w)
+        b = (b + w[b] + 1) % len(w)
 
 
-# assert(solve("51589") == 9)
-# assert(solve("92510") == 18)
-# # print(solve(9))
-# print(solve("59414"))
-print(solve("652601"))
+tc_1 = [
+    (5, "0124515891"),
+    (9,"5158916779"),
+    (18, "9251071085"),
+    (2018, "5941429882"),
+    (652601, "1221283494"),
+]
+for i, o in tc_1:
+    k = part1(i)
+    if k != o:
+        print("part1 of {} should yield {}, but gave {}".format(i, o, k))
+
+tc_2 = [
+    ("01245", 5),
+    ("51589", 9),
+    ("92510", 18),
+    ("59414", 2018),
+    ("652601", 20261485),
+]
+for ik, ok in tc_2:
+    kk = part2(ik)
+    if kk != ok:
+        print("part2 of {} should yield {}, but gave {}".format(ik, ok, kk))
+
+print(part2("652601"))
