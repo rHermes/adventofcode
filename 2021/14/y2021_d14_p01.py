72e89362cb1aecaba46298af5e55f925e05d4f48
Teodor Spæren <teodor@spaeren.no>
2021-12-14 08:26:02 +0100

    Cleanup of 2021 Day 14
    This cleaned up quite nicely.

--- 2021/14/y2021_d14_p01.py
+++ 2021/14/y2021_d14_p01.py
@@ -1,81 +1,30 @@
 import fileinput as fi
-import re
 import itertools as it
-import functools as ft
-import string
 import collections as cs
-import math
-import sys
-import heapq
 
-# findall, search, parse
-# from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
+inp = map(str.rstrip, fi.input())
+goal = next(inp)
 
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-def ortho(y, x, shape):
-    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
-    sy, sx = shape
-    if 0 < x: yield (y, x-1)
-    if x < sx-1: yield (y, x+1)
-    if 0 < y: yield (y-1, x)
-    if y < sy-1: yield (y+1, x)
-
-def adj(y, x, shape):
-    """Returns all points around a point, given the shape of the array"""
-    sy, sx = shape
-    for dy,dx in it.product([-1,0,1], [-1,0,1]):
-        if dy == 0 and dx == 0:
-            continue
-
-        py = y + dy
-        px = x + dx
-
-        if 0 <= px < sx and 0 <= py < sy:
-            yield (py,px)
-
-
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-grid = [[c for c in line] for line in lines]
+# Skip the blank line
+next(inp)
 
 rules = {}
-for line in groups[1].splitlines():
+for line in inp:
     a, b = line.split(" -> ")
     rules[a] = b
 
-goal = groups[0]
+pairs = cs.Counter(x+y for x, y in zip(goal,goal[1:]))
+counts = cs.Counter(goal)
 
-for i in range(10):
-    ngoal = ""
-    for j in range(len(goal)-1):
-        np = rules[goal[j]+goal[j+1]]
-        ngoal += goal[j] + np
+for _ in range(10):
+    ng = cs.Counter()
+    for pair, times in pairs.items():
+        c = rules[pair]
+        ng[pair[0] + c] += times
+        ng[c + pair[1]] += times
 
-    ngoal += goal[-1]
+        counts[c] += times
 
-    goal = ngoal
-    print("after step",i,":",cs.Counter(goal))
-    # print(cs.Counter(goal))
+    pairs = ng
 
-w = cs.Counter(goal)
-lol = []
-for x in w:
-    lol.append(w[x])
-
-print(max(lol)-min(lol))
+print(max(counts.values())-min(counts.values()))
--- 2021/14/y2021_d14_p02.py
+++ 2021/14/y2021_d14_p02.py
@@ -1,97 +1,30 @@
 import fileinput as fi
-import re
 import itertools as it
-import functools as ft
-import string
 import collections as cs
-import math
-import sys
-import heapq
 
-# findall, search, parse
-# from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
+inp = map(str.rstrip, fi.input())
+goal = next(inp)
 
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-def ortho(y, x, shape):
-    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
-    sy, sx = shape
-    if 0 < x: yield (y, x-1)
-    if x < sx-1: yield (y, x+1)
-    if 0 < y: yield (y-1, x)
-    if y < sy-1: yield (y+1, x)
-
-def adj(y, x, shape):
-    """Returns all points around a point, given the shape of the array"""
-    sy, sx = shape
-    for dy,dx in it.product([-1,0,1], [-1,0,1]):
-        if dy == 0 and dx == 0:
-            continue
-
-        py = y + dy
-        px = x + dx
-
-        if 0 <= px < sx and 0 <= py < sy:
-            yield (py,px)
-
-
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-grid = [[c for c in line] for line in lines]
+# Skip the blank line
+next(inp)
 
 rules = {}
-for line in groups[1].splitlines():
+for line in inp:
     a, b = line.split(" -> ")
     rules[a] = b
 
-goal = groups[0]
+pairs = cs.Counter(x+y for x, y in zip(goal,goal[1:]))
+counts = cs.Counter(goal)
 
-have = cs.defaultdict(int)
-for x,y in mit.pairwise(goal):
-    have[x+y] += 1
+for _ in range(40):
+    ng = cs.Counter()
+    for pair, times in pairs.items():
+        c = rules[pair]
+        ng[pair[0] + c] += times
+        ng[c + pair[1]] += times
 
-have = {k: v for k, v in have.items()}
+        counts[c] += times
 
-occur = cs.defaultdict(int)
-for x in goal:
-    occur[x] += 1 
+    pairs = ng
 
-print(have)
-print(occur)
-for i in range(40):
-    new_have = cs.defaultdict(int)
-    for key, val in have.items():
-        np = rules[key]
-        occur[np] += val
-        new_have[key[0] + np] += val
-        new_have[np + key[1]] += val
-
-
-    have = {k:v for k, v in new_have.items()}
-
-    print("after step",i,":",occur)
-
-# ans = cs.defaultdict(int)
-# for k, v in have.items():
-#     ans[k[0]] += v
-
-# ans[goal[0]] += 1
-# ans[goal[-1]] += 1
-
-print(occur)
-print(max(occur.values())-min(occur.values()))
+print(max(counts.values())-min(counts.values()))
