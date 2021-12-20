a593e475cbc4481a836fa08677244333656a9927
Teodor Spæren <teodor@spaeren.no>
2021-12-20 10:01:46 +0100

    Cleanup of 2018 Day 16
    Used a decorator function to register the operations. For part two, we
    actually only need to go through a small subset of the groups to figure
    out the structure.

--- 2018/16/y2018_d16_p01.py
+++ 2018/16/y2018_d16_p01.py
@@ -1,161 +1,116 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections as cs
-import math
-import sys
-import heapq
 
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-# from bidict import bidict
+all_ops = {}
 
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
+def reg_op(func):
+    """Decorator to add operator to all_ops table"""
+    x, oname = func.__name__.split("_")
+    assert(x == "op")
+    assert(oname not in all_ops)
+    all_ops[oname] = func
 
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
+    return func
 
-def ortho(y, x, shape):
-    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
-    sy, sx = shape
-    if 0 < x: yield (y, x-1)
-    if x < sx-1: yield (y, x+1)
-    if 0 < y: yield (y-1, x)
-    if y < sy-1: yield (y+1, x)
 
-def adj(y, x, shape):
-    """Returns all points around a point, given the shape of the array"""
-    sy, sx = shape
-    for dy,dx in it.product([-1,0,1], [-1,0,1]):
-        if dy == 0 and dx == 0:
-            continue
+@reg_op
+def op_addr(regs, a, b, c):
+    regs[c] = regs[a] + regs[b]
 
-        py = y + dy
-        px = x + dx
+@reg_op
+def op_addi(regs, a, b, c):
+    regs[c] = regs[a] + b
 
-        if 0 <= px < sx and 0 <= py < sy:
-            yield (py,px)
+@reg_op
+def op_mulr(regs, a, b, c):
+    regs[c] = regs[a] * regs[b]
+
+@reg_op
+def op_muli(regs, a, b, c):
+    regs[c] = regs[a] * b
+
+@reg_op
+def op_banr(regs, a, b, c):
+    regs[c] = regs[a] & regs[b]
+
+@reg_op
+def op_bani(regs, a, b, c):
+    regs[c] = regs[a] & b
+
+@reg_op
+def op_borr(regs, a, b, c):
+    regs[c] = regs[a] | regs[b]
+
+@reg_op
+def op_bori(regs, a, b, c):
+    regs[c] = regs[a] | b
+
+@reg_op
+def op_setr(regs, a, b, c):
+    regs[c] = regs[a]
+
+@reg_op
+def op_seti(regs, a, b, c):
+    regs[c] = a
+
+@reg_op
+def op_gtir(regs, a, b, c):
+    regs[c] = int(regs[b] < a)
+
+@reg_op
+def op_gtri(regs, a, b, c):
+    regs[c] = int(b < regs[a])
+
+@reg_op
+def op_gtrr(regs, a, b, c):
+    regs[c] = int(regs[b] < regs[a])
+
+@reg_op
+def op_eqir(regs, a, b, c):
+    regs[c] = int(a == regs[b])
+
+@reg_op
+def op_eqri(regs, a, b, c):
+    regs[c] = int(regs[a] == b)
+
+@reg_op
+def op_eqrr(regs, a, b, c):
+    regs[c] = int(regs[a] == regs[b])
+
+
+def parse_case(case):
+    lines = case.splitlines()
+    ireg = [int(x) for x in lines[0][len("Before: ["):-1].split(", ")]
+    code, a, b, c = map(int, lines[1].split())
+    oreg = [int(x) for x in lines[2][len("After:  ["):-1].split(", ")]
+
+    return ireg, (code, a, b, c), oreg
+
+
+def pos_ops(ireg, oreg, a, b, c):
+    pos = set()
+    for nm, op in all_ops.items():
+        regs = [x for x in ireg]
+        try:
+            op(regs, a, b, c)
+            if regs == oreg:
+                pos.add(nm)
+        except IndexError:
+            pass
+
+    return pos
+
+
+def solve(groups):
+    ans = 0
+    for group in groups[:-2]:
+        ireg, (code, a, b, c), oreg = parse_case(group)
+        pos = pos_ops(ireg, oreg, a, b, c)
+        ans += 3 <= len(pos)
+
+    return ans
 
 
 # Input parsing
 INPUT = "".join(fi.input()).rstrip()
 groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-grid = [[c for c in line] for line in lines]
-gsz = (len(grid), len(grid[0]))
-
-def op_addr(regs, a, b, c):
-    regs[c] = regs[a] + regs[b]
-
-def op_addi(regs, a, b, c):
-    regs[c] = regs[a] + b
-
-def op_mulr(regs, a, b, c):
-    regs[c] = regs[a] * regs[b]
-
-def op_muli(regs, a, b, c):
-    regs[c] = regs[a] * b
-
-def op_banr(regs, a, b, c):
-    regs[c] = regs[a] & regs[b]
-
-def op_bani(regs, a, b, c):
-    regs[c] = regs[a] & b
-
-def op_borr(regs, a, b, c):
-    regs[c] = regs[a] | regs[b]
-
-def op_bori(regs, a, b, c):
-    regs[c] = regs[a] | b
-
-def op_setr(regs, a, b, c):
-    regs[c] = regs[a]
-
-def op_seti(regs, a, b, c):
-    regs[c] = a
-
-def op_gtir(regs, a, b, c):
-    regs[c] = int(regs[b] < a)
-
-def op_gtri(regs, a, b, c):
-    regs[c] = int(b < regs[a])
-
-def op_gtrr(regs, a, b, c):
-    regs[c] = int(regs[b] < regs[a])
-
-def op_eqir(regs, a, b, c):
-    regs[c] = int(a == regs[b])
-
-def op_eqri(regs, a, b, c):
-    regs[c] = int(regs[a] == b)
-
-def op_eqrr(regs, a, b, c):
-    regs[c] = int(regs[a] == regs[b])
-
-all_ops = {
-    "addr": op_addr,
-    "addi": op_addi,
-    "mulr": op_mulr,
-    "muli": op_muli,
-    "banr": op_banr,
-    "bani": op_bani,
-    "borr": op_borr,
-    "bori": op_bori,
-    "setr": op_setr,
-    "seti": op_seti,
-    "gtir": op_gtir,
-    "gtri": op_gtri,
-    "gtrr": op_gtrr,
-    "eqir": op_eqir,
-    "eqri": op_eqri,
-    "eqrr": op_eqrr
-}
-
-assert(len(all_ops) == 16)
-
-def pos_ops(case):
-    lines = case.splitlines()
-    ireg = eval(lines[0].split(": ")[-1])
-    code, a, b, c = map(int, lines[1].split())
-    oreg = eval(lines[2].split(":  ")[-1])
-
-    pos = set()
-    for nm, op in all_ops.items():
-        regs = [x for x in ireg]
-        op(regs, a, b, c)
-        if regs == oreg:
-            pos.add(nm)
-
-
-
-    return pos
-
-
-def solve():
-    # regs = {k: 0 for k in range(4)}
-    # opcode, two values, outputo
-    #       , A and B,  C
-
-    ans = 0
-    for group in groups[:-2]:
-        ww = pos_ops(group)
-        if len(ww) >= 3:
-            ans += 1
-        # print(ww)
-
-    return ans
-
-
-print(solve())
+print(solve(groups))
--- 2018/16/y2018_d16_p02.py
+++ 2018/16/y2018_d16_p02.py
@@ -1,174 +1,127 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections as cs
-import math
-import sys
-import heapq
 
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-# from bidict import bidict
+all_ops = {}
 
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
+def reg_op(func):
+    """Decorator to add operator to all_ops table"""
+    x, oname = func.__name__.split("_")
+    assert(x == "op")
+    assert(oname not in all_ops)
+    all_ops[oname] = func
 
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-def ortho(y, x, shape):
-    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
-    sy, sx = shape
-    if 0 < x: yield (y, x-1)
-    if x < sx-1: yield (y, x+1)
-    if 0 < y: yield (y-1, x)
-    if y < sy-1: yield (y+1, x)
-
-def adj(y, x, shape):
-    """Returns all points around a point, given the shape of the array"""
-    sy, sx = shape
-    for dy,dx in it.product([-1,0,1], [-1,0,1]):
-        if dy == 0 and dx == 0:
-            continue
-
-        py = y + dy
-        px = x + dx
-
-        if 0 <= px < sx and 0 <= py < sy:
-            yield (py,px)
+    return func
 
 
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-grid = [[c for c in line] for line in lines]
-gsz = (len(grid), len(grid[0]))
-
+@reg_op
 def op_addr(regs, a, b, c):
     regs[c] = regs[a] + regs[b]
 
+@reg_op
 def op_addi(regs, a, b, c):
     regs[c] = regs[a] + b
 
+@reg_op
 def op_mulr(regs, a, b, c):
     regs[c] = regs[a] * regs[b]
 
+@reg_op
 def op_muli(regs, a, b, c):
     regs[c] = regs[a] * b
 
+@reg_op
 def op_banr(regs, a, b, c):
     regs[c] = regs[a] & regs[b]
 
+@reg_op
 def op_bani(regs, a, b, c):
     regs[c] = regs[a] & b
 
+@reg_op
 def op_borr(regs, a, b, c):
     regs[c] = regs[a] | regs[b]
 
+@reg_op
 def op_bori(regs, a, b, c):
     regs[c] = regs[a] | b
 
+@reg_op
 def op_setr(regs, a, b, c):
     regs[c] = regs[a]
 
+@reg_op
 def op_seti(regs, a, b, c):
     regs[c] = a
 
+@reg_op
 def op_gtir(regs, a, b, c):
     regs[c] = int(regs[b] < a)
 
+@reg_op
 def op_gtri(regs, a, b, c):
     regs[c] = int(b < regs[a])
 
+@reg_op
 def op_gtrr(regs, a, b, c):
     regs[c] = int(regs[b] < regs[a])
 
+@reg_op
 def op_eqir(regs, a, b, c):
     regs[c] = int(a == regs[b])
 
+@reg_op
 def op_eqri(regs, a, b, c):
     regs[c] = int(regs[a] == b)
 
+@reg_op
 def op_eqrr(regs, a, b, c):
     regs[c] = int(regs[a] == regs[b])
 
-all_ops = {
-    "addr": op_addr,
-    "addi": op_addi,
-    "mulr": op_mulr,
-    "muli": op_muli,
-    "banr": op_banr,
-    "bani": op_bani,
-    "borr": op_borr,
-    "bori": op_bori,
-    "setr": op_setr,
-    "seti": op_seti,
-    "gtir": op_gtir,
-    "gtri": op_gtri,
-    "gtrr": op_gtrr,
-    "eqir": op_eqir,
-    "eqri": op_eqri,
-    "eqrr": op_eqrr
-}
 
-assert(len(all_ops) == 16)
-
-def pos_ops(case):
+def parse_case(case):
     lines = case.splitlines()
-    ireg = eval(lines[0].split(": ")[-1])
+    ireg = [int(x) for x in lines[0][len("Before: ["):-1].split(", ")]
     code, a, b, c = map(int, lines[1].split())
-    oreg = eval(lines[2].split(":  ")[-1])
+    oreg = [int(x) for x in lines[2][len("After:  ["):-1].split(", ")]
 
+    return ireg, (code, a, b, c), oreg
+
+
+def pos_ops(ireg, oreg, a, b, c):
     pos = set()
     for nm, op in all_ops.items():
         regs = [x for x in ireg]
-        op(regs, a, b, c)
-        if regs == oreg:
-            pos.add(nm)
+        try:
+            op(regs, a, b, c)
+            if regs == oreg:
+                pos.add(nm)
+        except IndexError:
+            pass
+
+    return pos
 
 
-
-    return (code, pos)
-
-
-def solve():
-    # regs = {k: 0 for k in range(4)}
-    # opcode, two values, outputo
-    #       , A and B,  C
-
+def solve(groups):
     mm = [set(all_ops.keys()) for _ in range(16)]
+    known = {}
 
     for group in groups[:-2]:
-        code, pos = pos_ops(group)
-        print(code, pos)
+        ireg, (code, a, b, c), oreg = parse_case(group)
+        if code in known:
+            continue
+
+        pos = pos_ops(ireg, oreg, a, b, c)
         mm[code] &= pos
 
-    print(mm)
-    known = {}
-    while len(known) < 16:
-        # First remove all we know
-        for i in range(len(mm)):
-            if i in known:
-                continue
+        if len(mm[code]) == 1:
+            op = mm[code].pop()
+            known[code] = op
 
-            mm[i] -= set(known.values())
+            if len(known) == 16:
+                break
 
-            if len(mm[i]) == 1:
-                known[i] = list(mm[i])[0]
+            for v in mm:
+                v.discard(op)
 
-    print(known)
 
     regs = [0, 0, 0, 0]
     for line in groups[-1].splitlines():
@@ -178,9 +131,7 @@ def solve():
     return regs[0]
 
 
-
-            
-
-
-
-print(solve())
+# Input parsing
+INPUT = "".join(fi.input()).rstrip()
+groups = INPUT.split("\n\n")
+print(solve(groups))
