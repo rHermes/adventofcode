a53534252fc4607dac87385a49bdec6de2addb5f
Teodor Spæren <teodor@spaeren.no>
2022-12-03 06:48:13 +0100

    Cleanup of 2022 Day 3
    It reduced quite nicely. I should really study the more-itertools
    package so that I get the grouper stuff quickly next time. Also remember
    the "string" module, which has the "ascii_letters" values.

--- 2022/03/y2022_d03_p01.py
+++ 2022/03/y2022_d03_p01.py
@@ -1,81 +1,10 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
 import string
-import collections as cs
-import math
-import sys
-import heapq
 
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-# from bidict import bidict
+ans = 0
+for line in filter(bool, map(str.rstrip, fi.input())):
+    n = len(line)//2
+    c = set(line[:n]) & set(line[n:])
+    ans += sum(string.ascii_letters.index(x) + 1 for x in c)
 
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-def ortho(y, x, shape):
-    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
-    sy, sx = shape
-    if 0 < x: yield (y, x-1)
-    if x < sx-1: yield (y, x+1)
-    if 0 < y: yield (y-1, x)
-    if y < sy-1: yield (y+1, x)
-
-def adj(y, x, shape):
-    """Returns all points around a point, given the shape of the array"""
-    sy, sx = shape
-    for dy,dx in it.product([-1,0,1], [-1,0,1]):
-        if dy == 0 and dx == 0:
-            continue
-
-        py = y + dy
-        px = x + dx
-
-        if 0 <= px < sx and 0 <= py < sy:
-            yield (py,px)
-
-
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-grid = [[c for c in line] for line in lines]
-gsz = (len(grid), len(grid[0]))
-
-def solve():
-    s = 0
-    for line in lines:
-        fi = line[:len(line)//2]
-        fa = line[len(line)//2:]
-        print(fi)
-        print(fa)
-        a = set(fi)
-        b = set(fa)
-        c = a & b
-
-        for l in c:
-            if l.islower():
-                s += ord(l) - ord("a") + 1
-            else:
-                s += ord(l) - ord("A") + 27
-            
-        print(c)
-
-    return s
-
-
-print(solve())
+print(ans)
