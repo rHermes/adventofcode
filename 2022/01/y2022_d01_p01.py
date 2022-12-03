4b2c4045ce6b3bcb07877470d9ac81c8e676bfd1
Teodor Spæren <teodor@spaeren.no>
2022-12-03 07:13:32 +0100

    Cleanup of 2022 Day 1
    This cleaned up very well! I ended up using more-itertools here, as it
    has a very nice split_at function, which makes the grouping a breeze. I
    think I'm going to go for using more-itertools more, so that I get used
    to them. That way I can use them faster in competition.

--- 2022/01/y2022_d01_p01.py
+++ 2022/01/y2022_d01_p01.py
@@ -1,72 +1,8 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections as cs
-import math
-import sys
-import heapq
-
-# findall, search, parse
-from parse import *
 import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-# from bidict import bidict
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-def ortho(y, x, shape):
-    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
-    sy, sx = shape
-    if 0 < x: yield (y, x-1)
-    if x < sx-1: yield (y, x+1)
-    if 0 < y: yield (y-1, x)
-    if y < sy-1: yield (y+1, x)
-
-def adj(y, x, shape):
-    """Returns all points around a point, given the shape of the array"""
-    sy, sx = shape
-    for dy,dx in it.product([-1,0,1], [-1,0,1]):
-        if dy == 0 and dx == 0:
-            continue
-
-        py = y + dy
-        px = x + dx
-
-        if 0 <= px < sx and 0 <= py < sy:
-            yield (py,px)
 
 
 # Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-grid = [[c for c in line] for line in lines]
-gsz = (len(grid), len(grid[0]))
-
-def solve():
-    mx = 0
-    s = 0
-    for line in lines:
-        if line:
-            s += int(line)
-        else:
-            mx = max(s, mx)
-            s = 0
-
-
-    return mx
-
-print(solve())
+lines = map(str.rstrip, fi.input())
+groups = mit.split_at(lines, lambda x: not x)
+print(max(sum(map(int,group)) for group in groups))
