18941ed789c361aa0928f1db881ac2aa578aaa72
Teodor Spæren <teodor@spaeren.no>
2022-12-11 09:28:59 +0100

    Cleanup of 2022 Day 11
    This turned out pretty neat, and I wrote a nice explanation in the part
    two file.

--- 2022/11/y2022_d11_p02.py
+++ 2022/11/y2022_d11_p02.py
@@ -1,121 +1,78 @@
 import fileinput as fi
 import re
-import itertools as it
-import functools as ft
-import string
-import collections as cs
 import math
-import sys
 import heapq
 
-import tqdm
-# findall, search, parse
-# from parse import *
-# import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-# from bidict import bidict
+"""
+Ok, so the task is solved like this:
 
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
+We need to figure out a way to limit the numbers. The way we do this, is by
+using modulus arithmetic. + and * are well defined under this property, so what is
+left for us to do is to find the number M, to use as the modulus number.
 
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
+We need M to be such that ((a % M) % N) == a % N for all a's.
 
-def ortho(y, x, shape):
-    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
-    sy, sx = shape
-    if 0 < x: yield (y, x-1)
-    if x < sx-1: yield (y, x+1)
-    if 0 < y: yield (y-1, x)
-    if y < sy-1: yield (y+1, x)
+The first observation here is that N <= M, in order for this to work. The only other
+thing we need is that if a % M == 0, then a % N == 0. As long as these properties
+hold, we can safely perform operations in (mod M) and be sure that the answers in
+(mod N) will be the same.
 
-def adj(y, x, shape):
-    """Returns all points around a point, given the shape of the array"""
-    sy, sx = shape
-    for dy,dx in it.product([-1,0,1], [-1,0,1]):
-        if dy == 0 and dx == 0:
-            continue
+The only way to fit these requirements is if M is a multiple of N. Since we have
+multiple different (mod N), we need to find the least common multiple between all the
+N's, as that number will fulfill the requirements set out above, for all monkeys.
 
-        py = y + dy
-        px = x + dx
+You can read more about this stuff here:
 
-        if 0 <= px < sx and 0 <= py < sy:
-            yield (py,px)
+https://en.wikipedia.org/wiki/Chinese_remainder_theorem
+
+That theorem is about a different problem, but the idea is the same.
+
+"""
+
+regex = (r"Monkey (\d+):\n"
+	r"  Starting items: ((?:\d+, )*\d+)\n"
+	r"  Operation: new = (.+)\n"
+	r"  Test: divisible by (\d+)\n"
+	r"    If true: throw to monkey (\d+)\n"
+	r"    If false: throw to monkey (\d+)")
 
 
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-pos_numbers = [list(map(int, re.findall("[0-9]+", line))) for line in lines]
-grid = [[c for c in line] for line in lines]
-gsz = (len(grid), len(grid[0]))
+def parse_input(txt):
+    groups = txt.split("\n\n")
+    monkeys = []
+    for (id, start_items, op, divt, tmonkey, fmonkey) in re.findall(regex, txt):
+        id, divt, tmonkey, fmonkey = map(int, (id, divt, tmonkey, fmonkey))
+        start_items = [int(x) for x in start_items.split(", ")]
+        monkeys.append((start_items, eval("lambda old: " + op), divt, tmonkey, fmonkey))
 
-def one_round(inspects,monkeys, maxm):
+    return monkeys
+
+
+def one_round(monkeys, inspects, M):
     for i in range(len(monkeys)):
-       # print("Monkey {}".format(i))
-       items, op_str, divt, true_monkey, false_monkey = monkeys[i]
+       items, op, divt, true_monkey, false_monkey = monkeys[i]
+       inspects[i] += len(items)
        for item in items:
-           inspects[i] += 1
-           # print("  Monkey inspects an item with a worry level of {}.".format(item))
-           old = item
-           neww = eval(op_str) % maxm
+           neww = op(item) % M
+
            if neww % divt == 0:
                monkeys[true_monkey][0].append(neww)
            else:
                monkeys[false_monkey][0].append(neww)
 
        monkeys[i][0].clear()
-       # monkeys[i] = ([], op_str, divt, true_monkey, false_monkey)
 
-        
-
-
-def solve():
-    monkeys = []
-    for group in groups:
-        lines = group.splitlines()
-        # id = int(parse("Monkey {:d}:", lines[0])[0]
-        start_items = [int(x) for x in lines[1][len("  Starting items: "):].split(",")]
-        op_str = lines[2][len("  Operation: new = "):]
-        divt = int(lines[3].split()[-1])
-        true_monkey = int(lines[4].split()[-1])
-        false_monkey = int(lines[5].split()[-1])
-        monkeys.append((start_items, op_str, divt, true_monkey, false_monkey))
-        # print(id)
-        # print(start_items)
-        # print(op_str)
-        # print(divt)
-        # print(lines[1].split())
-        # print(id)
-
-    maxm = 1
-    for monkey in monkeys:
-        maxm *= monkey[2]
-  
-    for i in range(len(monkeys)):
-        print("Monkey {}: {}".format(i, ",".join([str(x) for x in monkeys[i][0]])))
+def solve(text):
+    monkeys = parse_input(text)
+    M = math.lcm(*[monkey[2] for monkey in monkeys])
 
     inspects = [0 for _ in monkeys]
+    for _ in range(10000):
+        one_round(monkeys, inspects, M)
 
-    for _ in tqdm.trange(10000):
-        one_round(inspects, monkeys, maxm)
-
-    ww = list(sorted(inspects))
-    a, b = ww[-2:]
+    a, b = heapq.nlargest(2, inspects)
     return a * b
 
-    print(inspects)
 
-    for i in range(len(monkeys)):
-        print("Monkey {}: {}".format(i, ",".join([str(x) for x in monkeys[i][0]])))
-
-
-print(solve())
+INPUT = "".join(fi.input()).rstrip()
+print(solve(INPUT))
