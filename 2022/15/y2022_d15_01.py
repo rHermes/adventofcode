e71a95e5382b9c261ce327647c7deaa55bec1132
Teodor Spæren <teodor@spaeren.no>
2022-12-15 12:18:38 +0100

    Cleanup of 2022 Day 15
    This reduced quite nicely, but I still have no idea of how to do part
    two without z3. I'll have to think about this some more, but first work.

--- 2022/15/y2022_d15_01.py
+++ 2022/15/y2022_d15_01.py
@@ -1,88 +1,29 @@
 import fileinput as fi
 import re
-import itertools as it
-import functools as ft
-import string
-import collections as cs
-import math
-import sys
-import heapq
-
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-# from bidict import bidict
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-def ortho(y, x, shape):
-    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
-    sy, sx = shape
-    if 0 < x: yield (y, x-1)
-    if x < sx-1: yield (y, x+1)
-    if 0 < y: yield (y-1, x)
-    if y < sy-1: yield (y+1, x)
-
-def adj(y, x, shape):
-    """Returns all points around a point, given the shape of the array"""
-    sy, sx = shape
-    for dy,dx in it.product([-1,0,1], [-1,0,1]):
-        if dy == 0 and dx == 0:
-            continue
-
-        py = y + dy
-        px = x + dx
-
-        if 0 <= px < sx and 0 <= py < sy:
-            yield (py,px)
 
 
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-pos_numbers = [list(map(int, re.findall("[0-9]+", line))) for line in lines]
-grid = [[c for c in line] for line in lines]
-gsz = (len(grid), len(grid[0]))
+regex = re.compile(r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)")
 
-def solve():
-    cannot = set()
-    beacons = set()
-    for line in lines:
-        sx,sy,bx,by = parse("Sensor at x={:d}, y={:d}: closest beacon is at x={:d}, y={:d}", line)
-        beacons.add((by,bx))
+cannot = set()
+beacons = set()
 
-        mandist = abs(bx - sx) + abs(by - sy)
-        dx = 0
-        while True:
-            # So we look at the things in row 
-            y = 2000000
-            x = sx + dx
+ty = 2000000
+for line in fi.input():
+    m = re.match(regex, line)
+    if not m:
+        continue
 
-            md = abs(x - sx) + abs(y - sy)
+    sx, sy, bx, by = map(int, m.groups())
 
-            if mandist < md:
-                break
+    beacon_dist = abs(bx - sx) + abs(by - sy)
+    if by == ty:
+        beacons.add(bx)
+   
+    # This is how many steps we have remaining, which is also how many on each
+    # side we have remaining, which cannot be our beacon.
+    remaining = beacon_dist - abs(ty - sy)
+    for x in range(-remaining,remaining+1):
+        cannot.add(sx + x)
 
-            cannot.add((y,sx + dx))
-            cannot.add((y,sx - dx))
-
-            dx += 1
-    
-    return len(cannot - beacons)
-            
-        
-
-print(solve())
+# Print the length of the cannots versus the beacons
+print(len(cannot - beacons))
--- 2022/15/y2022_d15_02.py
+++ 2022/15/y2022_d15_02.py
@@ -1,89 +1,39 @@
 import fileinput as fi
 import re
-import itertools as it
-import functools as ft
-import string
-import collections as cs
-import math
-import sys
-import heapq
 
-# findall, search, parse
-from parse import *
-import more_itertools as mit
 import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-# from bidict import bidict
 
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-def ortho(y, x, shape):
-    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
-    sy, sx = shape
-    if 0 < x: yield (y, x-1)
-    if x < sx-1: yield (y, x+1)
-    if 0 < y: yield (y-1, x)
-    if y < sy-1: yield (y+1, x)
-
-def adj(y, x, shape):
-    """Returns all points around a point, given the shape of the array"""
-    sy, sx = shape
-    for dy,dx in it.product([-1,0,1], [-1,0,1]):
-        if dy == 0 and dx == 0:
-            continue
-
-        py = y + dy
-        px = x + dx
-
-        if 0 <= px < sx and 0 <= py < sy:
-            yield (py,px)
-
-
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-pos_numbers = [list(map(int, re.findall("[0-9]+", line))) for line in lines]
-grid = [[c for c in line] for line in lines]
-gsz = (len(grid), len(grid[0]))
-
+# We need an abs for z3, so here it is
 def myabs(x):
-    return z3.If(x>=0, x,-x)
+    return z3.If(x>=0, x, -x)
 
-def solve():
-    cannot = set()
-    beacons = set()
-    s = z3.Solver()
-    tx, ty = z3.Ints("tx ty")
-    minb = 0
-    maxb = 4000000
-    s.add(minb <= tx)
-    s.add(minb <= ty)
-    s.add(tx <= maxb)
-    s.add(ty <= maxb)
+regex = re.compile(r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)")
 
+s = z3.Solver()
+tx, ty = z3.Ints("tx ty")
+maxb = 4000000
 
-    for line in lines:
-        sx,sy,bx,by = parse("Sensor at x={:d}, y={:d}: closest beacon is at x={:d}, y={:d}", line)
-        
-        mandist = abs(bx - sx) + abs(by - sy)
-        s.add((myabs(tx - sx) + myabs(ty - sy)) > mandist)
+# Constraint the two variables to be within the range given in the task
+s.add(0 <= tx, tx <= maxb)
+s.add(0 <= ty, ty <= maxb)
 
-    print(s.check())
+# For each (sensor, beacon) pair, we add the constraint that the target
+# location has to be further away from the sensor than the beacon is
+for line in fi.input():
+    m = re.match(regex, line)
+    if not m:
+        continue
+
+    sx, sy, bx, by = map(int, m.groups())
+
+    beacon_dist = abs(bx - sx) + abs(by - sy)
+    distress_dist = myabs(tx - sx) + myabs(ty - sy)
+    s.add(beacon_dist < distress_dist)
+
+if s.check() == z3.sat:
     m = s.model()
     vx = m[tx].as_long()
     vy = m[ty].as_long()
-    return vx * 4000000 + vy
-        
-
-print(solve())
+    print(vx * 4000000 + vy)
+else:
+    print("The problem is not solvable")
