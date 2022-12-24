fa0b77e86f2d67d273d292b3f9cf44504fb423d2
Teodor Spæren <teodor@spaeren.no>
2022-12-24 13:20:42 +0100

    2022 Day 23
    Well, we finally got the mandatory "Conway's game of life" variation
    puzzle. This one was fairly straight forward, except that I wasted A LOT
    of time on part one, on a shitty bug in my "adjacency" function. I
    forgot to exclude the "(0,0)" delta, and so I got 9 items, instead of
    just the 8 squares surrounding.
    
    I had to create a visualisation of this to figure it out and that took
    some time. This is the 3rd time this year, that I wished I had an
    unconstrained adjacency function, so I think that I'll include one like
    that in next year.
    
    Part two is not very fast, even when using pypy. I wonder if it's me
    doing something stupid or if it's just a slow task. I will look in the
    megathread to see if there are some smart ways of optimizing this.
    
    The reason I was not able to deliver this when the contest started, was
    that I was on a plane, with no elbow room to really work. So I decided
    to wait until we had arrived at our AirBnB.
    
    Actual time:
    
    Part 1: 00:39:23
    Part 2: 00:01:46
    Total : 00:41:09
    
    Score:
          --------Part 1--------   --------Part 2--------
    Day       Time   Rank  Score       Time   Rank  Score
     23   04:25:17   4870      0   04:27:03   4640      0

---
+++ 2022/23/y2022_d23_p02.py
@@ -0,0 +1,164 @@
+import fileinput as fi
+import re
+import itertools as it
+import functools as ft
+import string
+import collections as cs
+import collections.abc as abc
+import math
+import sys
+import heapq
+
+import typing
+
+# findall, search, parse
+from parse import *
+import more_itertools as mit
+# import z3
+# import numpy as np
+# import lark
+# import regex
+# import intervaltree as itree
+# from bidict import bidict
+
+# print(sys.getrecursionlimit())
+sys.setrecursionlimit(6500)
+
+# Debug logging
+DEBUG = True
+def gprint(*args, **kwargs):
+    if DEBUG: print(*args, **kwargs)
+
+positionT = tuple[int,int]
+def ortho(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
+    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
+    sy, sx = shape
+    if 0 < x: yield (y, x-1)
+    if x < sx-1: yield (y, x+1)
+    if 0 < y: yield (y-1, x)
+    if y < sy-1: yield (y+1, x)
+
+def adj(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
+    """Returns all points around a point, given the shape of the array"""
+    sy, sx = shape
+    for dy,dx in it.product([-1,0,1], [-1,0,1]):
+        if dy == 0 and dx == 0:
+            continue
+
+        py = y + dy
+        px = x + dx
+
+        if 0 <= px < sx and 0 <= py < sy:
+            yield (py,px)
+
+
+# Input parsing
+INPUT = "".join(fi.input()).rstrip()
+groups = INPUT.split("\n\n")
+lines = list(INPUT.splitlines())
+numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
+pos_numbers = [list(map(int, re.findall("[0-9]+", line))) for line in lines]
+grid = [[c for c in line] for line in lines]
+gsz = (len(grid), len(grid[0]))
+
+def print_grid(taken, miny=None, maxy=None, minx=None, maxx=None):
+    if miny is None:
+        miny = min(y for y,x in taken)
+    if maxy is None:
+        maxy = max(y for y,x in taken)
+    if minx is None:
+        minx = min(x for y,x in taken)
+    if maxx is None:
+        maxx = max(x for y,x in taken)
+
+    for y in range(miny,maxy+1):
+        for x in range(minx, maxx+1):
+            if (y,x) in taken:
+                print("#", end="")
+            else:
+                print(".", end="")
+        print("")
+
+    
+
+def solve():
+    my, mx = gsz
+    taken = set()
+    for y in range(my):
+        for x in range(mx):
+            if grid[y][x] == "#":
+                taken.add((y,x))
+
+    dirs = [
+        ((-1,0), (-1,1), (-1, -1)),
+        ((1,0), (1,1), (1,-1)),
+        ((0,-1), (-1, -1), (1, -1)),
+        ((0,1), (-1, 1), (1, 1))
+    ]
+    # print(my,mx)
+
+    # print("== Initial State ==")
+    # print_grid(taken, miny=0, maxy=my-1, minx=0, maxx=mx-1)
+    
+    for iround in range(1,1000000000000000000000000000):
+        moving = set()
+
+        for ty,tx in taken:
+            if (ty, tx) in moving:
+                continue
+
+            for dy,dx in it.product([-1,0,1], [-1,0,1]):
+                if dy == 0 and dx == 0:
+                    continue
+                py = ty + dy
+                px = tx + dx
+                
+                if (py,px) in taken:
+                    moving.add((py,px))
+                    moving.add((ty,tx))
+        
+        props = cs.defaultdict(set)
+        for (ty, tx) in moving:
+            for pm, am, bm in dirs:
+                if any((ty + dy, tx + dx) in taken for (dy,dx) in (pm, am, bm)):
+                    continue
+
+                iy, ix = ty + pm[0], tx + pm[1]
+                props[(iy,ix)].add((ty,tx))
+                break
+
+        first = dirs.pop(0)
+        dirs.append(first)
+        # print(dirs)
+
+        # We now try
+        actually_moved = 0
+        for new_pos, origs in props.items():
+            if 1 < len(origs):
+                continue
+
+            # print(taken)
+            oldpos = list(origs)[0]
+            # print(oldpos)
+            taken.remove(oldpos)
+            taken.add(new_pos)
+            actually_moved += 1
+
+        if actually_moved == 0:
+            break
+
+        # print("")
+        # print("== End of Round {} ==".format(iround))
+        # print_grid(taken, miny=0, maxy=my-1, minx=0, maxx=mx-1)
+
+
+    return iround
+
+            
+
+
+        
+                
+    
+
+print(solve())
