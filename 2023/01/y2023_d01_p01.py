e48d8583221a273127ed84dff074b1f8ceb2be79
Teodor Spæren <teodor@spaeren.no>
2023-12-04 10:59:22 +0100

    2023 Day 01
    I am writing this log retroactively, because I'm in Italy at the moment,
    and it's quite hectic. I will try to be better about this in the future.
    
    I have a couple of things going against me this year. Number one is that
    I haven't practiced any, so there is bound to be some rust in the system
    that will screw me over in the future. Number two is that I haven't
    typed on this keyboard before, so there is bound to be some frustration
    there too. This being said, I am very excited for the tasks this year!
    
    The tasks today was ok, I spent a bit of time on understanding the first
    part, but then it went by rather quickly. The problem came on the second
    part, where I missed out on the important clue, that there can be
    overlapping matches. After I figured this out, it was not so hard to
    solve it. I am pleased with the times, especially as I am not expecting
    much from this beginning!
    
    Score:
          --------Part 1--------   --------Part 2--------
    Day       Time   Rank  Score       Time   Rank  Score
      1   00:01:50    153      0   00:19:10   1624      0

---
+++ 2023/01/y2023_d01_p01.py
@@ -0,0 +1,82 @@
+import fileinput as fi
+import re
+import itertools as it
+import functools as ft
+import string
+import collections as cs
+import collections.abc as abc
+import math
+import sys
+import heapq
+
+import typing
+
+# findall, search, parse
+from parse import *
+import more_itertools as mit
+# import z3
+# import numpy as np
+# import lark
+# import regex
+# import intervaltree as itree
+# from bidict import bidict
+
+# print(sys.getrecursionlimit())
+sys.setrecursionlimit(6500)
+
+# Debug logging
+DEBUG = True
+def gprint(*args, **kwargs):
+    if DEBUG: print(*args, **kwargs)
+
+positionT = tuple[int,int]
+def ortho(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
+    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
+    sy, sx = shape
+    if 0 < x: yield (y, x-1)
+    if x < sx-1: yield (y, x+1)
+    if 0 < y: yield (y-1, x)
+    if y < sy-1: yield (y+1, x)
+
+def adj(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
+    """Returns all points around a point, given the shape of the array"""
+    sy, sx = shape
+    for dy,dx in it.product([-1,0,1], [-1,0,1]):
+        if dy == 0 and dx == 0:
+            continue
+
+        py = y + dy
+        px = x + dx
+
+        if 0 <= px < sx and 0 <= py < sy:
+            yield (py,px)
+
+
+# Input parsing
+INPUT = "".join(fi.input()).rstrip()
+groups = INPUT.split("\n\n")
+lines = list(INPUT.splitlines())
+numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
+pos_numbers = [list(map(int, re.findall("[0-9]+", line))) for line in lines]
+grid = [[c for c in line] for line in lines]
+gsz = (len(grid), len(grid[0]))
+
+
+def solve():
+    ans = 0
+    for line in lines:
+        if line is None:
+            continue 
+    
+        digits = []
+        for c in line:
+            if c in "0123456789":
+                digits.append(c)
+                
+            
+        ans += int(digits[0] + digits[-1])
+        gprint(line)
+
+    return ans
+
+print(solve())
