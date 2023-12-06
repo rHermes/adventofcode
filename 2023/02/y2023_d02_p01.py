2f894e32d8b5344ea049c3753c523c4047a6a499
Teodor Spæren <teodor@spaeren.no>
2023-12-06 16:03:14 +0100

    Cleanup of 2023 Day 2
    This didn't end up being much different from the first implementation,
    but I just cleaned it up a bit.

--- 2023/02/y2023_d02_p01.py
+++ 2023/02/y2023_d02_p01.py
@@ -1,101 +1,29 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections as cs
-import collections.abc as abc
-import math
-import sys
-import heapq
 
-import typing
-
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-# from bidict import bidict
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-positionT = tuple[int,int]
-def ortho(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
-    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
-    sy, sx = shape
-    if 0 < x: yield (y, x-1)
-    if x < sx-1: yield (y, x+1)
-    if 0 < y: yield (y-1, x)
-    if y < sy-1: yield (y+1, x)
-
-def adj(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
-    """Returns all points around a point, given the shape of the array"""
-    sy, sx = shape
-    for dy,dx in it.product([-1,0,1], [-1,0,1]):
-        if dy == 0 and dx == 0:
-            continue
-
-        py = y + dy
-        px = x + dx
-
-        if 0 <= px < sx and 0 <= py < sy:
-            yield (py,px)
-
-
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-pos_numbers = [list(map(int, re.findall("[0-9]+", line))) for line in lines]
-grid = [[c for c in line] for line in lines]
-gsz = (len(grid), len(grid[0]))
-
-def solve():
-    games = []
-    for line in lines:
-        if 0 >= len(line):
-            continue
-
-        pre, po = line.split(": ")
-        id = int(pre.split(" ")[1])
+games = []
+for line in map(str.rstrip, fi.input()):
+    id_part, po = line.split(": ")
+    id = int(id_part.split(" ")[1])
+    
+    rounds_str = po.split("; ")
+    rounds = []
+    for round in rounds_str:
+        items = round.split(", ")
+        balls = {"blue": 0, "green": 0, "red": 0}
+        for i in items:
+            amount, color = i.split(" ")
+            balls[color.strip()] += int(amount)
         
-        rounds = po.split("; ")
-        rr = []
-        for r in rounds:
-            items = r.split(", ")
-            ddd = {"blue": 0, "green": 0, "red": 0}
-            for i in items:
-                x, y = i.split(" ")
-                ddd[y.strip()] += int(x)
-            
-            rr.append(ddd)
-        
-        games.append((id, rr))
+        rounds.append(balls)
+    
+    games.append((id, rounds))
 
-    ans = 0
-    for id, game in games:
-        good = True
-        for r in game:
-            if r["red"] > 12 or r["green"] > 13 or r["blue"] > 14:
-                good = False
-                break
+ans = 0
+for id, game in games:
+    for r in game:
+        if 12 < r["red"] or 13 < r["green"] or 14 < r["blue"]:
+            break
+    else:
+        ans += id
             
-        if good:
-            ans += id
-            
-    return ans 
-        
-
-
-print(solve())
+print(ans)
