4f99e8f7218859ecc30c312a1a3508020f0038fa
Teodor Spæren <teodor@spaeren.no>
2023-12-09 15:01:05 +0100

    Cleanup of 2023 Day 7
    This ended up being quite compact, I took an idea out of Jonathan
    Paulson's solution today. Using the sorted value counts, is very smart!
    
    I think my code is actually a bit cleaner in places, but he did do this
    in realtime, so I guess that is much more impressive, haha.

--- 2023/07/y2023_d07_p01.py
+++ 2023/07/y2023_d07_p01.py
@@ -1,126 +1,30 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
 import collections as cs
-import collections.abc as abc
-import math
-import sys
-import heapq
 
-import typing
+trans = { "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
+        "T": 10, "J": 11, "Q": 12, "K": 13, "A": 14 }
 
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-# from bidict import bidict
+hand_to_score = {
+        (1, 1, 1, 1, 1): 1,
+        (1, 1, 1, 2): 2,
+        (1, 2, 2): 3,
+        (1, 1, 3): 4,
+        (2, 3): 5,
+        (1, 4): 6,
+        (5,): 7
+}
 
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
+scores = []
+for line in map(str.rstrip, fi.input()):
+    hand, bid = line.split(" ")
+    counts = cs.Counter(hand)
+    vals = tuple(sorted(counts.values()))
 
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-positionT = tuple[int,int]
-def ortho(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
-    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
-    sy, sx = shape
-    if 0 < x: yield (y, x-1)
-    if x < sx-1: yield (y, x+1)
-    if 0 < y: yield (y-1, x)
-    if y < sy-1: yield (y+1, x)
-
-def adj(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
-    """Returns all points around a point, given the shape of the array"""
-    sy, sx = shape
-    for dy,dx in it.product([-1,0,1], [-1,0,1]):
-        if dy == 0 and dx == 0:
-            continue
-
-        py = y + dy
-        px = x + dx
-
-        if 0 <= px < sx and 0 <= py < sy:
-            yield (py,px)
+    score = hand_to_score[vals]
+    nh = tuple(trans[c] for c in hand)
+    scores.append((score, nh, int(bid)))
 
 
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-pos_numbers = [list(map(int, re.findall("[0-9]+", line))) for line in lines]
-grid = [[c for c in line] for line in lines]
-gsz = (len(grid), len(grid[0]))
+scores.sort()
 
-
-trans = {
-        "2": 2,
-        "3": 3,
-        "4": 4,
-        "5": 5,
-        "6": 6,
-        "7": 7,
-        "8": 8,
-        "9": 9,
-        "T": 10,
-        "J": 11,
-        "Q": 12,
-        "K": 13,
-        "A": 14
-        }
-
-def solve():
-    scores = []
-    for line in lines:
-        hand, bid = line.split(" ")
-        bid = int(bid)
-        g = cs.Counter(hand)
-        # print(g)
-        score = 0
-        if len(g.keys()) == 5:
-            score = 1
-        elif len(g.keys()) == 4:
-            score = 2
-        elif len(g.keys()) == 3:
-            wl = list(g.values())
-            wl.sort(reverse=True)
-            if wl[0] == 3:
-                score = 4
-            else:
-                score = 3
-        elif len(g.keys()) == 2:
-            wl = list(g.values())
-            wl.sort()
-            if wl[0] == 1:
-                score = 6
-            else:
-                score = 5
-        else:
-            score = 7
-
-        nh = tuple(trans[c] for c in hand)
-        print(nh)
-        
-        scores.append((score, nh, bid))
-    
-    scores.sort()
-    print(scores)
-
-    ans = 0
-    for i, (score, nh, bid) in enumerate(scores, start=1):
-        print(i, bid)
-        ans += i * bid
-    return ans
-
-    
-
-print(solve())
+print(sum(i * bid for i, (_, _, bid) in enumerate(scores, start=1)))
