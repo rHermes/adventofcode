6bd3ed8dbaa03b997dd405c761f8e70297c1d55e
Teodor Spæren <teodor@spaeren.no>
2023-12-09 16:24:21 +0100

    Cleanup of 2023 Day 8
    This ended up being cleaner than I thought, but there are many
    assumptions built into this one. It will not handle non fickly imputs.
    
    I'm a bit done with this for today, but I might return to this one later
    if I have the energy.

--- 2023/08/y2023_d08_p01.py
+++ 2023/08/y2023_d08_p01.py
@@ -1,86 +1,31 @@
 import fileinput as fi
-import re
 import itertools as it
-import functools as ft
-import string
-import collections as cs
-import collections.abc as abc
-import math
-import sys
-import heapq
+import re
 
-import typing
+lines = map(str.rstrip, fi.input())
+dirs = next(lines)
 
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-# from bidict import bidict
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-positionT = tuple[int,int]
-def ortho(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
-    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
-    sy, sx = shape
-    if 0 < x: yield (y, x-1)
-    if x < sx-1: yield (y, x+1)
-    if 0 < y: yield (y-1, x)
-    if y < sy-1: yield (y+1, x)
-
-def adj(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
-    """Returns all points around a point, given the shape of the array"""
-    sy, sx = shape
-    for dy,dx in it.product([-1,0,1], [-1,0,1]):
-        if dy == 0 and dx == 0:
-            continue
-
-        py = y + dy
-        px = x + dx
-
-        if 0 <= px < sx and 0 <= py < sy:
-            yield (py,px)
-
-
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-pos_numbers = [list(map(int, re.findall("[0-9]+", line))) for line in lines]
-grid = [[c for c in line] for line in lines]
-gsz = (len(grid), len(grid[0]))
+# We skip a line, as it will be blank
+next(lines)
 
 nodes = {}
-def solve():
-    for line in groups[1].splitlines():
-        n, b, a = parse("{} = ({}, {})", line)
-        nodes[n] = (b, a)
+for line in lines:
+    m = re.match(r"(\w+) = \((\w+), (\w+)\)", line)
+    if not m:
+        continue
+    n, b, a = m.groups()
+    nodes[n] = (b, a)
+
+current = "AAA"
     
-    current = "AAA"
+steps = 0
+for c in it.cycle(dirs):
+    if current == "ZZZ":
+        break
     
-    steps = 0
-    for c in it.cycle(groups[0].strip()):
-        if current == "ZZZ":
-            return steps
-        
-        if c == "R":
-            current = nodes[current][1]
-        else:
-            current = nodes[current][0]
-        
-        steps += 1
+    current = nodes[current][c == "R"]
+    steps += 1
 
 
 
-print(solve())
+print(steps)
