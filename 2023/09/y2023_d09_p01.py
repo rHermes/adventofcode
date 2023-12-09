0e4cb2ff7d6b93a46a479723c85490317eb23ce0
Teodor Spæren <teodor@spaeren.no>
2023-12-09 16:02:29 +0100

    Cleanup of 2023 Day 9
    This ended up being ultra clean when I just cleaned it up a little. I
    wonder if there isn't a way to avoid keeping all rows in memory. I know
    we could just keep the last and first elements of everything except two
    rows, but I don't see the point in optimizing for it, when the current
    memory usage is so small.

--- 2023/09/y2023_d09_p01.py
+++ 2023/09/y2023_d09_p01.py
@@ -1,90 +1,20 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections as cs
-import collections.abc as abc
-import math
-import sys
-import heapq
 
-import typing
+ans = 0
+for line in fi.input():
+    rows = [[int(x) for x in line.split()]]
 
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-# from bidict import bidict
+    while not all(x == 0 for x in rows[-1]):
+        rows.append([y - x for x, y in zip(rows[-1][:-1], rows[-1][1:])])
+    
+    # We don't need the last row, as it's all zeros
+    rows.pop()
+    rows.reverse()
 
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
+    last_diff = 0
+    for row in rows:
+        last_diff = row[-1] + last_diff
 
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
+    ans += last_diff
 
-positionT = tuple[int,int]
-def ortho(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
-    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
-    sy, sx = shape
-    if 0 < x: yield (y, x-1)
-    if x < sx-1: yield (y, x+1)
-    if 0 < y: yield (y-1, x)
-    if y < sy-1: yield (y+1, x)
-
-def adj(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
-    """Returns all points around a point, given the shape of the array"""
-    sy, sx = shape
-    for dy,dx in it.product([-1,0,1], [-1,0,1]):
-        if dy == 0 and dx == 0:
-            continue
-
-        py = y + dy
-        px = x + dx
-
-        if 0 <= px < sx and 0 <= py < sy:
-            yield (py,px)
-
-
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-pos_numbers = [list(map(int, re.findall("[0-9]+", line))) for line in lines]
-grid = [[c for c in line] for line in lines]
-gsz = (len(grid), len(grid[0]))
-
-def solve():
-    ans = 0
-    for nums in numbers:
-        print(nums)
-        latest = [y-x for (y,x) in zip(nums[1:], nums[:-1])]
-        print(latest)
-        wew = [nums, latest]
-        while not all(x == 0 for x in latest):
-            good = [(y,x)  for y,x in zip(latest[1:], latest[:-1])]
-            latest = [y-x for (y,x) in good]
-            wew.append(latest)
-        
-        print(wew)
-        wew.reverse()
-
-        wew[1].append(wew[1][-1])
-        last_diff = 0
-        
-        for row in wew[1:]:
-            prev = row[-1]
-            prev += last_diff
-            last_diff = prev
-        ans += last_diff  
-
-    return ans
-
-print(solve())
+print(ans)
