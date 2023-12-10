a27e5db6fc9b259e75e2d7a4066c2c1984cbe38d
Teodor Spæren <teodor@spaeren.no>
2023-12-10 22:18:28 +0100

    Cleanup of 2023 Day 10
    This cleaned up really nicely! I learned about the even-odd rule and
    some more about ray casting. This is very nice, I love learning about
    these new tricks that can be used.
    
    I would never have gotten this one, but now I know. There is another
    very cool trick where you combine the "shoelace formula" and "pick's
    theorem" to figure this out. I will look at it later, as it looks like a
    really cool way to solve this problem!

--- 2023/10/y2023_d10_p01.py
+++ 2023/10/y2023_d10_p01.py
@@ -1,120 +1,65 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import graphlib
-import collections as cs
-import collections.abc as abc
-import math
-import sys
-import heapq
-
 import typing
+import collections as cs
 
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-# from bidict import bidict
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-positionT = tuple[int,int]
-def ortho(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
-    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
-    sy, sx = shape
-    if 0 < x: yield (y, x-1)
-    if x < sx-1: yield (y, x+1)
-    if 0 < y: yield (y-1, x)
-    if y < sy-1: yield (y+1, x)
-
-def adj(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
-    """Returns all points around a point, given the shape of the array"""
-    sy, sx = shape
-    for dy,dx in it.product([-1,0,1], [-1,0,1]):
-        if dy == 0 and dx == 0:
-            continue
-
-        py = y + dy
-        px = x + dx
-
-        if 0 <= px < sx and 0 <= py < sy:
-            yield (py,px)
-
-
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-pos_numbers = [list(map(int, re.findall("[0-9]+", line))) for line in lines]
-grid = [[c for c in line] for line in lines]
+grid = [list(line.rstrip()) for line in fi.input()]
 gsz = (len(grid), len(grid[0]))
 
-def solve():
-    G = cs.defaultdict(set)
-    Y, X = gsz
-    for y in range(Y):
-        for x in range(X):
-            c = grid[y][x]
-            east = (y,x+1)
-            north = (y-1, x)
-            west = (y, x-1)
-            south = (y+1,x)
-            if c == ".":
-                continue
-            elif c == "F":
-                G[(y,x)].update((south, east))
-            elif c == "|":
-                G[(y,x)].update((north, south))
-            elif c == "-":
-                G[(y,x)].update((east, west))
-            elif c == "L":
-                G[(y,x)].update((east, north))
-            elif c == "J":
-                G[(y,x)].update((west, north))
-            elif c == "7":
-                G[(y,x)].update((west, south))
-            elif c == "S":
-                # From my input
-                G[(y,x)].update((west, north))
-                # examepl
-                # G[(y,x)].update((south, east))
-                sy, sx = y, x
+# Create grid
+G: typing.DefaultDict[tuple[int,int],set[tuple[int,int]]] = cs.defaultdict(set)
+Y, X = gsz
+sy, sx = 0, 0
+for y in range(Y):
+    for x in range(X):
+        c = grid[y][x]
 
-    
-    print(G)
+        east = (y,x+1)
+        north = (y-1, x)
+        west = (y, x-1)
+        south = (y+1,x)
 
-    seen = set()
-    cur = cs.deque([(sy,sx,0)])
-    maxdepth = 0
-    while cur:
-        sy, sx, depth = cur.popleft()
-        if (sy,sx) in seen:
+        if c == ".":
             continue
+        elif c == "F":
+            G[(y,x)].update((south, east))
+        elif c == "|":
+            G[(y,x)].update((north, south))
+        elif c == "-":
+            G[(y,x)].update((east, west))
+        elif c == "L":
+            G[(y,x)].update((east, north))
+        elif c == "J":
+            G[(y,x)].update((west, north))
+        elif c == "7":
+            G[(y,x)].update((west, south))
+        elif c == "S":
+            parts = set()
+            if 0 < y and grid[y-1][x] in "F|7":
+                    parts.add(north)
+            if y < Y-1 and grid[y+1][x] in "|LJ":
+                    parts.add(south)
+            if 0 < x and grid[y][x-1] in "-LF":
+                    parts.add(west)
+            if x < X-1 and grid[y][x+1] in "-J7":
+                    parts.add(east)
 
-        maxdepth = max(depth, maxdepth)
+            # From my input
+            G[(y,x)].update(parts)
+            sy, sx = y, x
 
-        seen.add((sy,sx))
+seen = set()
+cur = cs.deque([(sy,sx,int(0))])
+maxdepth = 0
+while cur:
+    sy, sx, depth = cur.popleft()
+    if (sy,sx) in seen:
+        continue
 
-        for (ty,tx) in G[(sy,sx)]:
-            cur.append((ty,tx,depth+1))
+    maxdepth = max(depth, maxdepth)
 
-    return maxdepth
+    seen.add((sy,sx))
 
-                
+    for (ty,tx) in G[(sy,sx)] - seen:
+        cur.append((ty,tx,depth+1))
 
-
-
-print(solve())
+print(maxdepth)
--- 2023/10/y2023_d10_p02.py
+++ 2023/10/y2023_d10_p02.py
@@ -1,267 +1,94 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import graphlib
-import collections as cs
-import collections.abc as abc
-import math
-import sys
-import heapq
-
 import typing
+import collections as cs
 
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-# from bidict import bidict
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-positionT = tuple[int,int]
-def ortho(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
-    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
-    sy, sx = shape
-    if 0 < x: yield (y, x-1)
-    if x < sx-1: yield (y, x+1)
-    if 0 < y: yield (y-1, x)
-    if y < sy-1: yield (y+1, x)
-
-def adj(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
-    """Returns all points around a point, given the shape of the array"""
-    sy, sx = shape
-    for dy,dx in it.product([-1,0,1], [-1,0,1]):
-        if dy == 0 and dx == 0:
-            continue
-
-        py = y + dy
-        px = x + dx
-
-        if 0 <= px < sx and 0 <= py < sy:
-            yield (py,px)
-
-
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-pos_numbers = [list(map(int, re.findall("[0-9]+", line))) for line in lines]
-grid = [[c for c in line] for line in lines]
+grid = [list(line.rstrip()) for line in fi.input()]
 gsz = (len(grid), len(grid[0]))
 
-def solve():
-    G = cs.defaultdict(set)
-    Y, X = gsz
-    ground = set()
-    realground = set()
-    notground = set()
+# Create grid
+G: typing.DefaultDict[tuple[int,int],set[tuple[int,int]]] = cs.defaultdict(set)
+Y, X = gsz
+sy, sx = 0, 0
+for y in range(Y):
+    for x in range(X):
+        c = grid[y][x]
 
-    for y in range(0,2*Y):
-        for x in range(0, 2*X):
-            ground.add((y,x))
+        east = (y,x+1)
+        north = (y-1, x)
+        west = (y, x-1)
+        south = (y+1,x)
 
-    for y in range(Y):
-        for x in range(X):
-            c = grid[y][x]
-            vy, vx = 2*y, 2*x
-            east = (vy,vx+1)
-            east2 = (vy,vx+2)
-
-            north = (vy-1, vx)
-            north2 = (vy-2, vx)
-
-            west = (vy, vx-1)
-            west2 = (vy, vx-2)
-
-            south = (vy+1,vx)
-            south2 = (vy+2,vx)
-           
-            # We add everything around here to ground
-            # ground.update(((vy+1,vx+1), (vy+1, vx-1), (vy-1, vx+1), (vy-1, vx-1)))
-
-            realground.add((vy,vx))
-
-            if c == ".":
-                pass
-                # ground.update(((vy,vx), north, south, east, west))
-                # realground.add((vy,vx))
-            elif c == "F":
-                G[(vy,vx)].update((south2, east2))
-                # ground.update((west,north))
-                notground.update((south, east))
-            elif c == "|":
-                G[(vy,vx)].update((north2, south2))
-                # ground.update((east,west))
-                notground.update((south, north))
-            elif c == "-":
-                G[(vy,vx)].update((east2, west2))
-                # ground.update((north,south))
-                notground.update((east, west))
-            elif c == "L":
-                G[(vy,vx)].update((east2, north2))
-                # ground.update((west,south))
-                notground.update((east, north))
-            elif c == "J":
-                G[(vy,vx)].update((west2, north2))
-                # ground.update((east,south))
-                notground.update((west, north))
-            elif c == "7":
-                G[(vy,vx)].update((west2, south2))
-                # ground.update((east,north))
-                notground.update((west, south))
-            elif c == "S":
-                # From my input
-                G[(vy,vx)].update((west2, north2))
-                notground.update(((vy,vx), west, north))
-                starts = [(vy-1,vx-1)]
-                sy,sx = vy, vx
-                # starts = [(vy+1,vx-1), (vy+1,vx), (vy+1,vx+1), (vy, vx+1), (vy-1, vx+1)]
-                # examepl
-                # G[(vy,vx)].update((south2, east2))
-                # notground.update(((vy,vx), east, south))
-                # For example
-                # starts = [(vy+1, vx+1)]
-                # starts = [(vy-1, vx)]
-                # sy, sx = vy+1, vx+1
-
-                # Example 2
-                # G[(vy,vx)].update((west2, south2))
-                # notground.update((west, south))
-                # starts = [(vy+1,vx-1)]
-                # sy, sx = vy,vx
-
-
-    
-    # print(ground)
-    # print((sy,sx))
-
-    
-    cur = cs.deque([(sy,sx)])
-    mainloop = set()
-    while cur:
-        sy, sx = cur.popleft()
-        if (sy,sx) in mainloop:
+        if c == ".":
             continue
+        elif c == "F":
+            G[(y,x)].update((south, east))
+        elif c == "|":
+            G[(y,x)].update((north, south))
+        elif c == "-":
+            G[(y,x)].update((east, west))
+        elif c == "L":
+            G[(y,x)].update((east, north))
+        elif c == "J":
+            G[(y,x)].update((west, north))
+        elif c == "7":
+            G[(y,x)].update((west, south))
+        elif c == "S":
+            parts = set()
+            if 0 < y and grid[y-1][x] in "F|7":
+                    parts.add(north)
+            if y < Y-1 and grid[y+1][x] in "|LJ":
+                    parts.add(south)
+            if 0 < x and grid[y][x-1] in "-LF":
+                    parts.add(west)
+            if x < X-1 and grid[y][x+1] in "-J7":
+                    parts.add(east)
 
-        mainloop.add((sy,sx))
-
-        for node in G[(sy,sx)]:
-            cur.append(node)
-
-    
-    print(mainloop)
-    
-    ground -= notground
-    ground -= mainloop
-    realground -= mainloop
-    realground -= notground
-
-    # for y in range(Y):
-    #     for x in range(X):
-    #         c = grid[y][x]
-    #         vy, vx = 2*y, 2*x
-    #         if (vy,vx) in mainloop:
-    #             print(c, end="")
-    #         else:
-    #             print(" ", end="")
-
-        
-    #     print()
-    # for y in range(2*Y):
-    #     for x in range(2*X):
-    #         if (y,x) in realground:
-    #             print("X", end="")
-    #         elif (y, x) in mainloop:
-    #             c = grid[y//2][x//2]
-    #             print(c, end="")
-    #         elif (y, x) in ground:
-    #             print("█", end="")
-    #         else:
-    #             print(" ", end="")
-
-    #     print("")
-
-    # print("WEW: ", realground)
-    seen = set()
-    cur = cs.deque(starts)
-    maxdepth = 0
-    while cur:
-        sy, sx = cur.popleft()
-        if (sy,sx) in seen or (sy,sx) not in ground:
-            continue
-        
-        seen.add((sy,sx))
-
-        east = (sy,sx+1)
-        north = (sy-1, sx)
-        west = (sy, sx-1)
-        south = (sy+1, sx)
-
-        if east in ground:
-            cur.append(east)
-        if west in ground:
-            cur.append(west)
-        if north in ground:
-            cur.append(north)
-        if south in ground:
-            cur.append(south)
-
-    # for y in range(2*Y):
-    #     for x in range(2*X):
-    #         if (y,x) in seen:
-    #             print("X", end="")
-    #         # elif (y,x) in ground:
-    #         #     print("+", end="")
-    #         else:
-    #             print(" ", end="")
-
-    #     print("")
-
-    for y in range(Y):
-        for x in range(X):
-            vy, vx = 2*y, 2*x
-            c = grid[y][x]
-            if (vy,vx) in mainloop:
-                print(c, end="")
+            G[(y,x)].update(parts)
+            sy, sx = y, x
+            
+            # We have to S, for the ray casting algorithm to work.
+            if north in parts:
+                if south in parts:
+                    grid[y][x] = "|"
+                elif east in parts:
+                    grid[y][x] = "L"
+                else:
+                    grid[y][x] = "J"
             else:
-                print(".", end="")
-        print("")
-
-    # for y in range(Y):
-    #     for x in range(X):
-    #         vy, vx = 2*y, 2*x
-    #         c = grid[y][x]
-    #         if (vy,vx) in (seen & realground):
-    #             print("X", end="")
-    #         else:
-    #             print(" ", end="")
-    #     print("")
-    # print(seen)
-    # print(realground)
-    
-    # print(seen)
-    # good = [(y,x) for (y,x) in seen if y % 2 == 0 and x % 2 == 0]
-    # good.sort()
-    # print(good)
-    # return len(good) 
-    # # return len([(y,x) for (y,x) in seen if y % 2 == 0 and x % 2 == 0])
-    return len(seen & realground)
-
-                
+                # We must have south here
+                if east in parts:
+                    grid[y][x] = "F"
+                elif west in parts:
+                    grid[y][x] = "7"
+                else:
+                    grid[y][x] = "-"
 
 
 
-print(solve())
+# We perform a depth first search here.
+seen = set()
+cur = cs.deque([(sy,sx)])
+while cur:
+    sy, sx = cur.pop()
+    if (sy,sx) in seen:
+        continue
+    seen.add((sy,sx))
+
+    for (ty,tx) in G[(sy,sx)] - seen:
+        cur.append((ty,tx))
+
+# Now we check each row with the even odd rule. The reason we just count L, | and J
+# is that we imagine that the ray is being cast a 3/4 of the way up the cell. These are
+# the only cells which have an upper part. This makes it easier to determine if we are
+# inside or not
+ans = 0
+for y in range(Y):
+    inside = False
+    for x in range(X):
+        c = grid[y][x]
+        if (y,x) in seen and c in "L|J":
+            inside = not inside
+
+        ans += inside and (y,x) not in seen
+
+print(ans)
