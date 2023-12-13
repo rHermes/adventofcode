9518c038838283653f1022124263ef445822cbaf
Teodor Spæren <teodor@spaeren.no>
2023-12-13 12:00:45 +0100

    2023 Day 13
    I didn't have it in my to wake up today, so I timed myself when I
    arrived at work. I wouldn't have made the leaderboard either way with
    these scores.
    
    Today was mildly amusing, but it ended up just being a bruteforce
    solution with some modifications. I spent most of the time on part two,
    not understanding that we wanted a different solution. Once I realized
    it, I again made a mistake, thinking that I could just outlaw a single
    score. This worked, but the problem is that I was making an early return
    in my solution finding. Making a modification may also open up new
    opportunities. I don't really think that the text today was so clear, it
    felt a bit like a trick question.
    
    I am sure there is a very nice way to do this, by finding all pairs of
    rows who only have a single difference, and only mutating on those. I'll
    look into that when I clean up!
    
    Part 1: 00:10:19
    Part 2: 00:20:56
    Total : 00:31:15
    
    Score:
          --------Part 1--------   --------Part 2--------
    Day       Time   Rank  Score       Time   Rank  Score
     13   05:33:39  13593      0   05:54:37  10612      0

---
+++ 2023/13/y2023_d13_p02.py
@@ -0,0 +1,156 @@
+import fileinput as fi
+import re
+import itertools as it
+import functools as ft
+import string
+import collections as cs
+import collections.abc as abc
+import math
+import sys
+import heapq
+
+import typing
+
+# findall, search, parse
+from parse import *
+import more_itertools as mit
+# import z3
+# import numpy as np
+# import lark
+# import regex
+# import intervaltree as itree
+# from bidict import bidict
+
+# print(sys.getrecursionlimit())
+sys.setrecursionlimit(6500)
+
+# Debug logging
+DEBUG = True
+def gprint(*args, **kwargs):
+    if DEBUG: print(*args, **kwargs)
+
+positionT = tuple[int,int]
+def ortho(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
+    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
+    sy, sx = shape
+    if 0 < x: yield (y, x-1)
+    if x < sx-1: yield (y, x+1)
+    if 0 < y: yield (y-1, x)
+    if y < sy-1: yield (y+1, x)
+
+def adj(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
+    """Returns all points around a point, given the shape of the array"""
+    sy, sx = shape
+    for dy,dx in it.product([-1,0,1], [-1,0,1]):
+        if dy == 0 and dx == 0:
+            continue
+
+        py = y + dy
+        px = x + dx
+
+        if 0 <= px < sx and 0 <= py < sy:
+            yield (py,px)
+
+
+# Input parsing
+INPUT = "".join(fi.input()).rstrip()
+groups = INPUT.split("\n\n")
+lines = list(INPUT.splitlines())
+numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
+pos_numbers = [list(map(int, re.findall("[0-9]+", line))) for line in lines]
+
+
+def solve(grid):
+    # grid = [[c for c in line] for line in group.splitlines()]
+    Y, X = (len(grid), len(grid[0]))
+    
+    # print("")
+    # for row in grid:
+    #     print("".join(row))
+    
+    pos = set()
+    # We check vertical.
+    for y in range(1,Y):
+        up = y-1
+        down = y
+
+        while 0 <= up and down < Y:
+            uprow = grid[up]
+            drow = grid[down]
+
+            if uprow != drow:
+                break
+
+            # We advance
+            up -= 1
+            down += 1
+        else:
+            # print("We found a horizontal split at", (y-1) + 1)
+            # print(grid[y-1])
+            pos.add(y * 100)
+
+    # We check vertical.
+    for x in range(1,X):
+        left = x-1
+        right = x
+
+        while 0 <= left and right < X:
+            lcol = [grid[y][left] for y in range(Y)]
+            rcol = [grid[y][right] for y in range(Y)]
+
+            if lcol != rcol:
+                break
+
+            # We advance
+            left -= 1
+            right += 1
+        else:
+            # print("We found a vertical split at", (x-1) + 1)
+            pos.add(x)
+
+
+    # print("found no splits")
+    return pos
+
+def alters(group):
+    grid = [[c for c in line] for line in group.splitlines()]
+    Y, X = (len(grid), len(grid[0]))
+    for y in range(Y):
+        for x in range(X):
+            old = grid[y][x]
+            if old == "#":
+                grid[y][x] = "."
+            else:
+                grid[y][x] = "#"
+
+            yield grid
+            grid[y][x] = old
+
+
+
+
+    # print(group)
+
+ans = 0
+for group in groups:
+    # print(group)
+    grid = [[c for c in line] for line in group.splitlines()]
+    forbidden = solve(grid)
+    # print(forbidden)
+    for alter in alters(group):
+        news = solve(alter)
+        if news - forbidden:
+            # print("==== SOL === ")
+            # for row in alter:
+            #     print("".join(row))
+
+            # print(news)
+            ans += sum(news - forbidden)
+            # print("==== DONE === ")
+            break
+    else:
+        print(forbidden)
+        print("THIS IS A BIG PROBLEM")
+        # ans += forbidden
+    # ans += solve(group)
+print(ans)
