04a950fad0e7d409f7908004f4201c0702eb9eb8
Teodor Spæren <teodor@spaeren.no>
2024-01-01 20:47:09 +0100

    Optimization of 2023 Day 25
    This implements the Karger's algorithm to solve the problem. It is still
    quite slow, but I will try to improve it in various ways. I've found an
    interesting reddit comment that lists a couple of ways to solve this.

--- 2023/25/y2023_d25_p01.py
+++ 2023/25/y2023_d25_p01.py
@@ -1,252 +1,126 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
 import collections as cs
-import collections.abc as abc
-import math
-import sys
-import heapq
+import random
+from dataclasses import dataclass
 
-import typing
-
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-# from bidict import bidict
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-positionT = tuple[int,int]
-def ortho(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
-    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
-    sy, sx = shape
-    if 0 < x: yield (y, x-1)
-    if x < sx-1: yield (y, x+1)
-    if 0 < y: yield (y-1, x)
-    if y < sy-1: yield (y+1, x)
-
-def adj(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
-    """Returns all points around a point, given the shape of the array"""
-    sy, sx = shape
-    for dy,dx in it.product([-1,0,1], [-1,0,1]):
-        if dy == 0 and dx == 0:
-            continue
-
-        py = y + dy
-        px = x + dx
-
-        if 0 <= px < sx and 0 <= py < sy:
-            yield (py,px)
+@dataclass
+class DSUSet:
+    parent: str
+    size: int = 1
 
 
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-pos_numbers = [list(map(int, re.findall("[0-9]+", line))) for line in lines]
-grid = [[c for c in line] for line in lines]
-gsz = (len(grid), len(grid[0]))
+class DSU(object):
+    def __init__(self):
+        self.sets = {}
 
-G = cs.defaultdict(set)
-lsp = []
-for line in lines:
-    src, dsts = line.split(": ")
-    dsts = dsts.split(" ")
-    for d in dsts:
-        G[src].add(d)
-        G[d].add(src)
-        lsp.append((min(src,d), max(src,d)))
+    def MakeSet(self, x):
+        if x in self.sets:
+            return
 
-def get_groups(G):
-    seen = set()
-    groups = []
-    while len(seen) < len(G):
-        groups.append(set())
+        self.sets[x]  = DSUSet(x)
 
-        w = None
-        for k in G.keys():
-            if k not in seen:
-                w = k
+    def Find(self, x):
+        """Finds the root parent for an element"""
+        s = self.sets[x]
+
+        if s.parent != x:
+            s.parent = self.Find(s.parent)
+            return s.parent
+        else:
+            return x
+    
+    def Union(self, x, y):
+        """Add the two items into the same group"""
+        x = self.Find(x)
+        y = self.Find(y)
+
+        if x == y:
+            return
+
+        a = self.sets[x]
+        b = self.sets[y]
+
+        if a.size < b.size:
+            a, b = b, a
+            x, y = y, x
+
+        b.parent = x
+        a.size += b.size
+
+    def Size(self, x):
+        a = self.Find(x)
+        return self.sets[a].size
+        
+
+def krushal(V: set[str], E: list[tuple[str,str]], doUntil=2):
+    D = DSU()
+    for v in V:
+        D.MakeSet(v)
+        
+    ans = 0
+    rest = len(V)
+    for u, v in E:
+        a = D.Find(u)
+        b = D.Find(v)
+
+        if a != b:
+            rest -= 1
+            if rest < doUntil:
+                ans = D.Size(a) * D.Size(b)
+
+                # D.Union(a, b)
                 break
 
-        Q = cs.deque([w])
-        while Q:
-            s = Q.pop()
+            D.Union(a, b)
 
-            if s in seen:
-                continue
-            else:
-                seen.add(s)
-
-            groups[-1].add(s)
-
-            for des in G[s]:
-                if des not in seen:
-                    Q.append(des)
-
-    return groups
-
-def shortest_path(G, a, b):
-    seen = set()
-    prev = {}
-    Q = cs.deque([a])
-    seen.add(a)
-    while Q:
-        s = Q.popleft()
-
-        if s == b:
-            pth = []
-            cur = s
-            while cur != a:
-                pth.append(cur)
-                cur = prev[cur]
-
-            pth.append(a)
-            pth.reverse()
-            return pth
-
-        for d in G[s]:
-            if d not in seen:
-                prev[d] = s
-                seen.add(d)
-                Q.append(d)
     
-    return []
+    numCuts = 0
+    for u, v in E:
+        a = D.Find(u)
+        b = D.Find(v)
 
-
-# print("graph G {")
-# for s, d in lsp:
-#     print(s, "--", d, ";")
-
-# print("}")
-# import tqdm
-# print(len(lsp))
-# lsp.sort()
-# print(len(G.keys()))
-# print(shortest_path(G, "frs", "xhk"))
-
-
-def solve(G, A, B, cuts):
-    if 3 < len(cuts):
-        return None
-
-    # print(cuts)
-
-    p = shortest_path(G, A, B)
-    if not p:
-        print("WE GOT IT BOYS", cuts)
-        return cuts
-
-    for a,b in it.pairwise(p[1:-1]):
-        w = cuts + tuple([(a,b)])
-        G[a].remove(b)
-        G[b].remove(a)
-        ls = solve(G, A, B, w)
-        G[a].add(b)
-        G[b].add(a)
-
-        if ls:
-            return ls
-            
-
-    return None
+        if a != b:
+            numCuts += 1
     
+    # print(numCuts)
+    return (numCuts, ans)
 
-# print(solve(G, []))
+# def fastmincut(V
 
-# maxd = 5
-# maxa = None
-# maxb = None
-# maxps = []
-# nodes = list(G.keys())
+def print_dot_tree(E, F):
+    print("graph G {")
+          # }
+    for u, v in E:
+        # if (u, v) == F[-1]:
+        #     print ("\t{} -- {} [color=\"orange\", penwidth=2.0]".format(u, v))
+        if (u, v) in F:
+            print ("\t{} -- {} [color=\"red\", penwidth=2.0]".format(u, v))
+        else:
+            print("\t{} -- {}".format(u, v))
 
-# for i in range(len(nodes)):
-#     for j in range(i+1, len(nodes)):
-#         a = nodes[i]
-#         b = nodes[j]
-#         p = shortest_path(G, a,b)
-
-#         if maxd <= len(p):
-#             maxd = len(p)
-#             maxa = a
-#             maxb = b
-#             # maxp = p
-#             maxps.append((a,b))
-#             print(maxa, maxb, maxd)
-
-#     if len(maxps) > 20:
-#         break
-
-# maxps = [('zst', 'fnq'), ('zst', 'vdp'), ('zst', 'dzq'), ('zst', 'bjq'), ('zst', 'ddk'), ('zst', 'pkt'), ('zst', 'fpd'), ('zst', 'vsm'), ('zst', 'kqv'), ('mth', 'mqf'), ('mth', 'hhj'), ('mth', 'vnz'), ('mth', 'lxq'), ('mth', 'szf'), ('mth', 'mtv'), ('mth', 'bdh'), ('mth', 'xln'), ('mth', 'qgv'), ('mth', 'mrd'), ('mth', 'svq'), ('mth', 'gpb'), ('mth', 'kxz'), ('mth', 'ncf'), ('mth', 'jvq'), ('mth', 'plt'), ('mth', 'tmn'), ('mth', 'jxk'), ('mth', 'nbb'), ('mth', 'lsv'), ('mth', 'pfz'), ('mth', 'tzs')]
-
-w = solve(G, "zst", "fnq", tuple())
-
-for a,b in w:
-    G[a].remove(b)
-    G[b].remove(a)
-
-grps = get_groups(G)
-print([len(x) for x in grps])
-print(len(grps[0])*len(grps[1]))
-
-# maxps = [('rhn', 'frs'), ('xhk', 'frs'), ('frs', 'ntq')]
-
-# # print(maxps)
-# for I in range(1,4):
-#     heatmap = cs.Counter()
-#     for a, b in maxps:
-#         print(solve(G, a, b, tuple()))
-#         # p = shortest_path(G, a, b)
-#         # print(p)
-#     break
-
-    # print(heatmap)
-    # break
-    # pass
-
-# for igno in tqdm.tqdm(it.combinations(lsp, 3), total=6344781530):
-#     s = set()
-#     a,b,c = igno
-#     s.update(a)
-#     s.update(b)
-#     s.update(c)
-#     if len(s) < 6:
-#         continue
-    
-
-#     for src, d in igno:
-#         G[src].remove(d)
-#         G[d].remove(src)
-    
-    
-#     groups = get_groups(G)
-
-
-#     if len(groups) == 2:
-#         ws = [len(x) for x in groups]
-#         ans = 1
-#         for w in ws:
-#             ans *= w
-
-#         print(ans)
-#         break
+    print("}")
 
 
 
-#     for src, d in igno:
-#         G[src].add(d)
-#         G[d].add(src)
+# Parse input
+V = set()
+E = []
+for line in fi.input():
+    src, *dsts = line.replace(":", "").split()
+    V.add(src)
+    V.update(dsts)
+    for dst in dsts:
+        mmin, mmax = min(src, dst), max(src, dst)
+        E.append((mmin,mmax))
+
+
+# To pin the value for now
+random.seed(1)
+
+EE = list(E)
+while True:
+    random.shuffle(EE)
+    numCuts, ans = krushal(V, EE)
+    if numCuts <= 3:
+        print(ans)
+        break
