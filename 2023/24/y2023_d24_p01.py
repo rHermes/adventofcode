33ce9e2a3d02bcf64c4471cc1bcde6863fcf6eb5
Teodor Spæren <teodor@spaeren.no>
2024-01-07 23:57:02 +0100

    Improve 2023 Day 24 part 1
    This is a very interesting task and I've actually ran into some problems
    implementing it correctly without z3.
    
    The basic idea is that we need to express all the lines as functions of
    the same variable, and then try to see if we can eliminate that
    variable. The way I chose to do this, is to convert them all into
    equations of the form
    
    y = m*x + b
    
    We can do this fairly easily, because the functions for x and y for the
    hailstone paths are linear equations and so (fx(s), fy(s)) is also a
    linear equation, which we can convert to the above form, by simply
    evaluating it for 2 points and then using these to find the normal
    formula.
    
    Once we have them as a normal formula, we just have to figure out for
    which X, their Ys are the same. There is either zero, infinite or
    exactly one solution to this problem. There is actually a tricky part
    here, which is that we need to then calculate the corresponding time
    which this would have occurred for both lines and make sure it's
    positive. If it's not, it's happened in the past.
    
    When I initially implemented this,I actually got too many positives and
    it made me a bit curious. Mathematically I was pretty sure I was right,
    but it seemed that I got some false positives. I immediately thought
    that it could be due to floating point errors and I brought in my z3
    solution and checked which collision tests gave different answers for
    me. It turns out that it was only a few, but these where all ones with
    very close matches.
    
    To make sure this wasn't the problem, I switched to using the
    "fractions" library from the python standard library, to get exact
    representations of all the formulas. This ended up working wonderfully
    and it now gives the correct answer. The problem is that it is really
    not as fast as I would want and I want to see if there isn't a solution
    that doesn't depend on this.
    
    I have the feeling that I'm going about this the wrong way. There should
    be a faster way to do this. I'll check the reddit comments later, maybe
    there is some optimization I have missed. For now, I can only think of
    the optimization, where we remove a line all together, if it doesn't
    intersect the target square at all or in the past. I am not sure there
    even are such lines in the input, but I will check!
    
    Another cool thing to note here, is that this is the second time I'm
    writing this commit message. I ended up quickly writing this commit, so
    that the commit would make it before midnight, but I wanted to rewrite
    it to make sure it made sense. I am not sure this is going to work, but
    I'll guess we will see in a second!

--- 2023/24/y2023_d24_p01.py
+++ 2023/24/y2023_d24_p01.py
@@ -1,116 +1,49 @@
 import fileinput as fi
-import re
 import itertools as it
-import functools as ft
-import string
-import collections as cs
-import collections.abc as abc
-import math
-import sys
-import heapq
+from fractions import Fraction
 
-import typing
+def convert(pos, vel):
+    """This convert them over to standard (m, b) form"""
+    px, py, _ = pos
+    dx, dy, _ = vel
 
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-# from bidict import bidict
+    x0s = Fraction(-px, dx)
+    y0 = dy*x0s + py
 
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
+    x1s = Fraction((1-px), dx)
+    y1 = dy*x1s + py
 
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
+    m = (y1-y0)
+    b = y0
 
-positionT = tuple[int,int]
-def ortho(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
-    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
-    sy, sx = shape
-    if 0 < x: yield (y, x-1)
-    if x < sx-1: yield (y, x+1)
-    if 0 < y: yield (y-1, x)
-    if y < sy-1: yield (y+1, x)
-
-def adj(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
-    """Returns all points around a point, given the shape of the array"""
-    sy, sx = shape
-    for dy,dx in it.product([-1,0,1], [-1,0,1]):
-        if dy == 0 and dx == 0:
-            continue
-
-        py = y + dy
-        px = x + dx
-
-        if 0 <= px < sx and 0 <= py < sy:
-            yield (py,px)
+    return (m, b)
 
 
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-pos_numbers = [list(map(int, re.findall("[0-9]+", line))) for line in lines]
-grid = [[c for c in line] for line in lines]
-gsz = (len(grid), len(grid[0]))
+def collides(a, b, mmin=200000000000000, mmax=400000000000000):
+    (apx, apy, _), (adx, ady, _), (ma, ba) = a
+    (bpx, bpy, _), (bdx, bdy, _), (mb, bb) = b
 
-stones = []
-for line in lines:
-    pre, pos = line.split(" @ ")
-    pre = [int(x) for x in pre.split(", ")]
-    pos = [int(x) for x in pos.split(", ")]
-    stones.append((pre,pos))
+    nom = bb - ba
+    dem = ma - mb
+    if dem == 0:
+        return bb == ba
 
+    cx = nom / dem
+    cy = ma * cx + ba
+    at = (cx - apx) / adx
+    bt = (cx - bpx) / bdx
 
-def collide(A, B, mini, maxi):
-    (ax, ay, az), (avx, avy, avz) = A
-    (bx, by, bz), (bvx, bvy, bvz) = B
+    return 0 <= at and 0 <= bt and (mmin <= cx <= mmax) and (mmin <= cy <= mmax)
 
-    so = z3.Solver()
-    s = z3.Real("s")
-    t = z3.Real("t")
+lines = []
+for line in fi.input():
+    pos, vel = line.split(" @ ")
+    pos = tuple(int(x) for x in pos.split(", "))
+    vel = tuple(int(x) for x in vel.split(", "))
+    lines.append((pos, vel, convert(pos, vel)))
 
-    AX = ax + avx*s
-    AY = ay + avy*s
-
-    BX = bx + bvx*t
-    BY = by + bvy*t
-
-    so.add(mini <= AX, AX <= maxi)
-    so.add(mini <= AY, AY <= maxi)
-
-    so.add(mini <= BX, BX <= maxi)
-    so.add(mini <= BY, BY <= maxi)
-
-    so.add(0 <= s)
-    so.add(0 <= t)
-
-    so.add(AX == BX, AY == BY)
-
-    if so.check() == z3.sat:
-        # print("It can be done")
-        return True
-    else:
-        return False
-
-MINI = 7
-MINI = 200000000000000
-MAXI = 27
-MAXI = 400000000000000
 ans = 0
-for i in range(len(stones)):
-    print(i, len(stones))
-    for j in range(i+1, len(stones)):
-        a = stones[i]
-        b = stones[j]
-        
-        ans += collide(a, b, MINI, MAXI)
+for first, sec in it.combinations(lines, 2):
+    ans += collides(first, sec)
 
 print(ans)
