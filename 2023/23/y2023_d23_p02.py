1af8afd21c9df657a2c8041ed2fa5a4d3722a29f
Teodor Spæren <teodor@spaeren.no>
2024-01-24 17:59:41 +0100

    Cleanup of 2023 Day 23
    This was just another round of removing unneeded imports and removing
    comments.

--- 2023/23/y2023_d23_p02.py
+++ 2023/23/y2023_d23_p02.py
@@ -1,64 +1,8 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
 import collections as cs
-import collections.abc as abc
-import math
-import sys
-import heapq
-
-import typing
-
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-# import z3
-# import numpy as np
-# import lark
-# import regex
-# import intervaltree as itree
-# from bidict import bidict
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-positionT = tuple[int,int]
-def ortho(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
-    """Returns all orthagonaly adjacent points, respecting boundary conditions"""
-    sy, sx = shape
-    if 0 < x: yield (y, x-1)
-    if x < sx-1: yield (y, x+1)
-    if 0 < y: yield (y-1, x)
-    if y < sy-1: yield (y+1, x)
-
-def adj(y: int, x: int, shape: positionT) -> abc.Iterator[positionT]:
-    """Returns all points around a point, given the shape of the array"""
-    sy, sx = shape
-    for dy,dx in it.product([-1,0,1], [-1,0,1]):
-        if dy == 0 and dx == 0:
-            continue
-
-        py = y + dy
-        px = x + dx
-
-        if 0 <= px < sx and 0 <= py < sy:
-            yield (py,px)
-
 
 # Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
-numbers = [list(map(int, re.findall("-?[0-9]+", line))) for line in lines]
-pos_numbers = [list(map(int, re.findall("[0-9]+", line))) for line in lines]
-grid = [[c for c in line] for line in lines]
+grid = [[c for c in line.rstrip()] for line in fi.input()]
 gsz = (len(grid), len(grid[0]))
 Y,X = gsz
 
@@ -72,7 +16,6 @@ for x in range(X):
     if grid[Y-1][x] == ".":
         END = (Y-1,x)
 
-ans = 0
 
 # Let's find all intersections
 intersections = set([START, END])
@@ -90,8 +33,7 @@ for y in range(1,Y-1):
         if 3 <= good:
             intersections.add((y,x))
 
-print(intersections)
-
+# Now we calculate the distance between all intersections
 G = {}
 for ST in intersections:
     G[ST] = {}
@@ -110,8 +52,7 @@ for ST in intersections:
 
             G[ST][(cy,cx)] = steps
             continue
-    
-        # print(gv)
+
         west = (cy, cx-1)
         if 0 < cx and grid[cy][cx-1] != "#":
             Q.append((west, steps+1))
@@ -128,10 +69,9 @@ for ST in intersections:
         if cy < Y-1 and  grid[cy+1][cx] != "#":
             Q.append((south, steps+1))
 
-print(G)
-
 
 Q = [(START, frozenset(), 0)]
+ans = 0
 while Q:
     (cy,cx), prev, steps = Q.pop()
     if (cy,cx) == END:
@@ -147,6 +87,4 @@ while Q:
         if dst not in gv:
             Q.append((dst, gv, steps + cost))
 
-    # print(Q)
-# Because we skipped the first step
 print(ans)
