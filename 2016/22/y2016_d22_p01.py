5977327ccdf74ea14a2bac37ef3bc16edd86e82a
Teodor Spæren <teodor@spaeren.no>
2024-01-24 19:25:17 +0100

    Solve 2016 Day 22 Part 2
    This turned out to be very easy once you realized that we are not trying
    to solve the generalized version of the problem at all, but rather
    something specific to our input. It can be reduced to just a few types
    of nodes, either too big, empty or normal nodes. After that it's just a
    simple calculation of finding the shortest path to the tile before the
    target data and calculating how many steps it would take to move this
    data all the way to the left.
    
    This is of course assuming, that we have no blockers in the top column.

--- 2016/22/y2016_d22_p01.py
+++ 2016/22/y2016_d22_p01.py
@@ -1,57 +1,20 @@
 import fileinput as fi
-import re
 import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
 
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-import z3
-import numpy as np
-import lark
-import regex
-import intervaltree as itree
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
-
-# Input parsing
-INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
-lines = list(INPUT.splitlines())
+inp = fi.input()
+next(inp); next(inp)
 
 nodes = []
-for line in lines[2:]:
+for line in inp:
     nam, sz, used, avail, use = [x for x in line.split(" ") if x]
     sz = int(sz[:-1])
     used = int(used[:-1])
-    avail = int(avail[:-1])
-    use = int(use[:-1])
+    nodes.append((sz, used))
 
-    nodes.append((nam, sz, used, avail, use))
 
 ans = 0
-for i in range(len(nodes)):
-    inam, isz, iused, iavail, iuse = nodes[i]
-    if iuse == 0:
-        continue
-    # for j in range(i+1,len(nodes)):
-    for j in range(len(nodes)):
-        if i == j:
-            continue
-
-        jnam, jsz, jused, javail, juse = nodes[j]
-        if iused <= javail or (juse != 0 and jused <= iavail):
-            ans += 1
-
+for (asz, aused), (bsz, bused) in it.combinations(nodes, 2):
+    ans += aused != 0 and aused <= (bsz-bused)
+    ans += bused != 0 and bused <= (asz-aused)
 
 print(ans)
--- 2016/22/y2016_d22_p02.py
+++ 2016/22/y2016_d22_p02.py
@@ -1,130 +1,9 @@
 import fileinput as fi
-import re
-import itertools as it
-import functools as ft
-import string
-import collections
-import math
-import sys
-
-# findall, search, parse
-from parse import *
-import more_itertools as mit
-import z3
-import numpy as np
-import lark
-import regex
-import intervaltree as itree
-
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(6500)
-
-# Debug logging
-DEBUG = True
-def gprint(*args, **kwargs):
-    if DEBUG: print(*args, **kwargs)
+import collections as cs
 
 # Input parsing
 INPUT = "".join(fi.input()).rstrip()
-groups = INPUT.split("\n\n")
 lines = list(INPUT.splitlines())
-
-
-import heapq
-max_depth = 0
-
-def hash_state(target, nodes):
-    things = tuple(sorted((x, y, sz, used, avail) for (x,y), (sz, used,avail) in nodes.items()))
-    things = target + things
-    return things
-    # if things in seen:
-    #     dl = seen[things]
-    #     if dl < depth:
-    #         return None
-
-    # seen[things] = depth
-
-def solve(nodes, target, limits):
-    max_x, max_y = limits
-    Q = [(target[0] + target[1], 0, target, ())]
-
-    max_depth = 0
-    seen = set()
-    seen_targets = set()
-    while len(Q) > 0:
-        # Transform the grid
-        score, depth, target, moves = heapq.heappop(Q)
-        
-        if depth > max_depth:
-            max_depth = depth
-            print("New max depth: {}".format(max_depth))
-
-        if target not in seen_targets:
-            print("We saw target {} for the first time at depth: {}".format(target, depth))
-            seen_targets.add(target)
-        if target == (0, 0):
-            return depth
-
-        revert = {}
-        # print(moves)
-        for (sx, sy), (dx, dy) in moves:
-            if (sx,sy) not in revert:
-                revert[(sx,sy)] = nodes[(sx,sy)]
-
-            if (dx, dy) not in revert:
-                revert[(dx, dy)] = nodes[(dx,dy)]
-
-            ssz, sused, savail = nodes[(sx,sy)]
-            dsz, dused, davail = nodes[(dx,dy)]
-
-            nodes[(sx, sy)] = (ssz, 0, ssz)
-            nodes[(dx, dy)] = (dsz, dused + sused, davail - sused)
-
-        do = True
-        hh = hash(hash_state(target, nodes))
-        if hh in seen:
-            do = False
-        else:
-            seen.add(hh)
-
-            
-
-        for (px, py), (sz, used, avail) in nodes.items():
-            if not do:
-                break
-
-            if used == 0:
-                continue
-
-            for (dx, dy) in [(0,1), (0,-1), (1,0), (-1,0)]:
-                gx, gy = px + dx, py + dy
-                if not (0 <= gx <= max_x and 0 <= gy <= max_y):
-                    continue
-                # if (gx, gy) not in nodes:
-
-                gsz, gused, gavail = nodes[(gx, gy)]
-
-                if used > gavail:
-                    continue
-            
-                # Ok, potential candidate
-                if (px, py) == target:
-                    # heapq.heappush(Q, (depth + 1 + gx + gy, depth + 1, (gx, gy), moves + (((px, py), (gx, gy)),)))
-                    heapq.heappush(Q, (0, depth + 1, (gx, gy), moves + (((px, py), (gx, gy)),)))
-                else:
-                    # heapq.heappush(Q, (depth + 1 + target[0] + target[1], depth + 1, target, moves + (((px, py), (gx, gy)),)))
-                    heapq.heappush(Q, (0, depth + 1, target, moves + (((px, py), (gx, gy)),)))
-
-    
-        # revert board
-        for (x, y), (sz, used, avail) in revert.items():
-            nodes[(x,y)] = (sz, used, avail)
-
-
-
-
-
-
 
 target_x = 0
 max_y = 0
@@ -136,13 +15,61 @@ for line in lines[2:]:
     y = int(y[1:])
     sz = int(sz[:-1])
     used = int(used[:-1])
-    avail = int(avail[:-1])
-    use = int(use[:-1])
 
     target_x = max(target_x, x)
     max_y = max(max_y, y)
-    nodes[(x,y)] = (sz, used, avail) 
+    nodes[(x,y)] = (sz, used) 
 
-# print(target_x)
-# print(nodes)
-print(solve(nodes, (target_x, 0), (target_x, max_y)))
+Y = max_y
+X = target_x
+
+# We need to convert this node field, into something we can use.
+# we will begin with removing all nodes, that could never be moved
+# around
+open_spot = (0,0)
+blocked = set()
+for y in range(0, Y+1):
+    for x in range(0, X+1):
+        sz, used = nodes[(x,y)]
+        if 100 < sz:
+            blocked.add((x,y))
+        elif used == 0:
+            open_spot = (x,y)
+
+def bfs(blocked, start, stop, X, Y):
+    seen = set()
+    Q = cs.deque([(start, int(0))])
+    while Q:
+        pos, steps = Q.popleft()
+        if pos in seen:
+            continue
+        else:
+            seen.add(pos)
+
+        if pos == stop:
+            return steps
+
+        px, py = pos
+        for (dx, dy) in [(0,1), (0,-1), (1,0), (-1,0)]:
+            gx, gy = px + dx, py + dy
+            if not (0 <= gx <= X and 0 <= gy <= Y):
+                continue
+            if (gx,gy) in blocked:
+                continue
+
+            Q.append(((gx, gy), steps + 1))
+            # if (gx, gy) not in nodes:
+
+
+# There are 2 stages here.
+# how much it costs to get the solution into the step before.
+ans = bfs(blocked, open_spot, (target_x-1, 0), X, Y)
+
+# we move the target one closer
+ans += 1
+
+# We now need to move it (target_x-1) turns to the right,
+# which takes 5 steps for each move to the right we make.
+ans += 5*(target_x-1)
+
+print(ans)
